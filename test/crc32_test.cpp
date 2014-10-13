f2d36237a3e1084bd6fa46226f0926f6aafa4380
Michael Spieler <the.chaotic.coder@gmail.com>
2014-10-13 09:16:51 +0200

    Added simple test.

---
+++ test/crc32_test.cpp
@@ -0,0 +1,16 @@
+#include "CppUTest/TestHarness.h"
+#include "crc32.h"
+
+TEST_GROUP(CRC32TestGroup)
+{
+
+};
+
+TEST(CRC32TestGroup, SingleTest)
+{
+    const char *data = "123456789";
+    uint32_t init = 0;
+    uint32_t crc = crc32(init, data, strlen(data));
+    uint32_t expect = 0xcbf43926;
+    CHECK_EQUAL(expect, crc);
+}
