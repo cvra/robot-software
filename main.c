e26893f89276262d49ab21862361136a3c28f504
Antoine Albertelli <antoinea101@gmail.com>
2015-01-15 18:13:54 +0100

    Add thread name to thread command

--- main.c
+++ main.c
@@ -349,13 +349,13 @@ static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[]) {
     chprintf(chp, "Usage: threads\r\n");
     return;
   }
-  chprintf(chp, "    addr    stack prio refs     state time\r\n");
+  chprintf(chp, "    addr    stack prio refs     state       time\r\n");
   tp = chRegFirstThread();
   do {
-    chprintf(chp, "%.8lx %.8lx %4lu %4lu %9s %lu\r\n",
+    chprintf(chp, "%.8lx %.8lx %4lu %4lu %9s %10lu %s\r\n",
             (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
             (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
-            states[tp->p_state], (uint32_t)tp->p_time);
+            states[tp->p_state], (uint32_t)tp->p_time, tp->p_name);
     tp = chRegNextThread(tp);
   } while (tp != NULL);
 }
