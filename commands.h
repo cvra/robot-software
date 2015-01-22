d8766ef6d2b46d293c67aeda9d9757c2841384ba
Antoine Albertelli <antoinea101@gmail.com>
2015-01-22 14:05:55 +0100

    Split debug commands in another file

---
+++ commands.h
@@ -0,0 +1,10 @@
+#ifndef COMMANDS_H_
+#define COMMANDS_H_
+
+#include "shell.h"
+
+/** Array of standard shell commands. */
+extern const ShellCommand commands[];
+
+
+#endif
--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "test.h"
 
 #include "chprintf.h"
-#include "shell.h"
+#include "commands.h"
 
 #include <lwip/netif.h>
 #include "sntp/sntp.h"
@@ -273,137 +273,6 @@ static const SerialUSBConfig serusbcfg = {
 
 /* Command line related.                                                     */
 #define SHELL_WA_SIZE   THD_WORKING_AREA_SIZE(2048)
-#define TEST_WA_SIZE    THD_WORKING_AREA_SIZE(256)
-
-static void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[]) {
-    size_t n, size;
-
-    (void)argv;
-    if (argc > 0) {
-        chprintf(chp, "Usage: mem\r\n");
-        return;
-    }
-    n = chHeapStatus(NULL, &size);
-    chprintf(chp, "core free memory : %u bytes\r\n", chCoreGetStatusX());
-    chprintf(chp, "heap fragments   : %u\r\n", n);
-    chprintf(chp, "heap free total  : %u bytes\r\n", size);
-}
-
-static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[]) {
-    static const char *states[] = {CH_STATE_NAMES};
-    thread_t *tp;
-
-    (void)argv;
-    if (argc > 0) {
-        chprintf(chp, "Usage: threads\r\n");
-        return;
-    }
-    chprintf(chp, "    addr    stack prio refs     state       time\r\n");
-    tp = chRegFirstThread();
-    do {
-        chprintf(chp, "%.8lx %.8lx %4lu %4lu %9s %10lu %s\r\n",
-                (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
-                (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
-                states[tp->p_state], (uint32_t)tp->p_time, tp->p_name);
-        tp = chRegNextThread(tp);
-    } while (tp != NULL);
-}
-
-static void cmd_test(BaseSequentialStream *chp, int argc, char *argv[]) {
-    thread_t *tp;
-
-    (void)argv;
-    if (argc > 0) {
-        chprintf(chp, "Usage: test\r\n");
-        return;
-    }
-    tp = chThdCreateFromHeap(NULL, TEST_WA_SIZE, chThdGetPriorityX(),
-            TestThread, chp);
-    if (tp == NULL) {
-        chprintf(chp, "out of memory\r\n");
-        return;
-    }
-    chThdWait(tp);
-}
-
-static void cmd_ip(BaseSequentialStream *chp, int argc, char **argv) {
-    (void) argv;
-    (void) argc;
-
-    struct netif *n; /* used for iteration. */
-
-    for (n = netif_list; n != NULL; n = n->next) {
-        /* Converts the IP adress to a human readable format. */
-        char ip[17], gw[17], nm[17];
-        ipaddr_ntoa_r(&n->ip_addr, ip, 17);
-        ipaddr_ntoa_r(&n->netmask, nm, 17);
-        ipaddr_ntoa_r(&n->gw, gw, 17);
-
-        chprintf(chp, "%s%d: %s, nm: %s, gw:%s\r\n", n->name, n->num, ip, nm, gw);
-    }
-}
-
-static void cmd_crashme(BaseSequentialStream *chp, int argc, char **argv) {
-    (void) argv;
-    (void) argc;
-    (void) chp;
-
-    chSysHalt(__FUNCTION__);
-}
-
-static void cmd_panic_log(BaseSequentialStream *chp, int argc, char **argv) {
-    (void) argv;
-    (void) argc;
-    const char *message;
-
-    message = panic_log_read();
-
-    if (message == NULL) {
-        chprintf(chp, "Did not reboot after a panic.");
-    } else {
-        chprintf(chp, "%s", message);
-    }
-    chprintf(chp, "\r\n");
-}
-
-static void cmd_time(BaseSequentialStream *chp, int argc, char **argv)
-{
-    (void) argv;
-    (void) argc;
-
-    unix_timestamp_t ts;
-    int h, m;
-
-    /* Get current time */
-    int now = ST2US(chVTGetSystemTime());
-    ts = timestamp_local_us_to_unix(now);
-    chprintf(chp, "Current scheduler tick: %12ld\r\n", now);
-    chprintf(chp, "Current UNIX timestamp: %12ld\r\n", ts.s);
-    chprintf(chp, "current time (ms):      %12ld\r\n", ST2MS(chVTGetSystemTime()));
-
-    /* Get time since start of day */
-    ts.s = ts.s % (24 * 60 * 60);
-
-    h = ts.s / 3600;
-    ts.s = ts.s % 3600;
-
-    m = ts.s / 60;
-    ts.s = ts.s % 60;
-
-    chprintf(chp, "Current time: %02d:%02d:%02d\r\n", h, m, ts.s);
-}
-
-
-static const ShellCommand commands[] = {
-    {"mem", cmd_mem},
-    {"ip", cmd_ip},
-    {"threads", cmd_threads},
-    {"test", cmd_test},
-    {"panic_log", cmd_panic_log},
-    {"crashme", cmd_crashme},
-    {"time", cmd_time},
-    {NULL, NULL}
-};
 
 static const ShellConfig shell_cfg1 = {
     (BaseSequentialStream *)&SDU1,
