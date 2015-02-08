18b6b2e1e43ffac58d8ff288dd4a2f90c2c795ab
Florian Reinhard <florian.reinhard@epfl.ch>
2015-02-08 18:28:08 +0100

    PWM with synchronized drive pump recharge

--- src/main.c
+++ src/main.c
@@ -4,16 +4,69 @@
 
 BaseSequentialStream* stdout;
 
-#define PWM_PERIOD      2880
+#define PWM_PERIOD                  2880
+#define PWM_DIRECTION_CHANNEL       0
+#define PWM_POWER_CHANNEL           1
+
+#define DIRECTION_DC_LOW            0
+#define DIRECTION_DC_HIGH           PWM_PERIOD
+#define DIRECTION_DC_RECHARGE       (0.75 * PWM_PERIOD)     // 10us
+#define POWR_DC_RECHARGE_CORRECTION (PWM_PERIOD - DIRECTION_DC_RECHARGE)
+#define RECHARGE_COUNTDOWN_RELOAD   50                      // every 2ms
 
 #define ADC_MAX         4096
 #define ADC_TO_AMPS     0.001611328125f
 #define ADC_TO_VOLTS    0.005283043033f
 
+
+/*
+ * To prevent common-mode steps on the current measurement shunt resistor,
+ * one side of the H-bridge is static (high or low), and the other does PWM.
+ * The MOS-driver doesn't allow static high, so we have to switch to low every
+ * 2ms for a duration of 10us to recharge the charge pump.
+ *
+ * Both H-bridge sides are connected to the same timer for PWM. The static side
+ * is either at 0% or 100% duty cycle. In the case of 100%, every 2ms the duty
+ * cycle is changed to 75% (resulting in a low time of 10us for a PWM frequency
+ * of 25kHz) for a single cycle, to recharge the charge pump.
+ *
+ * At every 50th counter reset interrupt, the duty cycle of the static side is
+ * changed from 100% to 75% (or back, depending on the recharge flag).
+ */
+
+
+static uint32_t power_pwm;
+
+void pwm_counter_reset(PWMDriver *pwmd)
+{
+    static uint8_t recharge_countdown = RECHARGE_COUNTDOWN_RELOAD;
+    uint32_t direction_dc = pwmd->tim->CCR[PWM_DIRECTION_CHANNEL];
+
+    if (direction_dc == DIRECTION_DC_LOW) {
+        recharge_countdown = RECHARGE_COUNTDOWN_RELOAD;
+    } else if (direction_dc == DIRECTION_DC_RECHARGE) {
+        pwmd->tim->CCR[PWM_DIRECTION_CHANNEL] = DIRECTION_DC_HIGH;
+    }
+
+    if (recharge_countdown == 0) {
+        recharge_countdown = RECHARGE_COUNTDOWN_RELOAD;
+        pwmd->tim->CCR[PWM_DIRECTION_CHANNEL] = DIRECTION_DC_RECHARGE;
+        if (power_pwm < POWR_DC_RECHARGE_CORRECTION) {
+            pwmd->tim->CCR[PWM_POWER_CHANNEL] = 0;
+        } else {
+            pwmd->tim->CCR[PWM_POWER_CHANNEL] = power_pwm - POWR_DC_RECHARGE_CORRECTION;
+        }
+    } else {
+        pwmd->tim->CCR[PWM_POWER_CHANNEL] = power_pwm;
+    }
+
+    recharge_countdown--;       // don't forget this
+}
+
 static const PWMConfig pwm_cfg = {
-    72000000/10,
-    PWM_PERIOD,           // 2.5kHz
-    NULL,
+    72000000,
+    PWM_PERIOD,           // 25kHz
+    pwm_counter_reset,
     // activate channel 1 and 2
     {
         {PWM_OUTPUT_ACTIVE_HIGH, NULL},
@@ -28,6 +81,24 @@ static const PWMConfig pwm_cfg = {
 static void pwm_setup(void)
 {
     pwmStart(&PWMD1, &pwm_cfg);
+    pwmEnablePeriodicNotification(&PWMD1);
+}
+
+static void set_pwm(float dc)
+{
+    if (dc > 0.95) {
+        dc = 0.95;
+    } else if (dc < -0.95){
+        dc = -0.95;
+    }
+
+    if (dc < 0) {
+        pwmEnableChannel(&PWMD1, PWM_DIRECTION_CHANNEL, DIRECTION_DC_HIGH);
+        power_pwm = (1 + dc) * PWM_PERIOD;
+    } else {
+        pwmEnableChannel(&PWMD1, PWM_DIRECTION_CHANNEL, DIRECTION_DC_LOW);
+        power_pwm = dc * PWM_PERIOD;
+    }
 }
 
 
@@ -50,7 +121,7 @@ static THD_FUNCTION(quad_task, arg)
     STM32_TIM4->CR1    = 1;                         // start
 
     while(42){
-        chprintf(stdout, "ENC1: %d\n", STM32_TIM4->CNT);
+        //chprintf(stdout, "ENC1: %d\n", STM32_TIM4->CNT);
         chThdSleepMilliseconds(100);
     }
     return 0;
@@ -87,7 +158,7 @@ static THD_FUNCTION(adc_task, arg)
         }
         mean_current /= 1000;
 
-        //chprintf(stdout, "%f A, %f V\n", mean_current, adc_samples[3]*ADC_TO_VOLTS);
+        chprintf(stdout, "%f A, %f V\n", mean_current, adc_samples[3]*ADC_TO_VOLTS);
         chThdSleepMilliseconds(100);
     }
     return 0;
@@ -100,8 +171,7 @@ int main(void) {
 
     pwm_setup();
 
-    pwmEnableChannel(&PWMD1, 0, 0.0 * PWM_PERIOD);
-    pwmEnableChannel(&PWMD1, 1, 0.0 * PWM_PERIOD);
+    set_pwm(0.0);
 
     palSetPad(GPIOA, GPIOA_MOTOR_EN_A);
     palSetPad(GPIOA, GPIOA_MOTOR_EN_B);
@@ -118,8 +188,10 @@ int main(void) {
 
     while (1) {
         palSetPad(GPIOA, GPIOA_LED);
-        chThdSleepMilliseconds(500);
+        set_pwm(-0.2);
+        chThdSleepMilliseconds(1000);
         palClearPad(GPIOA, GPIOA_LED);
-        chThdSleepMilliseconds(500);
+        set_pwm(0.2);
+        chThdSleepMilliseconds(1000);
     }
 }
