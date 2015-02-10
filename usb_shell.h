82f422002a5391524c3b0155386cc14692fe3225
Antoine Albertelli <antoinea101@gmail.com>
2015-02-10 22:18:37 +0100

    Revert "Move USB - serial functions into own file"
    This reverts commit b9f7596cc7625276ed80bc6ea20c3dc0da681583.
    
    Conflicts:
    	main.c
    	package.yml
    	usb_shell.c

--- usb_shell.h
+++
@@ -1,9 +0,0 @@
-#ifndef USB_SHELL_C_
-#define USB_SHELL_C_
-
-#include "hal.h"
-
-/** Thread function keeping a shell open on the USB to serial port. */
-msg_t usb_shell_thread(void *dummy);
-
-#endif
