2688e2082b634bdca0ef543e9f818226ac020eaf
Antoine Albertelli <antoinea101@gmail.com>
2015-02-11 11:01:01 +0100

    Switch to forking TCP server

--- src/can_bridge.c
+++ src/can_bridge.c
@@ -4,8 +4,19 @@
 #define CAN_BRIDGE_STACKSIZE 2048
 THD_WORKING_AREA(wa_can_bridge, CAN_BRIDGE_STACKSIZE);
 
+#define CAN_BRIDGE_RX_STACKSIZE 1024
+#define CAN_BRIDGE_TX_STACKSIZE 1024
+
 /** Thread running the CAN bridge. */
 msg_t can_bridge_thread(void *p);
+msg_t can_bridge_rx_thread(void *p);
+msg_t can_bridge_tx_thread(void *p);
+
+struct can_bridge_instance_t {
+    struct netconn *conn;
+    binary_semaphore_t tx_finished;
+    mutex_t lock;
+};
 
 const char *data = "Hello, world\n";
 
@@ -21,6 +32,7 @@ void can_bridge_init(void)
 msg_t can_bridge_thread(void *p)
 {
     struct netconn *conn, *client_conn;
+    struct can_bridge_instance_t *instance;
     int error;
 
     (void)p;
@@ -32,6 +44,7 @@ msg_t can_bridge_thread(void *p)
     netconn_bind(conn, IP_ADDR_ANY, 1337);
     netconn_listen(conn);
 
+
     while (1) {
         error = netconn_accept(conn, &client_conn);
 
@@ -39,11 +52,56 @@ msg_t can_bridge_thread(void *p)
             continue;
         }
 
-        netconn_write(client_conn, data, strlen(data), NETCONN_NOCOPY);
-        netconn_close(client_conn);
+        instance = malloc(sizeof(struct can_bridge_instance_t));
+        instance->conn = client_conn;
 
-        netconn_delete(client_conn);
+        chBSemObjectInit(&instance->tx_finished, true);
+        chMtxObjectInit(&instance->lock);
+
+
+        chThdCreateFromHeap(NULL, /* Use system heap */
+                            CAN_BRIDGE_RX_STACKSIZE,
+                            CAN_BRIDGE_PRIO,
+                            can_bridge_rx_thread,
+                            (void *)instance);
+
+        chThdCreateFromHeap(NULL, /* Use system heap */
+                            CAN_BRIDGE_TX_STACKSIZE,
+                            CAN_BRIDGE_PRIO,
+                            can_bridge_rx_thread,
+                            (void *)instance);
     }
+    return MSG_OK;
+}
+
+msg_t can_bridge_tx_thread(void *p)
+{
+    chRegSetThreadName("can_bridge_tx");
+    struct can_bridge_instance_t *instance = (struct can_bridge_instance_t *)p;
+
+    /* XXX Do something */
+
+    chBSemSignal(&instance->tx_finished);
+
+    return MSG_OK;
+}
+
+msg_t can_bridge_rx_thread(void *p)
+{
+    chRegSetThreadName("can_bridge_rx");
+    struct can_bridge_instance_t *instance = (struct can_bridge_instance_t *)p;
+
+    /* Do something with the conn */
+    chMtxLock(&instance->lock);
+    netconn_write(instance->conn, data, strlen(data), NETCONN_NOCOPY);
+    chMtxUnlock(&instance->lock);
+
+    /* Do not delete connection before tx thread is finished too. */
+    chBSemWait(&instance->tx_finished);
+
+    netconn_close(instance->conn);
+    netconn_delete(instance->conn);
+
     return MSG_OK;
 }
 
