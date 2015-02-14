3318e9b9386373f1d91cffd95c4314ace4bcb982
Patrick Spieler <stapelzeiger@gmail.com>
2015-02-14 16:23:23 +0100

    fix sizeof
    the buffer offset is the sizeof of the uint32_t and not uint32_t*

--- src/panic_log.c
+++ src/panic_log.c
@@ -8,8 +8,8 @@ extern char __panic_log_len;
 void panic_log_write(const char *msg)
 {
     uint32_t *crc = (uint32_t *)&__panic_log;
-    char *buffer = &__panic_log + sizeof(crc);
-    size_t len = (size_t)&__panic_log_len - sizeof(crc);
+    char *buffer = &__panic_log + sizeof(uint32_t);
+    size_t len = (size_t)&__panic_log_len - sizeof(uint32_t);
 
     strncpy(buffer, msg, len);
     *crc = crc32(0, buffer, len);
@@ -21,8 +21,8 @@ void panic_log_write(const char *msg)
 const char *panic_log_read(void)
 {
     uint32_t *crc = (uint32_t *)&__panic_log;
-    char *buffer = &__panic_log + sizeof(crc);
-    size_t len = (size_t)&__panic_log_len - sizeof(crc);
+    char *buffer = &__panic_log + sizeof(uint32_t);
+    size_t len = (size_t)&__panic_log_len - sizeof(uint32_t);
 
     if (*crc == crc32(0, buffer, len)) {
         return buffer;
