6bc93bcfbf01cb8f7a1f6b65dd5b2d458c2fd769
Florian Reinhard <florian.reinhard@epfl.ch>
2015-02-22 23:21:17 +0100

    dirty looking contiunous mode for current ADC with cirular buffer

--- src/analog.c
+++ src/analog.c
@@ -6,9 +6,15 @@
 #define ADC_TO_AMPS     0.001611328125f // 3.3/4096/(0.01*50)
 #define ADC_TO_VOLTS    0.005281575521f // 3.3/4096/(18/(100+18))
 
-static float motor_current;
+#define ADC_NB_CHANNELS 4
+#define DMA_BUFFER_SIZE (234*1)
+#define CURRENT_PRE_FILTER_WINDOW_SIZE  1
+
+static int32_t motor_current;
+static float motor_current_f;
 static float battery_voltage;
 static float aux_in;
+
 
 float analog_get_battery_voltage(void)
 {
@@ -17,7 +23,8 @@ float analog_get_battery_voltage(void)
 
 float analog_get_motor_current(void)
 {
-    return motor_current;
+    motor_current_f = motor_current_f * 0.85 + (- ((float)motor_current * 2 / DMA_BUFFER_SIZE - ADC_MAX / 2) * ADC_TO_AMPS * 0.15);
+    return motor_current_f;
 }
 
 float analog_get_auxiliary(void)
@@ -25,54 +32,47 @@ float analog_get_auxiliary(void)
     return aux_in;
 }
 
+static void adc_callback(ADCDriver *adcp, adcsample_t *adc_samples, size_t n)
+{
+    (void)adcp;
+    uint32_t accumulator = 0;
+
+    int i;
+    for (i = 0; i < (int)(n * ADC_NB_CHANNELS); i += ADC_NB_CHANNELS) {
+        accumulator += adc_samples[i + 1];
+    }
+
+    motor_current = accumulator;
+
+    battery_voltage = adc_samples[3] * ADC_TO_VOLTS;
+    aux_in = (float)(adc_samples[0] + adc_samples[2])/(ADC_MAX*2);
+}
+
 static THD_FUNCTION(adc_task, arg)
 {
     (void)arg;
     chRegSetThreadName("adc read");
-    static adcsample_t adc_samples[4];
+    static adcsample_t adc_samples[ADC_NB_CHANNELS * DMA_BUFFER_SIZE];
     static const ADCConversionGroup adcgrpcfg1 = {
-        FALSE,                  // circular?
-        4,                      // nb channels
-        NULL,                   // callback fn
+        TRUE,                   // circular
+        ADC_NB_CHANNELS,        // nb channels
+        adc_callback,           // callback fn
         NULL,                   // error callback fn
-        0,                      // CFGR
+        ADC_CFGR_CONT,          // CFGR
         0,                      // TR1
-        6,                      // CCR : DUAL=regualar,simultaneous
-        {ADC_SMPR1_SMP_AN1(0), 0},                          // SMPRx : sample time minimum
+        6,                      // CCR : DUAL=regular,simultaneous
+        {ADC_SMPR1_SMP_AN1(7), 0},                          // SMPRx : sample time maximum -> ~117kHz
         {ADC_SQR1_NUM_CH(2) | ADC_SQR1_SQ1_N(1) | ADC_SQR1_SQ2_N(1), 0, 0, 0}, // SQRx : ADC1_CH1 2x
-        {ADC_SMPR1_SMP_AN2(0) | ADC_SMPR1_SMP_AN3(0), 0},   // SSMPRx : sample time minimum
+        {ADC_SMPR1_SMP_AN2(7) | ADC_SMPR1_SMP_AN3(7), 0},   // SSMPRx : sample time maximum -> ~117kHz
         {ADC_SQR1_NUM_CH(2) | ADC_SQR1_SQ1_N(2) | ADC_SQR1_SQ2_N(3), 0, 0, 0}, // SSQRx : ADC2_CH2, ADC2_CH3
     };
 
-    filter_iir_t current_filter;
-    // scipy.signal.butter(3, 0.1)
-
-
-    const float b[] = {0.01, 0.0};
-    const float a[] = {-0.99};
-
-    float current_filter_buffer[1];
-
-    filter_iir_init(&current_filter, b, a, 1, current_filter_buffer);
-
     adcStart(&ADCD1, NULL);
 
-    while (1) {
-        adcConvert(&ADCD1, &adcgrpcfg1, adc_samples, 1);
+    motor_current_f = 0;
 
-        motor_current = filter_iir_apply(&current_filter,
-                (- (adc_samples[1] - ADC_MAX / 2) * ADC_TO_AMPS));
+    adcConvert(&ADCD1, &adcgrpcfg1, adc_samples, DMA_BUFFER_SIZE); // should never return
 
-        // motor_current = - (adc_samples[1] - ADC_MAX / 2) * ADC_TO_AMPS;
-
-        // float filter_alpha = 0.02;
-        // motor_current = motor_current * (1-filter_alpha)
-        //     + (- (adc_samples[1] - ADC_MAX / 2) * ADC_TO_AMPS) * filter_alpha;
-
-        battery_voltage = adc_samples[3] * ADC_TO_VOLTS;
-        aux_in = (float)(adc_samples[0] + adc_samples[2])/(ADC_MAX*2);
-        chThdSleepMicroseconds(100);
-    }
     return 0;
 }
 
