b6c15d48eb9af90e75f8cce5f31ffa60791b3f48
Patrick Spieler <stapelzeiger@gmail.com>
2015-03-03 22:06:45 +0100

    move pid cascade to separate module

--- src/control.c
+++ src/control.c
@@ -1,34 +1,83 @@
 #include <ch.h>
 #include <hal.h>
+#include <math.h>
 #include <pid/pid.h>
 #include "motor_pwm.h"
 #include "analog.h"
 #include "encoder.h"
 #include "parameter/parameter.h"
 #include "main.h"
+#include "pid_cascade.h"
 
 #include "control.h"
 
 #define LOW_BATT_TH 12.f // [V]
 
-static float _current_setpt;
-static float _velocity_setpt;
 
-void control_set_current(float c)
+
+
+static struct pid_cascade_s ctrl;
+
+
+
+void control_update_position_setpoint(float pos)
 {
-    _current_setpt = c;
+
 }
 
-void control_set_velocity(float v)
+void control_update_velocity_setpoint(float vel)
 {
-    _velocity_setpt = v;
+
 }
 
-static float motor_voltage;
+void control_update_trajectory_setpoint(float pos, float vel, float acc, float torque)
+{
+
+}
+
+float control_get_motor_voltage(void)
+{
+    return ctrl.motor_voltage;
+}
+
+float control_get_current(void)
+{
+    return ctrl.current;
+}
+
+float control_get_torque(void)
+{
+    return ctrl.torque;
+}
+
+float control_get_velocity(void)
+{
+    return ctrl.velocity;
+}
+
+float control_get_position(void)
+{
+    return ctrl.position;
+}
+
+float control_get_current_error(void)
+{
+    return ctrl.current_error;
+}
+
+float control_get_velocity_error(void)
+{
+    return ctrl.velocity_error;
+}
+
+float control_get_position_error(void)
+{
+    return ctrl.position_error;
+}
+
 
 static void motor_set_voltage(float u)
 {
-    motor_voltage = u;
     float u_batt = analog_get_battery_voltage();
     if (u_batt > LOW_BATT_TH) {
         motor_pwm_enable();
@@ -37,12 +86,6 @@ static void motor_set_voltage(float u)
         motor_pwm_disable();
     }
 }
-
-float control_get_motor_voltage(void)
-{
-    return motor_voltage;
-}
-
 
 
 struct pid_param_s {
@@ -80,87 +123,7 @@ static struct pid_param_s pos_pid_params;
 static struct pid_param_s vel_pid_params;
 static struct pid_param_s cur_pid_params;
 
-static THD_FUNCTION(control_loop, arg)
-{
-    (void)arg;
-    chRegSetThreadName("Control Loop");
-
-    static pid_ctrl_t current_pid;
-    static pid_ctrl_t velocity_pid;
-    static pid_ctrl_t position_pid;
-
-    pid_init(&current_pid);
-    pid_init(&velocity_pid);
-    pid_init(&position_pid);
-
-    float motor_current_constant = 1;
-
-    uint32_t control_period_us = 0;
-    while (42) {
-        // update parameters if they changed
-        if (parameter_namespace_contains_changed(&param_ns_control)) {
-            if (parameter_namespace_contains_changed(&param_ns_pos_ctrl)) {
-                pid_param_update(&pos_pid_params, &position_pid);
-            }
-            if (parameter_namespace_contains_changed(&param_ns_vel_ctrl)) {
-                pid_param_update(&vel_pid_params, &velocity_pid);
-            }
-            if (parameter_namespace_contains_changed(&param_ns_cur_ctrl)) {
-                pid_param_update(&cur_pid_params, &current_pid);
-            }
-            if (parameter_changed(&param_loop_freq)) {
-                float freq = parameter_scalar_get(&param_loop_freq);
-                control_period_us = 1000000.f/freq;
-                pid_set_frequency(&current_pid, freq);
-                pid_set_frequency(&velocity_pid, freq);
-                pid_set_frequency(&position_pid, freq);
-            }
-        }
-
-        // setpoint interpolation
-        bool pos_control_enabled = false;
-        float position_setpt = 0;
-        float velocity_setpt = 100;
-        float feedforward_torque = 0;
-
-        // feedback input
-        float position = 0;
-        float velocity = encoder_get_speed();
-        float current = analog_get_motor_current();
-
-        // position control
-        float pos_ctrl_vel;
-        if (pos_control_enabled) {
-            pos_ctrl_vel = pid_process(&position_pid, position - position_setpt);
-        } else {
-            pid_reset_integral(&position_pid);
-            pos_ctrl_vel = 0;
-        }
-
-        // velocity control
-        velocity_setpt = velocity_setpt + pos_ctrl_vel;
-        float vel_ctrl_torque = pid_process(&velocity_pid, velocity - velocity_setpt);
-
-        // torque control
-        float torque_setpt = vel_ctrl_torque + feedforward_torque;
-        float current_setpt = torque_setpt * motor_current_constant;
-        float motor_voltage = pid_process(&current_pid, current - current_setpt);
-
-        motor_set_voltage(motor_voltage);
-
-        if (analog_get_battery_voltage() < LOW_BATT_TH) {
-            pid_reset_integral(&current_pid);
-            pid_reset_integral(&velocity_pid);
-            pid_reset_integral(&position_pid);
-        }
-
-        chThdSleepMicroseconds(control_period_us);
-    }
-
-    return 0;
-}
-
-void control_start(void)
+static void control_declare_parameters(void)
 {
     parameter_namespace_declare(&param_ns_control, &parameter_root_ns, "control");
     parameter_scalar_declare_with_default(&param_loop_freq, &param_ns_control, "loop_freq", 1000);
@@ -170,6 +133,69 @@ void control_start(void)
     pid_param_declare(&pos_pid_params, &param_ns_pos_ctrl);
     pid_param_declare(&vel_pid_params, &param_ns_vel_ctrl);
     pid_param_declare(&cur_pid_params, &param_ns_cur_ctrl);
+}
+
+
+static THD_FUNCTION(control_loop, arg)
+{
+    (void)arg;
+    chRegSetThreadName("Control Loop");
+
+    pid_init(&ctrl.current_pid);
+    pid_init(&ctrl.velocity_pid);
+    pid_init(&ctrl.position_pid);
+    ctrl.motor_current_constant = 0;
+    ctrl.velocity_limit = INFINITY;
+    ctrl.torque_limit = INFINITY;
+    ctrl.current_limit = INFINITY;
+
+    uint32_t control_period_us = 0;
+    while (true) {
+        // update parameters if they changed
+        if (parameter_namespace_contains_changed(&param_ns_control)) {
+            if (parameter_namespace_contains_changed(&param_ns_pos_ctrl)) {
+                pid_param_update(&pos_pid_params, &ctrl.position_pid);
+            }
+            if (parameter_namespace_contains_changed(&param_ns_vel_ctrl)) {
+                pid_param_update(&vel_pid_params, &ctrl.velocity_pid);
+            }
+            if (parameter_namespace_contains_changed(&param_ns_cur_ctrl)) {
+                pid_param_update(&cur_pid_params, &ctrl.current_pid);
+            }
+            if (parameter_changed(&param_loop_freq)) {
+                float freq = parameter_scalar_get(&param_loop_freq);
+                control_period_us = 1000000.f/freq;
+                pid_set_frequency(&ctrl.current_pid, freq);
+                pid_set_frequency(&ctrl.velocity_pid, freq);
+                pid_set_frequency(&ctrl.position_pid, freq);
+            }
+        }
+
+        if (analog_get_battery_voltage() < LOW_BATT_TH) {
+            pid_reset_integral(&ctrl.current_pid);
+            pid_reset_integral(&ctrl.velocity_pid);
+            pid_reset_integral(&ctrl.position_pid);
+        } else {
+            ctrl.position = 0;
+            ctrl.velocity = 0;
+            ctrl.current = 0;
+            ctrl.position_control_enabled = false;
+            ctrl.position_setpt = 0;
+            ctrl.velocity_setpt = 0;
+            ctrl.feedforward_torque = 0;
+
+            pid_cascade_control(&ctrl);
+            motor_set_voltage(ctrl.motor_voltage);
+        }
+
+        chThdSleepMicroseconds(control_period_us);
+    }
+    return 0;
+}
+
+void control_start(void)
+{
+    control_declare_parameters();
 
     static THD_WORKING_AREA(control_loop_wa, 256);
     chThdCreateStatic(control_loop_wa, sizeof(control_loop_wa), HIGHPRIO, control_loop, NULL);
--- src/control.h
+++ src/control.h
@@ -8,9 +8,18 @@ extern "C" {
 
 void control_start(void);
 
-void control_set_current(float c);
-void control_set_velocity(float v);
+void control_update_position_setpoint(float pos);
+void control_update_velocity_setpoint(float vel);
+void control_update_trajectory_setpoint(float pos, float vel, float acc, float torque);
+
 float control_get_motor_voltage(void);
+float control_get_current(void);
+float control_get_torque(void);
+float control_get_velocity(void);
+float control_get_position(void);
+float control_get_current_error(void);
+float control_get_velocity_error(void);
+float control_get_position_error(void);
 
 #ifdef __cplusplus
 }
