a26347df67da8d843a8d45cf569f864cb10db66a
Antoine Albertelli <antoinea101@gmail.com>
2015-03-06 16:58:17 +0100

    Add a buffer writer callback, since it is pretty useful

---
+++ tests/serial_datagram_buffer_writer_tests.cpp
@@ -0,0 +1,48 @@
+#include "CppUTest/TestHarness.h"
+#include "../serial_datagram.h"
+#include "../serial_datagram_buffer_writer.h"
+
+TEST_GROUP(SerialDatagramBuffferWriterTestCase)
+{
+    uint8_t buffer[20];
+    serial_datagram_buffer_writer_t writer;
+
+    char *data = (char *)"hello";
+
+    void setup(void)
+    {
+        serial_datagram_buffer_writer_init(&writer, buffer, sizeof buffer);
+    }
+};
+
+TEST(SerialDatagramBuffferWriterTestCase, CanInit)
+{
+    POINTERS_EQUAL(buffer, writer.buffer);
+    CHECK_EQUAL(sizeof buffer, writer.buffer_size);
+    CHECK_EQUAL(0, writer.write_index);
+}
+
+TEST(SerialDatagramBuffferWriterTestCase, CanWrite)
+{
+    serial_datagram_buffer_writer_cb((void *)&writer, (void *)data, 5);
+    serial_datagram_buffer_writer_cb((void *)&writer, (void *)data, 5);
+
+    STRCMP_EQUAL("hellohello", (char *)buffer);
+}
+
+TEST(SerialDatagramBuffferWriterTestCase, DoesntOverflow)
+{
+    uint8_t smallbuf[3];
+
+    memset(smallbuf, 0x55, sizeof smallbuf);
+
+    serial_datagram_buffer_writer_init(&writer, smallbuf, sizeof smallbuf - 1);
+
+    serial_datagram_buffer_writer_cb((void *)&writer, (void *)data, 5);
+
+    BYTES_EQUAL('h', smallbuf[0]);
+    BYTES_EQUAL('e', smallbuf[1]);
+    BYTES_EQUAL(0x55, smallbuf[2]);
+}
+
+
