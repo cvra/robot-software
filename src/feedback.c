f703955efb47811fd4a44005717dc79727fc8fd0
Florian Reinhard <florian.reinhard@epfl.ch>
2015-03-11 20:31:37 +0100

    feedback RPM description

--- src/feedback.c
+++ src/feedback.c
@@ -4,6 +4,18 @@
 void feedback_compute(struct feedback_s *feedback)
 {
     switch (feedback->input_selection) {
+        case FEEDBACK_RPM :
+            /* Call other module that updates the period on each interrupt
+             * (light barrier crossing) and integrates the position assuming
+             * constant velocity.
+             * The period is assumed being constant as long as the time since
+             * the last interrupt is less than the previous period.
+             * When more time than the previous period has passed (i.e. the
+             * actuator is decelerating), the speed is updated to the _maximal_
+             * possible speed (1 / [time since last interrupt]) and the
+             * position stops moving (SBB clock style).
+             */
+            break;
         case FEEDBACK_POTENTIOMETER : {
             float position =
                 feedback->potentiometer.gain * feedback->input.potentiometer
