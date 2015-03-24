3400ade748b02f0009b3dc5a6384ce251df4e5ad
Antoine Albertelli <antoinea101@gmail.com>
2015-03-24 13:39:01 +0100

    PEP8 fixes

--- python/test/test_messages.py
+++ python/test/test_messages.py
@@ -2,13 +2,12 @@ from cvra_rpc import message
 import msgpack
 import threading
 import socketserver
-import socket
 
 import unittest
 try:
-    from unittest.mock import *
+    from unittest.mock import Mock
 except ImportError:
-    from mock import *
+    from mock import Mock
 
 
 def msgpack_decode(data):
@@ -19,6 +18,7 @@ def msgpack_decode(data):
     u = msgpack.Unpacker(encoding='ascii')
     u.feed(data)
     return next(u)
+
 
 class MessageEncodingTestCase(unittest.TestCase):
     def test_encoding_message_name(self):
@@ -35,6 +35,7 @@ class MessageEncodingTestCase(unittest.TestCase):
         data = message.encode("foo", [1, 2, 3])
         self.assertEqual(msgpack_decode(data), ['foo', 1, 2, 3])
 
+
 class MessageDecodingTestCase(unittest.TestCase):
     def test_decoding_message(self):
         data = message.encode("foo", [1, 2, 3])
@@ -43,21 +44,22 @@ class MessageDecodingTestCase(unittest.TestCase):
         self.assertEqual('foo', name)
         self.assertEqual([1, 2, 3], args)
 
+
 class MessageRequestHandlerTestCase(unittest.TestCase):
     def test_factory(self):
-        callbacks = {'foo':Mock()}
+        callbacks = {'foo': Mock()}
         RequestHandler = message.create_request_handler(callbacks)
         self.assertEqual(callbacks, RequestHandler.callbacks)
 
     def test_correct_callback_called(self):
-        callbacks = {'foo':Mock(), 'bar':Mock()}
+        callbacks = {'foo': Mock(), 'bar': Mock()}
 
         data = message.encode('bar')
         message.handle_message(data, callbacks)
         callbacks['bar'].assert_any_call([])
 
     def test_args_forwarded(self):
-        callbacks = {'foo':Mock()}
+        callbacks = {'foo': Mock()}
         data = message.encode('foo', [1, 2, 3])
 
         message.handle_message(data, callbacks)
@@ -73,21 +75,21 @@ class MessageRequestHandlerTestCase(unittest.TestCase):
         TARGET = ('localhost', 9999)
         callbacks = {'foo': Mock(), 'bar': Mock()}
 
-        # Creates the server
+        # Creates the server
         RequestHandler = message.create_request_handler(callbacks)
         server = socketserver.UDPServer(TARGET, RequestHandler)
 
-        # Starts the server in another thread
+        # Starts the server in another thread
         server_thread = threading.Thread(target=server.serve_forever)
         server_thread.start()
 
-        # Sends the messages
+        # Sends the messages
         message.send(TARGET, 'foo', [1, 2, 3])
         message.send(TARGET, 'bar')
 
-        # Terminates the server thread
+        # Terminates the server thread
         server.shutdown()
 
-        # Checks that the callbacks were called
+        # Checks that the callbacks were called
         callbacks['foo'].assert_any_call([1, 2, 3])
         callbacks['bar'].assert_any_call([])
