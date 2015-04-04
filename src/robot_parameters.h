d9c5c0311a118a5fb80bc004ad2458e4fa6a9ac3
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2015-04-04 18:59:40 +0200

    Add robot base parameters in a dedicated parameter header file

---
+++ src/robot_parameters.h
@@ -0,0 +1,16 @@
+#ifndef ROBOT_PARAMETERS_H
+#define ROBOT_PARAMETERS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define RIGHT_WHEEL_RADIUS0 0.156f
+#define LEFT_WHEEL_RADIUS	0.156f
+#define WHEELBASE 			0.030f
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ROBOT_PARAMETERS_H */
