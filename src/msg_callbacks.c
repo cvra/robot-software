fa5c59f70fc2cb148d8850f743f0ebed6bf88ec6
Antoine Albertelli <antoinea101@gmail.com>
2015-04-05 18:07:34 +0200

    Add SimpleRPC command to change motor speed

--- src/msg_callbacks.c
+++ src/msg_callbacks.c
@@ -1,6 +1,8 @@
 #include "msg_callbacks.h"
 #include <hal.h>
 
+#include "motor_control.h"
+
 void message_cb(int argc, cmp_ctx_t *input)
 {
     (void) argc;
@@ -14,8 +16,21 @@ void message_cb(int argc, cmp_ctx_t *input)
     }
 }
 
+void message_fwd_callback(int argc, cmp_ctx_t *input)
+{
+    int res;
+    if (argc != 1) {
+        return;
+    }
+
+    cmp_read_int(input, &res);
+    m1_vel_setpt = res;
+    m2_vel_setpt = -res;
+}
+
 message_method_t message_callbacks[] = {
-    {.name = "test", .cb = message_cb}
+    {.name = "test", .cb = message_cb},
+    {.name = "fwd", .cb = message_fwd_callback},
 };
 
 int message_callbacks_len = sizeof message_callbacks / sizeof(message_callbacks[0]);
