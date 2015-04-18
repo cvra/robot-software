2614a0fffb67c6cb389bf1fb2cb786b9bb7dace8
Florian Reinhard <florian.reinhard@epfl.ch>
2015-04-18 18:59:39 +0200

    change ADC sampling time

--- src/analog.c
+++ src/analog.c
@@ -7,29 +7,27 @@
 #define ADC_TO_VOLTS    0.005281575521f // 3.3/4096/(18/(100+18))
 
 #define ADC_NB_CHANNELS 4
-#define DMA_BUFFER_SIZE (234*1)
+#define DMA_BUFFER_SIZE (128*2)         // expected DMA-callback with 3800Hz
 #define CURRENT_PRE_FILTER_WINDOW_SIZE  1
 
 static int32_t motor_current;
-static float motor_current_f;
-static float battery_voltage;
-static float aux_in;
+static int32_t battery_voltage;
+static int32_t aux_in;
 
 
 float analog_get_battery_voltage(void)
 {
-    return battery_voltage;
+    return (float)battery_voltage * ADC_TO_VOLTS;
 }
 
 float analog_get_motor_current(void)
 {
-    motor_current_f = motor_current_f * 0.85 + (- ((float)motor_current * 2 / DMA_BUFFER_SIZE - ADC_MAX / 2) * ADC_TO_AMPS * 0.15);
-    return motor_current_f;
+    return -((float)motor_current * 2 / DMA_BUFFER_SIZE - ADC_MAX / 2) * ADC_TO_AMPS;
 }
 
 float analog_get_auxiliary(void)
 {
-    return aux_in;
+    return (float)aux_in / (ADC_MAX * 2);
 }
 
 static void adc_callback(ADCDriver *adcp, adcsample_t *adc_samples, size_t n)
@@ -44,8 +42,8 @@ static void adc_callback(ADCDriver *adcp, adcsample_t *adc_samples, size_t n)
 
     motor_current = accumulator;
 
-    battery_voltage = adc_samples[3] * ADC_TO_VOLTS;
-    aux_in = (float)(adc_samples[0] + adc_samples[2])/(ADC_MAX*2);
+    battery_voltage = adc_samples[3];
+    aux_in = adc_samples[0] + adc_samples[2];
 }
 
 static THD_FUNCTION(adc_task, arg)
@@ -61,15 +59,16 @@ static THD_FUNCTION(adc_task, arg)
         ADC_CFGR_CONT,          // CFGR
         0,                      // TR1
         6,                      // CCR : DUAL=regular,simultaneous
-        {ADC_SMPR1_SMP_AN1(7), 0},                          // SMPRx : sample time maximum -> ~117kHz
+        {ADC_SMPR1_SMP_AN1(5), 0},                          // SMPRx : sample time -> ~973kHz
         {ADC_SQR1_NUM_CH(2) | ADC_SQR1_SQ1_N(1) | ADC_SQR1_SQ2_N(1), 0, 0, 0}, // SQRx : ADC1_CH1 2x
-        {ADC_SMPR1_SMP_AN2(7) | ADC_SMPR1_SMP_AN3(7), 0},   // SSMPRx : sample time maximum -> ~117kHz
+        {ADC_SMPR1_SMP_AN2(5) | ADC_SMPR1_SMP_AN3(5), 0},   // SSMPRx : sample time maximum -> ~973kHz
         {ADC_SQR1_NUM_CH(2) | ADC_SQR1_SQ1_N(2) | ADC_SQR1_SQ2_N(3), 0, 0, 0}, // SSQRx : ADC2_CH2, ADC2_CH3
+        // Expected sampling frequence is 973kHz / 2 = 486kHz
     };
 
     adcStart(&ADCD1, NULL);
 
-    motor_current_f = 0;
+    motor_current = 0;
 
     adcConvert(&ADCD1, &adcgrpcfg1, adc_samples, DMA_BUFFER_SIZE); // should never return
 
