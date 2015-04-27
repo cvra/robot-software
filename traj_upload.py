c218ae2af28820d0850cabf21805b3a21977404c
Antoine Albertelli <antoinea101@gmail.com>
2015-04-27 17:37:00 +0200

    Force all points to be floats

--- traj_upload.py
+++ traj_upload.py
@@ -11,11 +11,11 @@ def prepare_for_sending(traj):
     s = [int(a.timestamp) for a in traj]
     us = [int((a.timestamp - int(a.timestamp))*1e6) for a in traj]
 
-    x = list(zip(s, us, (a.x for a in traj)))
-    y = list(zip(s, us, (a.y for a in traj)))
-    theta = list(zip(s, us, (a.theta for a in traj)))
-    speed = list(zip(s, us, (a.speed for a in traj)))
-    omega = list(zip(s, us, (a.omega for a in traj)))
+    x = list(zip(s, us, (float(a.x) for a in traj)))
+    y = list(zip(s, us, (float(a.y) for a in traj)))
+    theta = list(zip(s, us, (float(a.theta) for a in traj)))
+    speed = list(zip(s, us, (float(a.speed) for a in traj)))
+    omega = list(zip(s, us, (float(a.omega) for a in traj)))
 
     return [x, y, theta, speed, omega]
 
--- traj_upload_test.py
+++ traj_upload_test.py
@@ -9,21 +9,21 @@ class TrajectoryTestCase(unittest.TestCase):
                         timestamp=1.4)
 
     def test_can_prepare_trajectory(self):
-        traj = [TrajectoryPoint(x='x', y='y', theta='t', speed='s',
-                                omega='o', timestamp=float(i))
+        traj = [TrajectoryPoint(x=1., y=2., theta=3., speed=4.,
+                                omega=5., timestamp=float(i))
                 for i in range(10)]
 
         traj_transformed = prepare_for_sending(traj)
 
-        self.assertEqual(traj_transformed[0], [(i, 0, 'x') for i in range(10)])
-        self.assertEqual(traj_transformed[1], [(i, 0, 'y') for i in range(10)])
-        self.assertEqual(traj_transformed[2], [(i, 0, 't') for i in range(10)])
-        self.assertEqual(traj_transformed[3], [(i, 0, 's') for i in range(10)])
-        self.assertEqual(traj_transformed[4], [(i, 0, 'o') for i in range(10)])
+        self.assertEqual(traj_transformed[0], [(i, 0, 1.) for i in range(10)])
+        self.assertEqual(traj_transformed[1], [(i, 0, 2.) for i in range(10)])
+        self.assertEqual(traj_transformed[2], [(i, 0, 3.) for i in range(10)])
+        self.assertEqual(traj_transformed[3], [(i, 0, 4.) for i in range(10)])
+        self.assertEqual(traj_transformed[4], [(i, 0, 5.) for i in range(10)])
 
     def test_can_send(self):
-        traj = [TrajectoryPoint(x='x', y='y', theta='t', speed='s',
-                                omega='o', timestamp=float(i))
+        traj = [TrajectoryPoint(x=1., y=2., theta=3., speed=4.,
+                                omega=5., timestamp=float(i))
                 for i in range(10)]
 
         preprocessed_traj = prepare_for_sending(traj)
