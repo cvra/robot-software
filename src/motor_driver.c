3bdc29807a92894a9ac7cad5474988d63d26e69c
Patrick Spieler <stapelzeiger@gmail.com>
2015-05-05 23:23:35 +0200

    setters

--- src/motor_driver.c
+++ src/motor_driver.c
@@ -1,14 +1,106 @@
+#include <ch.h>
 #include <string.h>
 #include "motor_driver.h"
 
 
 void motor_driver_init(motor_driver_t *d,
                        const char *actuator_id,
-                       parameter_namespace_t *ns)
+                       parameter_namespace_t *ns,
+                       memory_pool_t *traj_buffer_pool)
 {
+    chBSemObjectInit(&d->lock, false);
+    d->traj_buffer_pool = traj_buffer_pool;
     strncpy(d->id, actuator_id, MOTOR_ID_MAX_LEN);
     d->id[MOTOR_ID_MAX_LEN] = '\0';
     d->can_id = CAN_ID_NOT_SET;
     d->control_mode = MOTOR_CONTROL_MODE_DISABLED;
+    parameter_namespace_declare(&d->motor_ns, ns, d->id);
 }
 
+const char *motor_driver_get_id(motor_driver_t *d)
+{
+    return d->id;
+}
+
+// must be called with the locked driver
+static void free_trajectory_buffer(motor_driver_t *d)
+{
+    if (d->control_mode == MOTOR_CONTROL_MODE_TRAJECTORY
+        && d->setpt.trajectory != NULL) {
+        chPoolFree(d->traj_buffer_pool, d->setpt.trajectory);
+        d->setpt.trajectory = NULL;
+    }
+}
+
+void motor_driver_set_position(motor_driver_t *d, float position)
+{
+    chBSemWait(&d->lock);
+    free_trajectory_buffer(d);
+    d->control_mode = MOTOR_CONTROL_MODE_POSITION;
+    d->setpt.postition = position;
+    chBSemSignal(&d->lock);
+}
+
+void motor_driver_set_velocity(motor_driver_t *d, float velocity)
+{
+    chBSemWait(&d->lock);
+    free_trajectory_buffer(d);
+    d->control_mode = MOTOR_CONTROL_MODE_VELOCITY;
+    d->setpt.velocity = velocity;
+    chBSemSignal(&d->lock);
+}
+
+void motor_driver_set_torque(motor_driver_t *d, float torque)
+{
+    chBSemWait(&d->lock);
+    free_trajectory_buffer(d);
+    d->control_mode = MOTOR_CONTROL_MODE_TORQUE;
+    d->setpt.torque = torque;
+    chBSemSignal(&d->lock);
+}
+
+void motor_driver_set_voltage(motor_driver_t *d, float voltage)
+{
+    chBSemWait(&d->lock);
+    free_trajectory_buffer(d);
+    d->control_mode = MOTOR_CONTROL_MODE_VOLTAGE;
+    d->setpt.voltage = voltage;
+    chBSemSignal(&d->lock);
+}
+
+void motor_driver_update_trajectory(motor_driver_t *d, trajectory_t *traj)
+{
+    chBSemWait(&d->lock);
+    if (d->control_mode != MOTOR_CONTROL_MODE_TRAJECTORY) {
+        d->setpt.trajectory = chPoolAlloc(d->traj_buffer_pool);
+        d->control_mode = MOTOR_CONTROL_MODE_TRAJECTORY;
+    }
+    if (d->setpt.trajectory != NULL) {
+        // todo trajecory merge ...
+    } else {
+        chSysHalt("motor driver out of memory (trajectory buffer allocation)");
+    }
+    chBSemSignal(&d->lock);
+}
+
+
+void motor_driver_set_can_id(motor_driver_t *d, int can_id)
+{
+    d->can_id = can_id;
+}
+
+int motor_driver_get_can_id(motor_driver_t *d)
+{
+    return d->can_id;
+}
+
+
+void motor_driver_lock(motor_driver_t *d)
+{
+    chBSemWait(&d->lock);
+}
+
+void motor_driver_unlock(motor_driver_t *d)
+{
+    chBSemSignal(&d->lock);
+}
--- src/motor_driver.h
+++ src/motor_driver.h
@@ -1,7 +1,11 @@
 #ifndef MOTOR_DRIVER_H
 #define MOTOR_DRIVER_H
 
+#include <ch.h>
 #include <parameter/parameter.h>
+#include "unix_timestamp.h"
+typedef void trajectory_buffer_t;// todo
+typedef void trajectory_t;// todo
 
 #define MOTOR_ID_MAX_LEN 24
 
@@ -15,8 +19,21 @@
 typedef struct {
     char id[MOTOR_ID_MAX_LEN+1];
     int can_id;
-    int control_mode;
+    binary_semaphore_t lock;
+    memory_pool_t *traj_buffer_pool;
 
+    int control_mode;
+    union {
+        float postition;
+        float velocity;
+        float torque;
+        float voltage;
+        trajectory_buffer_t *trajectory;
+    } setpt;
+
+
+
+    parameter_namespace_t motor_ns;
 } motor_driver_t;
 
 
@@ -26,14 +43,17 @@ extern "C" {
 
 void motor_driver_init(motor_driver_t *d,
                        const char *actuator_id,
-                       parameter_namespace_t *ns);
+                       parameter_namespace_t *ns,
+                       memory_pool_t *traj_buffer_pool);
 
 // returns a pointer to the stored id
 const char *motor_driver_get_id(motor_driver_t *d);
 
-void motor_driver_set_velocity(motor_driver_t *d, float velocity);
 void motor_driver_set_position(motor_driver_t *d, float position);
-void motor_driver_update_trajectory(motor_driver_t *d, trajectory_t traj);
+void motor_driver_set_velocity(motor_driver_t *d, float velocity);
+void motor_driver_set_torque(motor_driver_t *d, float torque);
+void motor_driver_set_voltage(motor_driver_t *d, float voltage);
+void motor_driver_update_trajectory(motor_driver_t *d, trajectory_t *traj);
 
 void motor_driver_set_can_id(motor_driver_t *d, int can_id);
 
@@ -47,22 +67,23 @@ motor_driver_t* motor_driver_list_get_next(motor_driver_t *d);
 motor_driver_t* motor_driver_list_add(motor_driver_t *head, motor_driver_t *new_driver);
 
 
-enum control_mode motor_driver_get_control_mode(motor_driver_t *d);
+void motor_driver_lock(motor_driver_t *d);
+void motor_driver_unlock(motor_driver_t *d);
 
-float motor_driver_get_velocity(motor_driver_t *d);
-float motor_driver_get_position(motor_driver_t *d);
+// the get control mode and the corresponding setpoint must be called in a
+// motor_driver_lock/unlock critical section
+int motor_driver_get_control_mode(motor_driver_t *d);
+
+float motor_driver_get_position_setpt(motor_driver_t *d);
+float motor_driver_get_velocity_setpt(motor_driver_t *d);
+float motor_driver_get_torque_setpt(motor_driver_t *d);
+float motor_driver_get_voltage_setpt(motor_driver_t *d);
 void motor_driver_get_trajectory_point(motor_driver_t *d,
                                        unix_timestamp_t timestamp,
                                        float *position,
                                        float *velocity,
                                        float *acceleration,
                                        float *torque);
-
-// sets trajectory buffer allocated by motor manager (ChibiOS block allocater)
-void motor_driver_set_traj_buffer(motor_driver_t *d, trajectory_buffer_t *traj_buffer);
-// gets the trajectory buffer to free it (or check for its existance)
-trajectory_buffer_t *motor_driver_get_traj_buffer(motor_driver_t *d);
-
 
 /*
  * - Drivers are a linked list allocated by the motor_manager
