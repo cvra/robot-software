63b665d9f5618054dc890fe1b81537b61b4e5581
Michael Spieler <the.chaotic.coder@gmail.com>
2015-05-18 17:59:18 +0200

    Move STM32 timer settings to separate header file.

--- timestamp_stm32.c
+++ timestamp_stm32.c
@@ -10,17 +10,7 @@ static volatile uint32_t time_us_high;
 #include "timestamp.h"
 
 // settings
-#define TIMESTAMP_TIMER TIM7
-#define TIMER_REG       STM32_TIM7
-#define TIMER_IRQ_NAME  STM32_TIM7_HANDLER
-#define RCC_EN()        rccEnableTIM7(FALSE)
-#define RCC_RESET()     rccResetTIM7()
-#define NVIC_NB         STM32_TIM7_NUMBER
-
-#define COUNTER_MAX     0xffff
-#define PRESCALER       (STM32_PCLK1/1000000)   // CK_CNT = CK_INT / (PSC[15:0] + 1)
-#define INTERRUPT_PRIO  5
-
+#include "timestamp_stm32_settings.h"
 
 static inline uint32_t timer_read(void)
 {
