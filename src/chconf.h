6e75b9079df54c7f648f880db4524347703b95d4
Patrick Spieler <stapelzeiger@gmail.com>
2015-05-20 01:38:56 +0200

    enable CH_DBG_FILL_THREADS

--- src/chconf.h
+++ src/chconf.h
@@ -382,7 +382,7 @@
  *
  * @note    The default is @p FALSE.
  */
-#define CH_DBG_FILL_THREADS                 FALSE
+#define CH_DBG_FILL_THREADS                 TRUE
 
 /**
  * @brief   Debug option, threads profiling.
