099836fd80073245d0fafbf129a76bb29fd93074
Patrick Spieler <stapelzeiger@gmail.com>
2015-05-20 21:45:49 +0200

    increase control timeout

--- src/control.c
+++ src/control.c
@@ -17,7 +17,7 @@
 #include "control.h"
 
 #define LOW_BATT_TH 12.f // [V]
-#define DEFAULT_CTRL_TIMEOUT 0.1f // [s]
+#define DEFAULT_CTRL_TIMEOUT 0.3f // [s]
 
 struct pid_param_s {
     parameter_t kp;
