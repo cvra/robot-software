5a7a7ca1c7159a1c783dee0512a1be02ccaa7ace
Antoine Albertelli <antoinea101@gmail.com>
2015-05-21 04:03:45 +0200

    Fix heap size script

--- python/heap_size.py
+++ python/heap_size.py
@@ -4,9 +4,14 @@ import sys
 
 RAMSIZE = 111000
 
-command = "arm-none-eabi-size {}".format(sys.argv[1]).split()
-result = subprocess.check_output(command).splitlines()[1]
-result = result.split()
-data, bss = int(result[1]), int(result[2])
+command = "arm-none-eabi-nm --numeric-sort --print-size {}".format(sys.argv[1]).split()
+result = subprocess.check_output(command).decode().splitlines()
 
-print("Heap space left: {} bytes".format(RAMSIZE - (data + bss)))
+heap_base = [s for s in result if "__heap_base__" in s][-1]
+heap_end = [s for s in result if "__heap_end__" in s][-1]
+
+heap_base = int(heap_base.split()[0], 16)
+heap_end = int(heap_end.split()[0], 16)
+
+
+print("Heap space left: {} bytes".format(heap_end - heap_base))
