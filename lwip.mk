6cba6216de74151b4a6f71988c12ce6f18d9aa3d
Antoine Albertelli <antoinea101@gmail.com>
2015-05-23 17:45:44 +0200

    Move lwip bindings to src/

--- lwip.mk
+++ lwip.mk
@@ -6,22 +6,11 @@ LWNETIFSRC = \
 
 LWCORESRC = $(wildcard $(LWIP)/src/core/*.c) $(wildcard $(LWIP)/src/core/ipv4/*.c)
 
-LWAPISRC = \
-        ${LWIP}/src/api/api_lib.c \
-        ${LWIP}/src/api/api_msg.c \
-        ${LWIP}/src/api/err.c \
-        ${LWIP}/src/api/netbuf.c \
-        ${LWIP}/src/api/netdb.c \
-        ${LWIP}/src/api/netifapi.c \
-        ${LWIP}/src/api/sockets.c \
-        ${LWIP}/src/api/tcpip.c
+LWAPISRC = $(wildcard $(LWIP)/src/api/*.c)
 
-LWIP_BINDINGS_SRC = lwip_bindings/arch/sys_arch.c lwip_bindings/lwipthread.c
+LWIP_BINDINGS_SRC = src/lwip_bindings/arch/sys_arch.c src/lwip_bindings/lwipthread.c
 
 LWSRC = $(LWNETIFSRC) $(LWCORESRC) $(LWAPISRC) $(LWIP_BINDINGS_SRC)
 
-LWINC = \
-        lwip_bindings \
-        ${LWIP}/src/include \
-        ${LWIP}/src/include/ipv4
+LWINC = src/lwip_bindings ${LWIP}/src/include ${LWIP}/src/include/ipv4
 
