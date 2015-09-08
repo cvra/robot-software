eb7d2db2e52a16fe0cb9805bde6136c6111fcd5b
Michael Spieler <the.chaotic.coder@gmail.com>
2015-09-08 15:03:34 +0200

    Fix panic_log bug, use safer strnlen().

--- src/panic_log.c
+++ src/panic_log.c
@@ -3,6 +3,7 @@
 #include <crc/crc32.h>
 
 __attribute__ ((section(".noinit")))
+__attribute__ ((aligned(4)))
 char panic_log[1024];
 const size_t panic_log_len = sizeof(panic_log);
 
@@ -10,14 +11,15 @@ void panic_log_write(const char *msg)
 {
     uint32_t *crc = (uint32_t *)&panic_log[0];
     char *buffer = &panic_log[0] + sizeof(uint32_t);
-    size_t len = (size_t)&panic_log_len - sizeof(uint32_t);
+    size_t len = sizeof(panic_log) - sizeof(uint32_t);
 
     strncpy(buffer, msg, len);
     // Terminates the buffer
-    if (strlen(msg) >= len) {
+    size_t msg_len = strnlen(msg, len);
+    if (msg_len >= len) {
         buffer[len - 1] = '\0';
     } else {
-        buffer[strlen(msg)] = '\0';
+        buffer[msg_len] = '\0';
     }
 
     *crc = crc32(0, buffer, len);
@@ -27,7 +29,7 @@ const char *panic_log_read(void)
 {
     uint32_t *crc = (uint32_t *)&panic_log[0];
     char *buffer = &panic_log[0] + sizeof(uint32_t);
-    size_t len = (size_t)&panic_log_len - sizeof(uint32_t);
+    size_t len = sizeof(panic_log) - sizeof(uint32_t);
 
     if (*crc == crc32(0, buffer, len)) {
         return buffer;
