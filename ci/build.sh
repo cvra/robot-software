cf88a2962e3c452ab15ebb8af9873e578699541b
Antoine Albertelli <antoinea101@gmail.com>
2016-01-28 09:59:10 +0100

    Enable Travis CI using scripts from cvra/motor-control-firmware

---
+++ ci/build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Make the script fail if any command in it fail
+set -e
+
+if [ "$BUILD_TYPE" == "" ]
+then
+    echo "Please define \$BUILD_TYPE!"
+    exit 1
+fi
+
+source env/bin/activate
+export PATH=gcc-arm-none-eabi-4_9-2014q4/bin/:$PATH
+
+export CFLAGS="$CFLAGS -I $HOME/cpputest/include/"
+export CXXFLAGS="$CXXFLAGS -I $HOME/cpputest/include/"
+export LDFLAGS="$CXXFLAGS -L $HOME/cpputest/lib/"
+
+packager/packager.py
+
+case $BUILD_TYPE in
+    tests)
+        mkdir build
+        cd build
+        cmake ..
+        make check
+        ;;
+
+    build)
+        make dsdlc
+        make
+        ;;
+    *)
+        echo "Unknown build type $BUILD_TYPE"
+        exit 1
+        ;;
+esac
