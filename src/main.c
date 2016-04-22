e371071795c9b7b6697cf45b407bf51e0c0d74ae
Michael Spieler <michael.spieler@epfl.ch>
2016-04-22 19:40:37 +0200

    board and config files

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <ch.h>
+#include <hal.h>
+
+int main(void)
+{
+    halInit();
+    chSysInit();
+
+    while (1) {
+        palSetPad(GPIOA, GPIOA_LED);
+        chThdSleepMilliseconds(500);
+        palClearPad(GPIOA, GPIOA_LED);
+        chThdSleepMilliseconds(500);
+    }
+}
