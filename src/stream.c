15f07287b3a9ac61494ec0abed2ea4ff27c60e12
Florian Reinhard <florian.reinhard@epfl.ch>
2016-05-21 17:06:34 +0200

    change feedback stream topic names

--- src/stream.c
+++ src/stream.c
@@ -14,12 +14,14 @@
 #include "priorities.h"
 
 #define STREAM_STACKSIZE 1024
+#define TOPIC_NAME_LEN   40
 
 THD_WORKING_AREA(wa_stream, STREAM_STACKSIZE);
 
 static void stream_thread(void *p)
 {
     static uint8_t buffer[64];
+    static char topic_name[TOPIC_NAME_LEN];
     cmp_ctx_t ctx;
     cmp_mem_access_t mem;
     ip_addr_t server;
@@ -39,90 +41,100 @@ static void stream_thread(void *p)
             if (motor_driver_get_stream_change_status(&drv_list[i]) != 0) {
                 if (motor_driver_get_stream_change_status(&drv_list[i])
                     & (1 << MOTOR_STREAM_CURRENT_SETPT)) {
-                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), "current_setp");
-                    cmp_write_map(&ctx, 1);
-                    cmp_write_str(&ctx, motor_driver_get_id(&drv_list[i]), strlen(motor_driver_get_id(&drv_list[i])));
+                    strncpy(topic_name, "actuator/", TOPIC_NAME_LEN);
+                    strncat(topic_name, motor_driver_get_id(&drv_list[i]), TOPIC_NAME_LEN);
+                    strncat(topic_name, "/current_setp", TOPIC_NAME_LEN);
+                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), topic_name);
                     cmp_write_float(&ctx, motor_driver_get_and_clear_stream_value(&drv_list[i], MOTOR_STREAM_CURRENT_SETPT));
                     message_transmit(buffer, cmp_mem_access_get_pos(&mem), &server, STREAM_PORT);
                 }
 
                 if (motor_driver_get_stream_change_status(&drv_list[i])
                     & (1 << MOTOR_STREAM_CURRENT)) {
-                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), "current");
-                    cmp_write_map(&ctx, 1);
-                    cmp_write_str(&ctx, motor_driver_get_id(&drv_list[i]), strlen(motor_driver_get_id(&drv_list[i])));
+                    strncpy(topic_name, "actuator/", TOPIC_NAME_LEN);
+                    strncat(topic_name, motor_driver_get_id(&drv_list[i]), TOPIC_NAME_LEN);
+                    strncat(topic_name, "/current", TOPIC_NAME_LEN);
+                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), topic_name);
                     cmp_write_float(&ctx, motor_driver_get_and_clear_stream_value(&drv_list[i], MOTOR_STREAM_CURRENT));
                     message_transmit(buffer, cmp_mem_access_get_pos(&mem), &server, STREAM_PORT);
                 }
 
                 if (motor_driver_get_stream_change_status(&drv_list[i])
                     & (1 << MOTOR_STREAM_MOTOR_VOLTAGE)) {
-                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), "voltage");
-                    cmp_write_map(&ctx, 1);
-                    cmp_write_str(&ctx, motor_driver_get_id(&drv_list[i]), strlen(motor_driver_get_id(&drv_list[i])));
+                    strncpy(topic_name, "actuator/", TOPIC_NAME_LEN);
+                    strncat(topic_name, motor_driver_get_id(&drv_list[i]), TOPIC_NAME_LEN);
+                    strncat(topic_name, "/voltage", TOPIC_NAME_LEN);
+                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), topic_name);
                     cmp_write_float(&ctx, motor_driver_get_and_clear_stream_value(&drv_list[i], MOTOR_STREAM_MOTOR_VOLTAGE));
                     message_transmit(buffer, cmp_mem_access_get_pos(&mem), &server, STREAM_PORT);
                 }
 
                 if (motor_driver_get_stream_change_status(&drv_list[i])
                     & (1 << MOTOR_STREAM_VELOCITY_SETPT)) {
-                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), "velocity_setp");
-                    cmp_write_map(&ctx, 1);
-                    cmp_write_str(&ctx, motor_driver_get_id(&drv_list[i]), strlen(motor_driver_get_id(&drv_list[i])));
+                    strncpy(topic_name, "actuator/", TOPIC_NAME_LEN);
+                    strncat(topic_name, motor_driver_get_id(&drv_list[i]), TOPIC_NAME_LEN);
+                    strncat(topic_name, "/velocity_setp", TOPIC_NAME_LEN);
+                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), topic_name);
                     cmp_write_float(&ctx, motor_driver_get_and_clear_stream_value(&drv_list[i], MOTOR_STREAM_VELOCITY_SETPT));
                     message_transmit(buffer, cmp_mem_access_get_pos(&mem), &server, STREAM_PORT);
                 }
 
                 if (motor_driver_get_stream_change_status(&drv_list[i])
                     & (1 << MOTOR_STREAM_VELOCITY)) {
-                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), "velocity");
-                    cmp_write_map(&ctx, 1);
-                    cmp_write_str(&ctx, motor_driver_get_id(&drv_list[i]), strlen(motor_driver_get_id(&drv_list[i])));
+                    strncpy(topic_name, "actuator/", TOPIC_NAME_LEN);
+                    strncat(topic_name, motor_driver_get_id(&drv_list[i]), TOPIC_NAME_LEN);
+                    strncat(topic_name, "/velocity", TOPIC_NAME_LEN);
+                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), topic_name);
                     cmp_write_float(&ctx, motor_driver_get_and_clear_stream_value(&drv_list[i], MOTOR_STREAM_VELOCITY));
                     message_transmit(buffer, cmp_mem_access_get_pos(&mem), &server, STREAM_PORT);
                 }
 
                 if (motor_driver_get_stream_change_status(&drv_list[i])
                     & (1 << MOTOR_STREAM_POSITION_SETPT)) {
-                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), "position_setp");
-                    cmp_write_map(&ctx, 1);
-                    cmp_write_str(&ctx, motor_driver_get_id(&drv_list[i]), strlen(motor_driver_get_id(&drv_list[i])));
+                    strncpy(topic_name, "actuator/", TOPIC_NAME_LEN);
+                    strncat(topic_name, motor_driver_get_id(&drv_list[i]), TOPIC_NAME_LEN);
+                    strncat(topic_name, "/position_setp", TOPIC_NAME_LEN);
+                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), topic_name);
                     cmp_write_float(&ctx, motor_driver_get_and_clear_stream_value(&drv_list[i], MOTOR_STREAM_POSITION_SETPT));
                     message_transmit(buffer, cmp_mem_access_get_pos(&mem), &server, STREAM_PORT);
                 }
 
                 if (motor_driver_get_stream_change_status(&drv_list[i])
                     & (1 << MOTOR_STREAM_POSITION)) {
-                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), "position");
-                    cmp_write_map(&ctx, 1);
-                    cmp_write_str(&ctx, motor_driver_get_id(&drv_list[i]), strlen(motor_driver_get_id(&drv_list[i])));
+                    strncpy(topic_name, "actuator/", TOPIC_NAME_LEN);
+                    strncat(topic_name, motor_driver_get_id(&drv_list[i]), TOPIC_NAME_LEN);
+                    strncat(topic_name, "/position", TOPIC_NAME_LEN);
+                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), topic_name);
                     cmp_write_float(&ctx, motor_driver_get_and_clear_stream_value(&drv_list[i], MOTOR_STREAM_POSITION));
                     message_transmit(buffer, cmp_mem_access_get_pos(&mem), &server, STREAM_PORT);
                 }
 
                 if (motor_driver_get_stream_change_status(&drv_list[i])
                     & (1 << MOTOR_STREAM_INDEX)) {
-                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), "index");
-                    cmp_write_map(&ctx, 1);
-                    cmp_write_str(&ctx, motor_driver_get_id(&drv_list[i]), strlen(motor_driver_get_id(&drv_list[i])));
+                    strncpy(topic_name, "actuator/", TOPIC_NAME_LEN);
+                    strncat(topic_name, motor_driver_get_id(&drv_list[i]), TOPIC_NAME_LEN);
+                    strncat(topic_name, "/index", TOPIC_NAME_LEN);
+                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), topic_name);
                     cmp_write_float(&ctx, motor_driver_get_and_clear_stream_value(&drv_list[i], MOTOR_STREAM_INDEX));
                     message_transmit(buffer, cmp_mem_access_get_pos(&mem), &server, STREAM_PORT);
                 }
 
                 if (motor_driver_get_stream_change_status(&drv_list[i])
                     & (1 << MOTOR_STREAM_MOTOR_ENCODER)) {
-                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), "encoder");
-                    cmp_write_map(&ctx, 1);
-                    cmp_write_str(&ctx, motor_driver_get_id(&drv_list[i]), strlen(motor_driver_get_id(&drv_list[i])));
+                    strncpy(topic_name, "actuator/", TOPIC_NAME_LEN);
+                    strncat(topic_name, motor_driver_get_id(&drv_list[i]), TOPIC_NAME_LEN);
+                    strncat(topic_name, "/encoder", TOPIC_NAME_LEN);
+                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), topic_name);
                     cmp_write_float(&ctx, motor_driver_get_and_clear_stream_value(&drv_list[i], MOTOR_STREAM_MOTOR_ENCODER));
                     message_transmit(buffer, cmp_mem_access_get_pos(&mem), &server, STREAM_PORT);
                 }
 
                 if (motor_driver_get_stream_change_status(&drv_list[i])
                     & (1 << MOTOR_STREAM_MOTOR_TORQUE)) {
-                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), "torque");
-                    cmp_write_map(&ctx, 1);
-                    cmp_write_str(&ctx, motor_driver_get_id(&drv_list[i]), strlen(motor_driver_get_id(&drv_list[i])));
+                    strncpy(topic_name, "actuator/", TOPIC_NAME_LEN);
+                    strncat(topic_name, motor_driver_get_id(&drv_list[i]), TOPIC_NAME_LEN);
+                    strncat(topic_name, "/torque", TOPIC_NAME_LEN);
+                    message_write_header(&ctx, &mem, buffer, sizeof(buffer), topic_name);
                     cmp_write_float(&ctx, motor_driver_get_and_clear_stream_value(&drv_list[i], MOTOR_STREAM_MOTOR_TORQUE));
                     message_transmit(buffer, cmp_mem_access_get_pos(&mem), &server, STREAM_PORT);
                 }
