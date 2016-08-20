9355be13c14050fd0f5e029a03ac8b03d2a6d00c
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2016-08-20 16:22:43 +0200

    Clip wheel velocity setpoint

--- src/aversive/cvra/cvra_motors.c
+++ src/aversive/cvra/cvra_motors.c
@@ -4,6 +4,8 @@
 #include "hal.h"
 #include "chprintf.h"
 
+#define MAX_MOTOR_VELOCITY_SCALE 1000.f
+
 uint32_t left_encoder_prev, right_encoder_prev;
 int32_t left_encoder_value, right_encoder_value;
 
@@ -15,18 +17,31 @@ void cvra_encoder_init(void)
     right_encoder_value = right_encoder_prev;
 }
 
-void cvra_motor_left_wheel_set_velocity(void* motor, int32_t velocity)
+void cvra_motor_set_velocity(const char *id, void *motor, int32_t velocity)
 {
     cvra_motor_t *dev = (cvra_motor_t *)motor;
-    motor_manager_set_velocity(dev->m, "left-wheel", velocity / dev->max_velocity);
-    chprintf((BaseSequentialStream *)&SD3, "Left vel %.2f\r\n", velocity / dev->max_velocity);
+
+    float vel;
+    if (velocity > MAX_MOTOR_VELOCITY_SCALE) {
+        vel = dev->max_velocity;
+    } else if (velocity < - MAX_MOTOR_VELOCITY_SCALE) {
+        vel = - dev->max_velocity;
+    } else {
+        vel = (float)velocity * dev->max_velocity / MAX_MOTOR_VELOCITY_SCALE;
+    }
+
+    motor_manager_set_velocity(dev->m, id, vel);
+    chprintf((BaseSequentialStream *)&SD3, "%s %.2f\r\n", id, vel);
+}
+
+void cvra_motor_left_wheel_set_velocity(void* motor, int32_t velocity)
+{
+    cvra_motor_set_velocity("left-wheel", motor, velocity);
 }
 
 void cvra_motor_right_wheel_set_velocity(void* motor, int32_t velocity)
 {
-    cvra_motor_t *dev = (cvra_motor_t *)motor;
-    motor_manager_set_velocity(dev->m, "right-wheel", velocity / dev->max_velocity);
-    chprintf((BaseSequentialStream *)&SD3, "Right vel %.2f\r\n", velocity / dev->max_velocity);
+    cvra_motor_set_velocity("right-wheel", motor, velocity);
 }
 
 int32_t cvra_encoder_get_left_ext(void *nothing)
