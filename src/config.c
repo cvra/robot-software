a68acbfc5f4c66efb4717d20bebaacc013f401db
Michael Spieler <michael.spieler@epfl.ch>
2016-08-22 16:03:40 +0200

    Add aversive control parameters to config tree

--- src/config.c
+++ src/config.c
@@ -9,13 +9,22 @@ parameter_namespace_t actuator_config;
 
 parameter_namespace_t master_config;
 
-static parameter_t foo;
-
 static parameter_namespace_t odometry_config;
 static parameter_t odometry_track;
 static parameter_t odometry_radius;
 
-
+static parameter_namespace_t aversive_config;
+static parameter_namespace_t aversive_control;
+static parameter_namespace_t aversive_angle;
+static parameter_t aversive_angle_kp;
+static parameter_t aversive_angle_ki;
+static parameter_t aversive_angle_kd;
+static parameter_t aversive_angle_ilimit;
+static parameter_namespace_t aversive_distance;
+static parameter_t aversive_distance_kp;
+static parameter_t aversive_distance_ki;
+static parameter_t aversive_distance_kd;
+static parameter_t aversive_distance_ilimit;
 
 void config_init(void)
 {
@@ -34,7 +43,18 @@ void config_init(void)
                                           "radius",
                                           ROBOT_EXTERNAL_WHEEL_RADIUS);
 
-    parameter_scalar_declare(&foo, &master_config, "foo");
+    parameter_namespace_declare(&aversive_config, &master_config, "aversive");
+    parameter_namespace_declare(&aversive_control, &aversive_config, "control");
+    parameter_namespace_declare(&aversive_angle, &aversive_control, "angle");
+    parameter_scalar_declare(&aversive_angle_kp, &aversive_angle, "kp");
+    parameter_scalar_declare(&aversive_angle_ki, &aversive_angle, "ki");
+    parameter_scalar_declare(&aversive_angle_kd, &aversive_angle, "kd");
+    parameter_scalar_declare(&aversive_angle_ilimit, &aversive_angle, "ilimit");
+    parameter_namespace_declare(&aversive_distance, &aversive_control, "distance");
+    parameter_scalar_declare(&aversive_distance_kp, &aversive_distance, "kp");
+    parameter_scalar_declare(&aversive_distance_ki, &aversive_distance, "ki");
+    parameter_scalar_declare(&aversive_distance_kd, &aversive_distance, "kd");
+    parameter_scalar_declare(&aversive_distance_ilimit, &aversive_distance, "ilimit");
 }
 
 float config_get_scalar(const char *id)
