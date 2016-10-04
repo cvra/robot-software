80b26666dd5c98184cacae8f3193be886b2292ae
Michael Spieler <michael.spieler@epfl.ch>
2016-10-04 23:15:41 +0200

    Add trace system

---
+++ master-firmware/src/trace/trace_points.h
@@ -0,0 +1,19 @@
+#ifndef TRACE_POINTS_H
+#define TRACE_POINTS_H
+
+#include "trace.h"
+
+#define TRACE_POINTS \
+    C(TRACE_POINT_PANIC) \
+    C(TRACE_POINT_CONTEXT_SWITCH) \
+    C(TRACE_POINT_RPC_MESSAGE_RCV) \
+    C(TRACE_POINT_RPC_MESSAGE_SEND) \
+
+/* List of all trace points in numerical format. */
+#undef C
+#define C(x) x,
+enum {
+    TRACE_POINTS
+};
+
+#endif /* TRACE_POINTS_H */
