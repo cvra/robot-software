bab2ff0206c6e8710be74649adeb8e1ec57191d9
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2016-10-08 20:00:28 +0200

    Cleanup PID plot tool and document usage

--- tools/plot_pid.py
+++ tools/plot_pid.py
@@ -18,7 +18,7 @@ MASTER_BOARD_STREAM_ADDR = ('0.0.0.0', 20042)
 
 # this must only be modified from a Bokeh session allback
 start = time.time()
-pid_consign = ColumnDataSource(data=dict(x=[0], y=[0]))
+pid_setpoint = ColumnDataSource(data=dict(x=[0], y=[0]))
 pid_measured = ColumnDataSource(data=dict(x=[0], y=[0]))
 
 # This is important! Save curdoc() to make sure all threads
@@ -32,19 +32,19 @@ topic_name_input = TextInput(value="topic", title="PID topic name:")
 plot_buffer = TextInput(value="400", title="Plot buffer:")
 
 @gen.coroutine
-def update(now, consign, measured):
-    pid_consign.stream(dict(x=[now], y=[consign]), int(plot_buffer.value))
+def update(now, setpoint, measured):
+    pid_setpoint.stream(dict(x=[now], y=[setpoint]), int(plot_buffer.value))
     pid_measured.stream(dict(x=[now], y=[measured]), int(plot_buffer.value))
 
 def msg_cb(todo, msg, args):
     if (msg == topic_name_input.value):
         now = time.time() - start
         print('[{:.3f}] receiving: {} {}'.format(now, msg, args))
-        consign = args[0]
+        setpoint = args[0]
         measured = args[1]
 
         # but update the document from callback
-        doc.add_next_tick_callback(partial(update, now=now, consign=consign, measured=measured))
+        doc.add_next_tick_callback(partial(update, now=now, setpoint=setpoint, measured=measured))
 
 RequestHandler = message.create_request_handler({}, msg_cb)
 msg_server = UDPServer(MASTER_BOARD_STREAM_ADDR, RequestHandler)
@@ -53,8 +53,8 @@ msg_sub_thd.daemon = True
 msg_sub_thd.start()
 
 p = figure(plot_width=1500, plot_height=800)
-l_consign = p.line(x='x', y='y', color="red", source=pid_consign)
-l_measured = p.line(x='x', y='y', color="blue", source=pid_measured)
+l_setpoint = p.line(x='x', y='y', color="red", source=pid_setpoint, legend='Setpoint')
+l_measured = p.line(x='x', y='y', color="blue", source=pid_measured, legend='Measured')
 
 doc.add_root(row(children=[topic_name_input, plot_buffer]))
 doc.add_root(p)
