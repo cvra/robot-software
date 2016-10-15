3a7c66002846626e2ec558c350bc5741b09d7e73
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2016-10-15 22:11:05 +0200

    Add missing include to trajectory helpers

--- master-firmware/src/robot_helpers/trajectory_helpers.c
+++ master-firmware/src/robot_helpers/trajectory_helpers.c
@@ -1,6 +1,8 @@
 #include "trajectory_manager/trajectory_manager.h"
 #include "base/base_controller.h"
 
+#include "robot_helpers/trajectory_helpers.h"
+
 void trajectory_wait_for_finish(void)
 {
     while(!trajectory_finished(&robot.traj)) {
