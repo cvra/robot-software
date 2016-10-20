c5c17d3213eccbfe0b7b06152f4c5e7df0fef555
Antoine Albertelli <antoine@antoinealb.net>
2016-10-20 15:52:02 +0200

    Add nanopb generation to build script

--- master-firmware/build.sh
+++ master-firmware/build.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+cd $(dirname $0)
+
 # red colored summary of submodules that are not updated
 printf "\033[1;31m\n"
 git submodule summary | sed -e "s/\x1b\[.\{1,5\}m//g" # the regex removes color
 printf "\033[0m"
+
+pushd ../lib/nanopb/generator/proto
+make
+popd
 
 printf "\033[1;34m\n%s\033[0m\n" "Running packager"
 packager
