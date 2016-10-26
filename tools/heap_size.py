e2f259b8617d13e4a2e9d71dda6c0267d592dc9a
Antoine Albertelli <antoine@antoinealb.net>
2016-10-26 22:51:42 +0200

    Properly parse arguments in heap size script

--- tools/heap_size.py
+++ tools/heap_size.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
+"""
+Shows how much heap memory is left in a ChibiOS project.
+"""
 import subprocess
-import sys
+import argparse
 
-RAMSIZE = 111000
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("binary", help="Compiled program (ELF format)")
 
-command = "arm-none-eabi-nm --numeric-sort --print-size {}".format(sys.argv[1]).split()
+args = parser.parse_args()
+
+command = "arm-none-eabi-nm --numeric-sort --print-size {}".format(args.binary).split()
 result = subprocess.check_output(command).decode().splitlines()
 
 heap_base = [s for s in result if "__heap_base__" in s][-1]
@@ -12,6 +18,5 @@ heap_end = [s for s in result if "__heap_end__" in s][-1]
 
 heap_base = int(heap_base.split()[0], 16)
 heap_end = int(heap_end.split()[0], 16)
-
 
 print("Heap space left: {} bytes".format(heap_end - heap_base))
