3cd897e8435bd7e2db0170750382abd0f5ac00d3
Antoine Albertelli <antoine@antoinealb.net>
2016-10-27 15:02:27 +0200

    Use argparse to open files directly

--- config/config_to_msgpack.py
+++ config/config_to_msgpack.py
@@ -17,25 +17,27 @@ def keys_to_str(to_convert):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('config_file', type=open, help='YAML file containing the config')
-    parser.add_argument('output', help='Name of the MessagePack encoded output file')
+    parser.add_argument('config_file', type=argparse.FileType(),
+                        help='YAML file containing the config')
+    parser.add_argument('output',
+                        type=argparse.FileType('w'),
+                        help='Name of the MessagePack encoded output file')
     args = parser.parse_args()
 
     config = yaml.load(args.config_file)
     config = keys_to_str(config)
     binary = msgpack.packb(config, use_single_float=True)
 
-    outfile = open(args.output, 'w')
-    outfile.write('/* generated file */\n')
-    outfile.write('#include <stddef.h>\n')
-    outfile.write('unsigned char config_msgpack[] = {')
-    outfile.write('0x{:02x}'.format(binary[0]))
+    args.output.write('/* generated file */\n')
+    args.output.write('#include <stddef.h>\n')
+    args.output.write('unsigned char config_msgpack[] = {')
+    args.output.write('0x{:02x}'.format(binary[0]))
     for b in binary[1:]:
-        outfile.write(',0x{:02x}'.format(b))
-    outfile.write('};\n')
-    outfile.write('const size_t config_msgpack_size = {};\n'.format(len(binary)))
-    outfile.flush()
-    outfile.close()
+        args.output.write(',0x{:02x}'.format(b))
+    args.output.write('};\n')
+    args.output.write('const size_t config_msgpack_size = {};\n'.format(len(binary)))
+    args.output.flush()
+    args.output.close()
 
 if __name__ == '__main__':
     main()
