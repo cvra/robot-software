00d1fe9100f6ec66689fb9327592eff7dcddc7ee
Antoine Albertelli <antoine@antoinealb.net>
2016-11-06 22:03:45 +0100

    Move linkerscripts to mirror master firmware

--- motor-control-firmware/board/board.mk
+++ motor-control-firmware/board/board.mk
@@ -8,7 +8,7 @@ ifeq ($(USE_BOOTLOADER), yes)
   BOARDDEFS = -DCORTEX_VTOR_INIT=0x08003800
   BOARDDEFS += -DCONFIG_ADDR=0x08002800
   BOARDDEFS += -DCONFIG_PAGE_SIZE=2048
-  BOARDLDSCRIPT = board/board_with_bootloader.ld
+  BOARDLDSCRIPT = ../linker/STM32F303xC_bootloader.ld
 else
-  BOARDLDSCRIPT = board/board.ld
+  BOARDLDSCRIPT = ../linker/STM32F303xC.ld
 endif
