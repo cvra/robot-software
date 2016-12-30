2df0c052db1db83ecabbd36a54e391bd11f1f30d
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2016-12-30 23:23:05 +0100

    Import Scara arm kinematics from old codebase

---
+++ master-firmware/src/scara/scara_kinematics.h
@@ -0,0 +1,38 @@
+#ifndef SCARA_KINEMATICS_H
+#define SCARA_KINEMATICS_H
+
+#include <aversive/math/geometry/vect_base.h>
+
+typedef enum {
+    SHOULDER_FRONT,
+    SHOULDER_BACK,
+} shoulder_mode_t;
+
+/** Computes the possible positions for the elbow.
+ *
+ * @param [in] x,y The coordinates of the hand.
+ * @param [in] l1,l2 The length of the two parts of the arm.
+ * @param [out] p1, p2 The possible positions.
+ * @returns The number of possible positons (0, 1 or 2).
+ */
+int compute_possible_elbow_positions(point_t target, float l1, float l2, point_t *p1, point_t *p2);
+
+/**Inverts the shoulder mode depending on wheter the arm is facing the
+ * left or right side of the robot. This is needed as "forward" and "backward"
+ * are in robot frame while the transformations should be applied in arm frame.
+ */
+shoulder_mode_t mode_for_orientation(shoulder_mode_t mode, float scara_angle_offset);
+
+/** Choose the correct elbow solution to avoid colision with the robot and to
+ * respect the given mode.
+ * @note The mode is for angle_offset > 0. Use mode_for_orientation to change it
+ * if needed.
+ */
+point_t choose_shoulder_solution(point_t target, point_t elbow1, point_t elbow2, shoulder_mode_t mode);
+
+point_t scara_forward_cinematics(float alpha, float beta, float length[2]);
+
+float compute_shoulder_angle(point_t elbow, point_t hand);
+float compute_elbow_angle(point_t elbow, point_t hand);
+
+#endif /* SCARA_KINEMATICS_H */
---
+++ master-firmware/tests/scara_kinematics.cpp
@@ -0,0 +1,33 @@
+#include "CppUTest/TestHarness.h"
+#include <math.h>
+
+extern "C" {
+#include "scara/scara_kinematics.h"
+}
+
+#define RAD(x) ((x/180.)*M_PI)
+
+TEST_GROUP(GetAngleFromArmPosTestGroup)
+{
+};
+
+TEST(GetAngleFromArmPosTestGroup, SimpleShoulderAngle)
+{
+    float angle;
+    point_t elbow, hand;
+    elbow.x = 100;
+    elbow.y = 0;
+    angle = compute_shoulder_angle(elbow, hand);
+
+    DOUBLES_EQUAL(RAD(0), angle, 1e-3);
+}
+
+TEST(GetAngleFromArmPosTestGroup, SimpleElbowAngle)
+{
+    float angle;
+    point_t elbow = {10, 10};
+    point_t hand =  {20, 20};
+
+    angle = compute_elbow_angle(elbow, hand);
+    DOUBLES_EQUAL(RAD(45), angle, 1e-3);
+}
