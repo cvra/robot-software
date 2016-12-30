bc171b2ce5390faa977b95f36438d61652e8ec6f
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2016-12-31 00:05:24 +0100

    Port more kinematics tests

--- master-firmware/tests/scara_kinematics.cpp
+++ master-firmware/tests/scara_kinematics.cpp
@@ -31,3 +31,96 @@ TEST(GetAngleFromArmPosTestGroup, SimpleElbowAngle)
     angle = scara_compute_elbow_angle(elbow, hand);
     DOUBLES_EQUAL(RAD(45), angle, 1e-3);
 }
+
+TEST_GROUP(ChooseElbowPositionTestGroup)
+{
+};
+
+TEST(ChooseElbowPositionTestGroup, ModeMirrorIdentity)
+{
+    shoulder_mode_t m = SHOULDER_FRONT;
+    float angle = 1;
+    CHECK_EQUAL(m, scara_orientation_mode(m, angle));
+}
+
+TEST(ChooseElbowPositionTestGroup, ModeMirrorInvert)
+{
+    shoulder_mode_t m = SHOULDER_FRONT;
+    float angle = -1;
+    CHECK_EQUAL(SHOULDER_BACK, scara_orientation_mode(m, angle));
+}
+
+TEST(ChooseElbowPositionTestGroup, ModeMirrorInvertBack)
+{
+    shoulder_mode_t m = SHOULDER_BACK;
+    float angle = -1;
+    CHECK_EQUAL(SHOULDER_FRONT, scara_orientation_mode(m, angle));
+}
+
+TEST(ChooseElbowPositionTestGroup, ChooseElbowSolutionOutsideRobot)
+{
+    // First test case : target_x < 0 ('inside' the robot)
+    point_t elbow1 = {10, 10};
+    point_t elbow2 = {-10, 10};
+    point_t target = {-10, 20};
+    point_t chosen;
+
+    chosen = scara_shoulder_solve(target, elbow1, elbow2, SHOULDER_FRONT);
+    CHECK_EQUAL(elbow1.x, chosen.x);
+}
+
+TEST(ChooseElbowPositionTestGroup, ChooseElbowSolutionOutsideRobotBis)
+{
+    // First test case : target_x < 0 ('inside' the robot)
+    point_t elbow1 = {-10, 10};
+    point_t elbow2 = {10, 10};
+    point_t target = {-10, 20};
+    point_t chosen;
+
+    chosen = scara_shoulder_solve(target, elbow1, elbow2, SHOULDER_FRONT);
+    CHECK_EQUAL(elbow2.x, chosen.x);
+}
+
+TEST(ChooseElbowPositionTestGroup, ChooseElbowBackward)
+{
+    point_t elbow1 = {10, 10};
+    point_t elbow2 = {10, -10};
+    point_t target = {10, 0};
+    point_t chosen;
+
+    chosen = scara_shoulder_solve(target, elbow1, elbow2, SHOULDER_BACK);
+    CHECK_EQUAL(elbow1.y, chosen.y);
+}
+
+TEST(ChooseElbowPositionTestGroup, ChooseElbowBackwardBis)
+{
+    point_t elbow1 = {10, -10};
+    point_t elbow2 = {10, 10};
+    point_t target = {10, 0};
+    point_t chosen;
+
+    chosen = scara_shoulder_solve(target, elbow1, elbow2, SHOULDER_BACK);
+    CHECK_EQUAL(elbow2.y, chosen.y);
+}
+
+TEST(ChooseElbowPositionTestGroup, ChooseElbowForward)
+{
+    point_t elbow1 = {10, -10};
+    point_t elbow2 = {10, 10};
+    point_t target = {10, 0};
+    point_t chosen;
+
+    chosen = scara_shoulder_solve(target, elbow1, elbow2, SHOULDER_FRONT);
+    CHECK_EQUAL(elbow1.y, chosen.y);
+}
+
+TEST(ChooseElbowPositionTestGroup, ChooseElbowForwardBis)
+{
+    point_t elbow1 = {10, 10};
+    point_t elbow2 = {10, -10};
+    point_t target = {10, 0};
+    point_t chosen;
+
+    chosen = scara_shoulder_solve(target, elbow1, elbow2, SHOULDER_FRONT);
+    CHECK_EQUAL(elbow2.y, chosen.y);
+}
