af5e16cd766c3408d84cd54cc1bf7b2964ee4464
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2016-12-30 23:10:25 +0100

    Import Scara arm trajectory manager from old codebase

---
+++ master-firmware/src/scara/scara_trajectories.c
@@ -0,0 +1,132 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "scara_port.h"
+#include "scara_trajectories.h"
+
+int32_t (*debra_scara_time_get)(void);
+
+
+static float smoothstep(float t)
+{
+    if(t < 0.0f) return 0.0f;
+    if(t > 1.0f) return 1.0f;
+    return t*t*t*(t*(6.0f*t-15.0f)+10.0f);
+}
+
+static float interpolate(float t, float a, float b)
+{
+    return (1 - t) * a + t * b;
+}
+
+void scara_trajectory_init(scara_trajectory_t *traj) {
+    memset(traj, 0, sizeof(scara_trajectory_t));
+}
+
+
+void scara_trajectory_append_point(scara_trajectory_t *traj, const float x, const float y, const float z,
+                                   scara_coordinate_t system, const float duration)
+{
+
+    traj->frame_count += 1;
+    traj->frames = realloc(traj->frames, traj->frame_count*sizeof(scara_waypoint_t));
+
+    if (traj->frames == NULL) {
+        scara_panic();
+    }
+
+    traj->frames[traj->frame_count-1].position[0] = x;
+    traj->frames[traj->frame_count-1].position[1] = y;
+    traj->frames[traj->frame_count-1].position[2] = z;
+    traj->frames[traj->frame_count-1].coordinate_type = system;
+
+    if (traj->frame_count == 1) {
+        traj->frames[0].date = scara_time_get();
+    } else {
+        traj->frames[traj->frame_count-1].date = traj->frames[traj->frame_count-2].date+1000000*duration;
+    }
+
+
+    traj->frames[traj->frame_count-1].length[0] = 135.16;
+    traj->frames[traj->frame_count-1].length[1] = 106.5;
+
+    if (traj->frame_count > 1) {
+        traj->frames[traj->frame_count-1].hand_angle = traj->frames[traj->frame_count-2].hand_angle;
+    } else {
+        traj->frames[traj->frame_count-1].hand_angle = 0;
+    }
+}
+
+void scara_trajectory_append_point_with_length(scara_trajectory_t *traj, const float x, const float y, const float z,
+                                   scara_coordinate_t system, const float duration, const float l1, const float l2) {
+    scara_trajectory_append_point(traj, x, y, z, system, duration);
+
+    traj->frames[traj->frame_count-1].length[0] = l1;
+    traj->frames[traj->frame_count-1].length[1] = l2;
+}
+
+void scara_trajectory_delete(scara_trajectory_t *traj)
+{
+    if (traj->frame_count != 0) {
+        free(traj->frames);
+        traj->frames = NULL;
+        traj->frame_count = 0;
+    }
+}
+
+void scara_trajectory_copy(scara_trajectory_t *dest, scara_trajectory_t *src)
+{
+    dest->frame_count = src->frame_count;
+    dest->frames = malloc(dest->frame_count * sizeof(scara_waypoint_t));
+    memcpy(dest->frames, src->frames, dest->frame_count * sizeof(scara_waypoint_t));
+}
+
+int scara_trajectory_finished(scara_trajectory_t *traj)
+{
+    int last_frame = traj->frame_count - 1;
+
+    if (traj->frame_count == 0) {
+        return 1;
+    }
+
+    if (traj->frames[last_frame].date < scara_time_get()) {
+        return 1;
+    }
+
+    return 0;
+}
+
+void scara_trajectory_set_hand_angle(scara_trajectory_t *traj, float angle)
+{
+    int last_frame = traj->frame_count - 1;
+
+    if (last_frame == -1) {
+        return;
+    }
+
+    traj->frames[last_frame].hand_angle = angle;
+}
+
+scara_waypoint_t scara_trajectory_interpolate_waypoints(scara_waypoint_t k1, scara_waypoint_t k2, int32_t date)
+{
+    float t;
+    scara_waypoint_t result;
+    int i;
+
+    result.date = date;
+
+    t = (date - k1.date) / (float)(k2.date - k1.date);
+    t = smoothstep(t);
+
+    for (i=0; i<3; i++) {
+        result.position[i] = interpolate(t, k1.position[i], k2.position[i]);
+    }
+
+    for (i=0; i<2; i++) {
+        result.length[i] = interpolate(t, k1.length[i], k2.length[i]);
+    }
+
+    result.hand_angle = interpolate(t, k1.hand_angle, k2.hand_angle);
+
+    return result;
+}
---
+++ master-firmware/src/scara/scara_trajectories.h
@@ -0,0 +1,43 @@
+#ifndef SCARA_TRAJECTORIES_H
+#define SCARA_TRAJECTORIES_H
+
+#include "scara_waypoint.h"
+
+/** Adds a point to a given trajectory.
+ * @param [in, out] traj The trajectory structure to add the point to.
+ * @param [in] x,y,z The point to add.
+ * @param [in] system The coordinate system of the point.
+ * @param [in] duration The time between this point and the previous one in second.
+ *
+ * @note This function tests if the given trajectory is empty, and if it is, it assumes the first point
+ * date is now.
+ */
+void scara_trajectory_append_point(scara_trajectory_t *traj, const float x, const float y, const float z,
+                                   scara_coordinate_t system, const float duration);
+
+void scara_trajectory_set_hand_angle(scara_trajectory_t *traj, float angle);
+
+/** Zeroes an scara_trajectory_t structure to avoid problems.
+ * @param traj The trajectory to zero.
+ */
+void scara_trajectory_init(scara_trajectory_t *traj);
+
+
+/** same as scara_trajectory_append_point but with a custom length. */
+void scara_trajectory_append_point_with_length(scara_trajectory_t *traj, const float x, const float y, const float z,
+                                   scara_coordinate_t system, const float duration, const float l1, const float l2);
+
+
+void scara_trajectory_delete(scara_trajectory_t *traj);
+
+void scara_trajectory_copy(scara_trajectory_t *dest, scara_trajectory_t *src);
+
+int scara_trajectory_finished(scara_trajectory_t *traj);
+
+/** Interpolates two waypoint.
+ * @warning All frames must be expressed in arm frame. Otherwise invalid results
+ * will be yielded.
+ */
+scara_waypoint_t scara_trajectory_interpolate_waypoints(scara_waypoint_t k1, scara_waypoint_t k2, int32_t date);
+
+#endif /* SCARA_TRAJECTORIES_H */
