0d3c5977aa858047cbbb4ec3491225909dab0eb9
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2017-04-18 00:40:43 +0200

    Pass pointer to bus correctly to function
    Not pointer to pointer which is wrong and dangerous

--- master-firmware/src/robot_helpers/strategy_helpers.c
+++ master-firmware/src/robot_helpers/strategy_helpers.c
@@ -61,7 +61,7 @@ void strategy_align_y(int32_t y, int32_t robot_size, struct _robot* robot, messa
 {
     trajectory_set_mode_aligning(&robot->mode, &robot->traj, &robot->distance_bd, &robot->angle_bd);
 
-    trajectory_align_with_wall(robot, &bus);
+    trajectory_align_with_wall(robot, bus);
     robot->pos.pos_d.y = robot_size / 2;
     robot->pos.pos_s16.y = robot_size / 2;
 
