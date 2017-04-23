dd67336b74526eefcea4e17746691d7180bceeda
Michael Spieler <michael.spieler@epfl.ch>
2017-04-23 17:15:36 +0200

    Move UART parameter listener

--- motor-control-firmware/src/main.c
+++ motor-control-firmware/src/main.c
@@ -7,51 +7,17 @@
 #include "setpoint.h"
 #include "analog.h"
 #include "encoder.h"
-#include "cmp_mem_access/cmp_mem_access.h"
-#include "serial-datagram/serial_datagram.h"
 #include "parameter/parameter.h"
-#include "parameter/parameter_msgpack.h"
-#include <string.h>
 #include <math.h>
 #include "bootloader_config.h"
 #include "uavcan/uavcan_node.h"
 #include "timestamp/timestamp_stm32.h"
 #include "index.h"
 #include "uart_stream.h"
+#include "parameter_listener.h"
 
 BaseSequentialStream* ch_stdout;
 parameter_namespace_t parameter_root_ns;
-
-static void error_cb(void *arg, const char *id, const char *err)
-{
-    (void)arg;
-    (void)id;
-    (void)err;
-}
-
-static void parameter_decode_cb(const void *dtgrm, size_t len, void *arg)
-{
-    int ret = parameter_msgpack_read(&parameter_root_ns, (char*)dtgrm, len, error_cb, arg);
-    chprintf(ch_stdout, "ok %d\n", ret);
-    // parameter_print(&parameter_root_ns);
-}
-
-static THD_WORKING_AREA(parameter_listener_wa, 512);
-static THD_FUNCTION(parameter_listener, arg)
-{
-    static char rcv_buf[200];
-    static serial_datagram_rcv_handler_t rcv_handler;
-    serial_datagram_rcv_handler_init(&rcv_handler, &rcv_buf, sizeof(rcv_buf), parameter_decode_cb, NULL);
-    while (1) {
-        char c = chSequentialStreamGet((BaseSequentialStream*)arg);
-        int ret = serial_datagram_receive(&rcv_handler, &c, 1);
-        if (ret != SERIAL_DATAGRAM_RCV_NO_ERROR) {
-            chprintf(ch_stdout, "serial datagram error %d\n", ret);
-        }
-        (void)ret; // ingore errors
-    }
-}
-
 
 void panic_hook(const char* reason)
 {
@@ -161,7 +127,7 @@ int main(void) {
     index_init();
 
     // uart_stream_start(ch_stdout);
-    chThdCreateStatic(parameter_listener_wa, sizeof(parameter_listener_wa), LOWPRIO, parameter_listener, &SD3);
+    parameter_listener_start(ch_stdout);
     chThdCreateStatic(led_thread_wa, sizeof(led_thread_wa), LOWPRIO, led_thread, NULL);
 
     static struct uavcan_node_arg node_arg;
