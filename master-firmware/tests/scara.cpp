b5980cb66a7346694870d4e4f6abb6cf0ba1c41d
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2017-05-15 00:07:10 +0200

    Fix unit tests

--- master-firmware/tests/scara.cpp
+++ master-firmware/tests/scara.cpp
@@ -28,6 +28,24 @@ float get_motor_pos(void *m)
     return *(float *)m;
 }
 
+void set_wrist_pos(void *m, float heading, float pitch)
+{
+    (void)pitch;
+    *(float *)m = heading;
+}
+
+void set_wrist_vel(void *m, float heading, float pitch)
+{
+    (void)pitch;
+    *(float *)m = heading;
+}
+
+void get_wrist_pos(void *m, float* heading, float* pitch)
+{
+    (void)pitch;
+    *heading = *(float *)m;
+}
+
 
 TEST_GROUP(ArmTestGroup)
 {
@@ -51,7 +69,7 @@ TEST_GROUP(ArmTestGroup)
         scara_set_z_callbacks(&arm, set_motor_pos, get_motor_pos, &z_pos);
         scara_set_shoulder_callbacks(&arm, set_motor_pos, set_motor_vel, get_motor_pos, &shoulder_angle);
         scara_set_elbow_callbacks(&arm, set_motor_pos, set_motor_vel, get_motor_pos, &elbow_angle);
-        scara_set_wrist_callbacks(&arm, set_motor_pos, set_motor_vel, get_motor_pos, &wrist_angle);
+        scara_set_wrist_callbacks(&arm, set_wrist_pos, set_wrist_vel, get_wrist_pos, &wrist_angle);
     }
 
     void teardown()
@@ -282,34 +300,36 @@ TEST_GROUP(JacobianTestGroup)
 /* See doc/Debra Kinematics.ipynb for values. */
 TEST(JacobianTestGroup, SmokeTest)
 {
-    float f_x = 1, f_y = 1, f_theta = 0;
-    float alpha = 0.2, beta = -0.2, gamma = 0.;
+    float f_x = 1, f_y = 1, f_theta = 0, f_pitch = 0;
+    float alpha = 0.2, beta = -0.2, gamma = 0., delta = 0.;
     float l1 = 100, l2 = 200, l3 = 30;
-    float torque_alpha, torque_beta, torque_gamma;
+    float torque_alpha, torque_beta, torque_gamma, torque_delta;
 
-    scara_jacobian_compute(f_x, f_y, f_theta,
-                            alpha, beta, gamma,
-                            l1, l2, l3,
-                             &torque_alpha,  &torque_beta,  &torque_gamma);
+    scara_jacobian_compute(f_x, f_y, f_theta, f_pitch,
+                           alpha, beta, gamma, delta,
+                           l1, l2, l3,
+                           &torque_alpha,  &torque_beta,  &torque_gamma, &torque_delta);
 
     DOUBLES_EQUAL(-0.04874946, torque_alpha, 1e-3);
     DOUBLES_EQUAL(0.07341263, torque_beta, 1e-3);
     DOUBLES_EQUAL(0.0034615, torque_gamma, 1e-3);
+    DOUBLES_EQUAL(0, torque_delta, 1e-3);
 }
 
 TEST(JacobianTestGroup, TestSingularity)
 {
-    float f_x = 1, f_y = 1, f_theta = 0;
-    float alpha = 0.2e-3, beta = -0.2e-3, gamma = 0.;
+    float f_x = 1, f_y = 1, f_theta = 0, f_pitch = 0;
+    float alpha = 0.2e-3, beta = -0.2e-3, gamma = 0., delta = 0.;
     float l1 = 100, l2 = 200, l3 = 30;
-    float torque_alpha, torque_beta, torque_gamma;
+    float torque_alpha, torque_beta, torque_gamma, torque_delta;
 
-    scara_jacobian_compute(f_x, f_y, f_theta,
-                            alpha, beta, gamma,
-                            l1, l2, l3,
-                             &torque_alpha,  &torque_beta,  &torque_gamma);
+    scara_jacobian_compute(f_x, f_y, f_theta, f_pitch,
+                           alpha, beta, gamma, delta,
+                           l1, l2, l3,
+                           &torque_alpha,  &torque_beta,  &torque_gamma, &torque_delta);
 
     DOUBLES_EQUAL(0.00056127, torque_alpha, 1e-3);
     DOUBLES_EQUAL(0.00358869, torque_beta, 1e-3);
     DOUBLES_EQUAL(-0.00035656, torque_gamma, 1e-3);
+    DOUBLES_EQUAL(0, torque_delta, 1e-3);
 }
