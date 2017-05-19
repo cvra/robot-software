6984f3b5fe24dfe2114c2a809fcd5a2036ae13b5
Antoine Albertelli <antoine@antoinealb.net>
2017-05-19 10:01:58 +0200

    Periodically publish hand pwm

--- master-firmware/src/can/hand_driver.cpp
+++ master-firmware/src/can/hand_driver.cpp
@@ -22,6 +22,8 @@ static CONDVAR_DECL(left_hand_sensors_topic_condvar);
 static CONDVAR_DECL(right_hand_sensors_topic_condvar);
 static hand_sensors_t left_hand_sensors_topic_value, right_hand_sensors_topic_value;
 
+static finger_state_t left_status[4], right_status[4];
+
 
 static uavcan::LazyConstructor<uavcan::Publisher<cvra::io::ServoPWM> > fingers_pub;
 
@@ -55,9 +57,55 @@ static void digital_input_cb(const uavcan::ReceivedDataStructure<cvra::io::Digit
 
 }
 
+static void send_fingers_pwm(const char *name, finger_state_t status[4])
+{
+    cvra::io::ServoPWM pwm_signals;
+
+    uint8_t node_id = bus_enumerator_get_can_id(&bus_enumerator, name);
+    if (node_id == BUS_ENUMERATOR_CAN_ID_NOT_SET || node_id == BUS_ENUMERATOR_STRING_ID_NOT_FOUND) {
+        WARNING("Hand %s was not identified correctly [node_id: %d]", name, node_id);
+        return;
+    }
+
+    pwm_signals.node_id = node_id;
+
+    for (size_t i = 0; i < 4; i++) {
+        if (status[i] == FINGER_CLOSED) {
+            pwm_signals.servo_pos[i] = FINGERS_PULSE_CLOSED[0][i];
+        } else if (status[i] == FINGER_OPEN) {
+            pwm_signals.servo_pos[i] = FINGERS_PULSE_OPEN[0][i];
+        } else {
+            pwm_signals.servo_pos[i] = 0.8 * FINGERS_PULSE_CLOSED[0][i] + 0.2 *
+                                       FINGERS_PULSE_OPEN[0][i];
+        }
+    }
+
+    fingers_pub->broadcast(pwm_signals);
+
+    DEBUG("Sent to hand \"%s\" node %d, signals: %.3f %.3f %.3f %.3f",
+           name, node_id,
+           pwm_signals.servo_pos[0],
+           pwm_signals.servo_pos[1],
+           pwm_signals.servo_pos[2],
+           pwm_signals.servo_pos[3]);
+}
+
+static void timer_cb(const uavcan::TimerEvent &event)
+{
+    (void) event;
+    send_fingers_pwm("left-hand", left_status);
+    send_fingers_pwm("right-hand", right_status);
+}
+
 
 int hand_driver_init(uavcan::INode &node)
 {
+
+    for (int i = 0; i < 4; i++) {
+        left_status[i] = FINGER_CLOSED;
+        right_status[i] = FINGER_CLOSED;
+    }
+
     messagebus_topic_init(&left_hand_sensors_topic,
                           &left_hand_sensors_topic_lock,
                           &left_hand_sensors_topic_condvar,
@@ -82,6 +130,11 @@ int hand_driver_init(uavcan::INode &node)
         fingers_pub.construct<uavcan::INode &>(node);
     }
 
+    static uavcan::Timer periodic_timer(node);
+    periodic_timer.setCallback(timer_cb);
+    periodic_timer.startPeriodic(uavcan::MonotonicDuration::fromMSec(100));
+
+
     return 0;
 }
 
@@ -89,35 +142,20 @@ extern "C" {
 
 void hand_driver_set_fingers(const char *hand_id, finger_state_t* status)
 {
-    cvra::io::ServoPWM pwm_signals;
-
-    uint8_t node_id = bus_enumerator_get_can_id(&bus_enumerator, hand_id);
-    if (node_id == BUS_ENUMERATOR_CAN_ID_NOT_SET || node_id == BUS_ENUMERATOR_STRING_ID_NOT_FOUND) {
-        WARNING("Hand %s was not identified correctly [node_id: %d]", hand_id, node_id);
+    int i;
+    finger_state_t *dst;
+    if (!strcmp(hand_id, "left-hand")) {
+        dst = left_status;
+    } else if (!strcmp(hand_id, "right-hand")) {
+        dst = right_status;
+    } else {
+        ERROR("Unknown hand \"%s\"", hand_id);
         return;
     }
 
-    pwm_signals.node_id = node_id;
-
-    for (size_t i = 0; i < 4; i++) {
-        if (status[i] == FINGER_CLOSED) {
-            pwm_signals.servo_pos[i] = FINGERS_PULSE_CLOSED[0][i];
-        } else if (status[i] == FINGER_OPEN) {
-            pwm_signals.servo_pos[i] = FINGERS_PULSE_OPEN[0][i];
-        } else {
-            pwm_signals.servo_pos[i] = 0.8 * FINGERS_PULSE_CLOSED[0][i] + 0.2 *
-                                       FINGERS_PULSE_OPEN[0][i];
-        }
+    for (i = 0; i < 4; i++) {
+        dst[i] = status[i];
     }
-
-    fingers_pub->broadcast(pwm_signals);
-
-    NOTICE("Sent to hand node %d, signals: %.3f %.3f %.3f %.3f",
-           node_id,
-           pwm_signals.servo_pos[0],
-           pwm_signals.servo_pos[1],
-           pwm_signals.servo_pos[2],
-           pwm_signals.servo_pos[3]);
 }
 
 void hand_driver_set_fingers_float(const char *hand_id, float* signal)
