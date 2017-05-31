68e8d10dc3a7582bf944784d9c470908082140a4
Michael Spieler <michael.spieler@epfl.ch>
2017-05-31 22:33:19 +0200

    Move board files

--- master-firmware/src/board/ST_NUCLEO144_F429ZI/board.mk
+++
@@ -1,8 +0,0 @@
-BOARDINC = src/board/ST_NUCLEO144_F429ZI
-BOARDSRC = $(BOARDINC)/board.c
-
-ifeq ($(USE_BOOTLOADER), yes)
-  $(error bootloader for ST_NUCLEO144_F429ZI not supported)
-else
-  LDSCRIPT= linker/STM32F429xI.ld
-endif
