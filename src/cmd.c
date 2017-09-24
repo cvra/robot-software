4cf7f52b56bf520dea6332c60d50632d4f72a4a7
Antoine Albertelli <antoine@antoinealb.net>
2017-09-24 16:04:06 +0200

    Add a diagnostic command for messagebus
    Allows listing the topics, as well as measuring the publish rate on the
    topic.

--- src/cmd.c
+++ src/cmd.c
@@ -4,6 +4,8 @@
 #include <hal.h>
 #include <chprintf.h>
 #include <shell.h>
+
+#include "main.h"
 #include "mpu9250.h"
 
 #define TEST_WA_SIZE        THD_WORKING_AREA_SIZE(256)
@@ -17,9 +19,67 @@ static void cmd_reboot(BaseSequentialStream *chp, int argc, char **argv)
     NVIC_SystemReset();
 }
 
+static void cmd_topics(BaseSequentialStream *chp, int argc, char *argv[])
+{
+    (void) argc;
+    (void) argv;
+
+    const char *usage = "usage:\r\n"
+                        "topics list -- Lists all available topics.\r\n"
+                        "topics hz topic_name -- Displays the rate of the"
+                        "topic over a 5 second window.";
+
+    if (argc < 1) {
+        chprintf(chp, "%s\r\n", usage);
+        return;
+    }
+
+    if (!strcmp(argv[0], "list")) {
+        chprintf(chp, "available topics:\r\n");
+
+        MESSAGEBUS_TOPIC_FOREACH(&bus, topic) {
+            chprintf(chp, "%s\r\n", topic->name);
+        }
+    } else if (!strcmp(argv[0], "hz")) {
+        if (argc != 2) {
+            chprintf(chp, "%s\r\n", usage);
+            return;
+        }
+
+        messagebus_topic_t *topic = messagebus_find_topic(&bus, argv[1]);
+        if (topic == NULL) {
+            chprintf(chp, "Cannot find topic \"%s\".\r\n", argv[1]);
+            return;
+        }
+
+        chprintf(chp, "Waiting for publish for 5 seconds...\r\n");
+
+        systime_t start = chVTGetSystemTime();
+        unsigned int message_counter = 0;
+
+        while (chVTGetSystemTime() < start + MS2ST(5000)) {
+            chMtxLock(topic->lock);
+            if (chCondWaitTimeout(topic->condvar, MS2ST(10)) != MSG_TIMEOUT) {
+                message_counter ++;
+                chMtxUnlock(topic->lock);
+            }
+        }
+
+        if (message_counter == 0) {
+            chprintf(chp, "No messages.\r\n");
+        } else {
+            chprintf(chp, "Average rate: %.2f Hz\r\n", message_counter / 5.);
+        }
+    } else {
+        chprintf(chp, "%s\r\n", usage);
+        return;
+    }
+}
+
 static ShellConfig shell_cfg;
 const ShellCommand shell_commands[] = {
     {"reboot", cmd_reboot},
+    {"topics", cmd_topics},
     {NULL, NULL}
 };
 
