15a94537360910a1070b854756833fd9134e319e
Antoine Albertelli <antoine@antoinealb.net>
2017-09-26 22:54:30 +0200

    Add decawave's sdk to the build
    Also add a simple test to make sure everything builds and to evaluate
    what shims needs to be evaluated.

---
+++ tests/decawave.cpp
@@ -0,0 +1,58 @@
+#include <CppUTest/TestHarness.h>
+#include <CppUTestExt/MockSupport.h>
+#include "decadriver/deca_device_api.h"
+#include "decadriver/deca_regs.h"
+
+extern "C"
+int readfromspi(uint16 headerLength, const uint8 *headerBuffer,
+                uint32 readlength, uint8 *readBuffer)
+{
+    (void) readlength;
+    mock("spi").actualCall("read")
+               .withMemoryBufferParameter("header", (const unsigned char *)headerBuffer, headerLength)
+               .withOutputParameter("buf", (unsigned char *)readBuffer);
+
+    return DWT_SUCCESS;
+}
+
+extern "C"
+int writetospi(uint16 headerLength, const uint8 *headerBuffer,
+               uint32 bodyLength, const uint8 *bodyBuffer)
+{
+
+    return DWT_SUCCESS;
+}
+
+extern "C"
+void deca_sleep(unsigned int time_ms)
+{
+}
+
+extern "C"
+decaIrqStatus_t decamutexon(void)
+{
+    return 0;
+}
+
+extern "C"
+void decamutexoff(decaIrqStatus_t s)
+{
+}
+
+TEST_GROUP(Decawave)
+{
+};
+
+/* little sanity check to make sure everything is working as expected. */
+TEST(Decawave, ReadId)
+{
+    uint8_t header[] = {0x00};
+    uint8_t reply[] = {0x30, 0x01, 0xca, 0xde};
+    mock("spi").expectOneCall("read")
+                .withMemoryBufferParameter("header", header, sizeof(header))
+                .withOutputParameterReturning("buf", reply, sizeof(reply));
+
+    auto id = dwt_readdevid();
+
+    CHECK_EQUAL(DWT_DEVICE_ID, id);
+}
