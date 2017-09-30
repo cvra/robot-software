387c89be6886a1d646203b2151ec7d5dd77be527
Antoine Albertelli <antoine@antoinealb.net>
2017-09-30 12:59:51 +0200

    Implement correction step for EKF

--- models/tests/test_kalman_filter.py
+++ models/tests/test_kalman_filter.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
+from numpy.linalg import inv
 import numpy as np
+
 
 class EKFPredictor:
     def __init__(self, dynamics, dynamics_jacobian, R):
@@ -36,11 +38,42 @@ class EKFPredictor:
         return self.predict(mu, sigma, u)
 
 
+class EKFCorrector:
+    def __init__(self, measurement, measurement_jacobian, Q):
+        """
+        Parameters:
+
+        measurement is a function of the state returning the expected measurement.
+
+        measurement jacobian is a function of the state returning the jacobian
+        of the measurement.
+
+        Q is the covariance of the measurement.
+        """
+        self.h = measurement
+        self.H = measurement_jacobian
+        self.Q = Q
+
+    def correct(self, mu, sigma, z):
+        H = self.H(mu)
+        K = sigma @ H.T @ inv(H @ sigma @ H.T + self.Q)
+        mu = mu + K @ (z - self.h(mu))
+        sigma = (np.eye(H.shape[0]) - K @ H) @ sigma
+        return mu, sigma
+
+    def __call__(self, mu, sigma, z):
+        """
+        Shortcut notation for update.
+        """
+        return self.correct(mu, sigma, z)
+
+
 class PredictorTest(TestCase):
     def setUp(self):
         self.g = lambda s, u: s + u
-        self.G = lambda s, u: np.array([[1]]) # linearized
-        self.R = np.array([[0.1]]) # noise induced
+        self.G = lambda s, u: np.array([[1]])  # linearized
+        self.R = np.array([[0.1]])  # noise induced
+
         self.f = EKFPredictor(self.g, self.G, self.R)
 
     def test_constructor(self):
@@ -72,3 +105,69 @@ class PredictorTest(TestCase):
 
         self.assertAlmostEqual(mu2, 0.1, places=5)
         self.assertAlmostEqual(sigma2[0][0], 0.2, places=5)
+
+
+class UpdaterTestCase(TestCase):
+    def setUp(self):
+        self.h = lambda s: s
+        self.H = lambda _: np.array([[1]])
+        self.Q = np.array([[0.1]])
+        self.f = EKFCorrector(self.h, self.H, self.Q)
+
+    def test_constructor(self):
+        """
+        Checks that we can create an measurement updater.
+        """
+        self.assertEqual(self.h, self.f.h)
+        self.assertEqual(self.H, self.f.H)
+        self.assertEqual(self.Q, self.f.Q)
+
+    def test_update(self):
+        """
+        Checks that we can correctly correct the state.
+
+        Just a basic smoke test
+        """
+        mu, sigma = np.array([0]), np.array([[0.1]])
+        z = np.array([2])
+        mu, sigma = self.f.correct(mu, sigma, z)
+
+        self.assertAlmostEqual(1.0, mu[0], places=4)
+        self.assertAlmostEqual(0.05, sigma[0, 0], places=4)
+
+    def test_correction_short_form(self):
+        """
+        Checks that we can use the short form of the corrector.
+        """
+        mu, sigma = np.array([0]), np.array([[0.1]])
+        z = np.array([2])
+        mu, sigma = self.f(mu, sigma, z)
+
+        self.assertAlmostEqual(1.0, mu[0], places=4)
+        self.assertAlmostEqual(0.05, sigma[0, 0], places=4)
+
+
+class EKFIntegration(TestCase):
+    def setUp(self):
+        self.g = lambda s, u: s
+        self.G = lambda s, u: np.array([[1]])  # linearized
+        self.R = np.array([[0.1]])  # noise induced
+
+        self.predictor = EKFPredictor(self.g, self.G, self.R)
+
+        self.h = lambda s: s
+        self.H = lambda _: np.array([[1]])
+        self.Q = np.array([[0.1]])
+        self.corrector = EKFCorrector(self.h, self.H, self.Q)
+
+    def test_does_it_converge(self):
+        u = np.array([0])
+        z = np.array([2])
+
+        mu, sigma = np.array([0]), np.array([10])
+
+        for _ in range(100):
+            mu, sigma = self.predictor.predict(mu, sigma, u)
+            mu, sigma = self.corrector.correct(mu, sigma, z)
+
+        self.assertAlmostEqual(mu[0], 2, places=4)
