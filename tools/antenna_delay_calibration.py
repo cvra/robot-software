4477b523c27349e5ce6f419a0c3471c396a83c5f
Antoine Albertelli <antoine@antoinealb.net>
2017-11-24 09:55:56 +0100

    Add a tool for antenna calibration

---
+++ tools/antenna_delay_calibration.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Calibrates beacons
+"""
+
+import argparse
+import uavcan
+import os.path
+import threading
+from statistics import mean
+
+# Speed of light in decawave units
+SPEED_OF_LIGHT = 299792458.0 / (128 * 499.2e6)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--distance", "-d", type=float, help="Distance separating the two beacons.", required=True)
+    parser.add_argument("--port", "-p", help="Serial port to which the adapter is connected or name of socketcan interface.", required=True)
+
+    return parser.parse_args()
+
+def set_antenna_delay(node, value):
+    request = uavcan.protocol.param.GetSet.Request()
+    request.name = '/uwb/antenna_delay'
+    request.value = uavcan.protocol.param.Value(real_value=value)
+
+def main():
+    args = parse_args()
+    node = uavcan.make_node(args.port, node_id=123)
+    node.lock = threading.RLock()
+
+    # TODO This path is a bit too hardcoded
+    dsdl_path = os.path.join(os.path.dirname(__file__), '..', 'beacon_messages')
+    uavcan.load_dsdl(dsdl_path)
+
+    samples = []
+    def range_cb(event):
+        samples.append(event.message.range)
+        print(event.message.range)
+        if len(samples) >= 100:
+            bias = mean(samples) - args.distance
+            print("Estimated bias: {:.3f}".format(bias))
+            print("Delta to add in decawave units: {}".format(int((bias / 2) / SPEED_OF_LIGHT)))
+
+    node.add_handler(uavcan.thirdparty.beacon_messages.equipment.RadioRange, range_cb)
+
+    node.spin()
+
+
+if __name__ == '__main__':
+    main()
