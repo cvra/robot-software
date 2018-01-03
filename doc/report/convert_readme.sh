da00f4a2ea06d70ca2e489bc7573d55f1e5fac99
Antoine Albertelli <antoine@antoinealb.net>
2018-01-03 02:14:11 +0100

    Include a chapter on source code org

--- doc/report/convert_readme.sh
+++ doc/report/convert_readme.sh
@@ -10,3 +10,8 @@ sed -i s/doc\\/report\\///g user_guide.tex
 
 # Convert links to footnotes
 sed -i "s/\\\\href{\\([^}]*\\)}{\\([^}]*\\)}/\2\\\\footnote{\\\\url{\1}}/" user_guide.tex
+
+pandoc --from markdown --to latex --output code_source_org.tex code_source_org.md
+
+# Use non numbered section for code source organization
+sed -i "s/section/section*/" code_source_org.tex
