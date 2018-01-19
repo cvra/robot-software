3575249479de152dcb5320b4f4c7e62b34333526
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-01-19 22:33:43 +0100

    Node discovery tool: Wrap uavcan node in class

--- tools/studio/node_discovery.py
+++ tools/studio/node_discovery.py
@@ -2,18 +2,25 @@ import threading
 import time
 import uavcan
 
-def uavcan_thread(node):
-    while True:
-        try:
-            node.spin(0.1)
-            time.sleep(0.01)
-        except uavcan.UAVCANException as ex:
-            print('Node error:', ex)
-            return
+class UavcanNode:
+    def __init__(self, interface, node_id):
+        self.node = uavcan.make_node(interface, node_id=node_id)
+
+    def spin(self):
+        threading.Thread(target=self._uavcan_thread).start()
+
+    def _uavcan_thread(self):
+        while True:
+            try:
+                self.node.spin(0.1)
+                time.sleep(0.01)
+            except uavcan.UAVCANException as ex:
+                print('Node error:', ex)
+                return
 
 def main():
-    node = uavcan.make_node("slcan0", node_id=127)
-    threading.Thread(target=uavcan_thread, args=(node,)).start()
+    node = UavcanNode(interface="slcan0", node_id=127)
+    node.spin()
 
 if __name__ == '__main__':
     main()
