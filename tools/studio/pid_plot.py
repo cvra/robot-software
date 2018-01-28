f957c0ebcbe7f2c25c3c9bfdca712b5f252d0c8a
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-01-28 22:15:26 +0100

    Live plot: Move PID data reception to model

--- tools/studio/pid_plot.py
+++ tools/studio/pid_plot.py
@@ -42,28 +42,89 @@ class PidViewer(QtGui.QWidget):
         self.setWindowTitle("PID Plotter")
         self.show()
 
-class PidPlotController:
-    def __init__(self, node):
-        self.logger = logging.getLogger('PidPlotController')
-        self.node = node
-        self.model = NodeStatusMonitor(node)
-        self.viewer = PidViewer()
-        self.curve = self.viewer.plot.getPort()
+class PidFeedbackRecorder():
+    nodes = []
+    on_new_node_callback = None
+    tracked_node = None
 
-        self.data = self._empty_pid_data()
-        self.node_to_plot = None
+    PID_LOOPS = ['Current', 'Velocity', 'Position']
+    tracked_pid_loop = PID_LOOPS[0]
+
+    EMPTY_DATA = { 'setpoint': { 'time': np.array([]), 'value': np.array([]) },
+                   'measured': { 'time': np.array([]), 'value': np.array([]) }, }
+    data = EMPTY_DATA
+
+    def __init__(self, node):
+        self.logger = logging.getLogger('PidFeedbackRecorder')
+        self.node = node
+        self.data_lock = threading.RLock()
+
+        self.monitor = NodeStatusMonitor(node)
+        self.monitor.set_on_new_node_callback(self._update_nodes)
+
+        self.clear()
         self.node.add_handler(uavcan.thirdparty.cvra.motor.feedback.CurrentPID, self._current_pid_callback)
         self.node.add_handler(uavcan.thirdparty.cvra.motor.feedback.VelocityPID, self._velocity_pid_callback)
         self.node.add_handler(uavcan.thirdparty.cvra.motor.feedback.PositionPID, self._position_pid_callback)
 
-        self.nodes = []
-        self.model.set_on_new_node_callback(self.update_selection)
+    def on_new_node(self, callback):
+        self.on_new_node = callback
+
+    def node_id_to_name(self, node_id):
+        return self.monitor.node_id_to_name(node_id)
+
+    def clear(self):
+        with self.data_lock:
+            self.data = self.EMPTY_DATA
+
+    def _update_nodes(self):
+        known_nodes = self.monitor.known_nodes
+        nodes_with_name = {k: v for k, v in known_nodes.items() if 'name' in v.keys()}
+        self.nodes = list(nodes_with_name[node]['name'] for node in nodes_with_name)
+
+        if self.on_new_node:
+            self.on_new_node()
+
+    def _current_pid_callback(self, event):
+        if self.tracked_pid_loop == 'Current':
+            self._pid_callback(node_id=event.transfer.source_node_id,
+                               setpoint=event.message.current_setpoint,
+                               measured=event.message.current)
+
+    def _velocity_pid_callback(self, event):
+        if self.tracked_pid_loop == 'Velocity':
+            self._pid_callback(node_id=event.transfer.source_node_id,
+                               setpoint=event.message.velocity_setpoint,
+                               measured=event.message.velocity)
+
+    def _position_pid_callback(self, event):
+        if self.tracked_pid_loop == 'Position':
+            self._pid_callback(node_id=event.transfer.source_node_id,
+                               setpoint=event.message.position_setpoint,
+                               measured=event.message.position)
+
+    def _pid_callback(self, node_id, setpoint, measured):
+        node_name = self.monitor.node_id_to_name(node_id)
+        if self.tracked_node is not None and self.tracked_node == node_name:
+            current_time = time.time()
+            self.data['setpoint']['time'] = np.append(self.data['setpoint']['time'], current_time)
+            self.data['measured']['time'] = np.append(self.data['measured']['time'], current_time)
+            self.data['setpoint']['value'] = np.append(self.data['setpoint']['value'], setpoint)
+            self.data['measured']['value'] = np.append(self.data['measured']['value'], measured)
+
+class PidPlotController:
+    def __init__(self, node):
+        self.logger = logging.getLogger('PidPlotController')
+        self.node = node
+        self.model = PidFeedbackRecorder(node)
+        self.viewer = PidViewer()
+        self.curve = self.viewer.plot.getPort()
+
+        self.model.on_new_node(self.update_selection)
 
         self.viewer.node_selector.set_callback(self._change_selected_node)
 
-        self.pid_loops = ['Current', 'Velocity', 'Position']
-        self.pid_loop_to_plot = self.pid_loops[0]
-        self.viewer.pid_loop_selector.set_nodes(self.pid_loops)
+        self.viewer.pid_loop_selector.set_nodes(self.model.PID_LOOPS)
         self.viewer.pid_loop_selector.set_callback(self._change_selected_pid_loop)
 
         threading.Thread(target=self.run).start()
@@ -71,67 +132,22 @@ class PidPlotController:
     def run(self):
         self.logger.info('PID widget started')
         while True:
-            self.curve.put(self.data)
+            self.curve.put(self.model.data)
             time.sleep(0.2)
 
     def update_selection(self):
         self.logger.debug('New node detected, updating available nodes list')
-        known_nodes = self.model.known_nodes
-        nodes_with_name = {k: v for k, v in known_nodes.items() if 'name' in v.keys()}
-        self.nodes = list(nodes_with_name[node]['name'] for node in nodes_with_name)
-        self.viewer.node_selector.set_nodes(self.nodes)
-
-    def _current_pid_callback(self, event):
-        if self.pid_loop_to_plot == 'Current':
-            self._pid_callback(node_id=event.transfer.source_node_id,
-                               setpoint=event.message.current_setpoint,
-                               measured=event.message.current)
-
-    def _velocity_pid_callback(self, event):
-        if self.pid_loop_to_plot == 'Velocity':
-            self._pid_callback(node_id=event.transfer.source_node_id,
-                               setpoint=event.message.velocity_setpoint,
-                               measured=event.message.velocity)
-
-    def _position_pid_callback(self, event):
-        if self.pid_loop_to_plot == 'Position':
-            self._pid_callback(node_id=event.transfer.source_node_id,
-                               setpoint=event.message.position_setpoint,
-                               measured=event.message.position)
-
-    def _pid_callback(self, node_id, setpoint, measured):
-        node_name = self.model.node_id_to_name(node_id)
-
-        if self.node_to_plot is None or self.node_to_plot != node_name:
-            return
-
-        current_time = time.time()
-        self.data['setpoint']['time'] = np.append(self.data['setpoint']['time'], current_time)
-        self.data['measured']['time'] = np.append(self.data['measured']['time'], current_time)
-        self.data['setpoint']['value'] = np.append(self.data['setpoint']['value'], setpoint)
-        self.data['measured']['value'] = np.append(self.data['measured']['value'], measured)
+        self.viewer.node_selector.set_nodes(self.model.nodes)
 
     def _change_selected_node(self, i):
-        self.node_to_plot = self.nodes[i]
-        self.data = self._empty_pid_data()
-        self.logger.info('Selected node {}'.format(self.node_to_plot))
+        self.model.tracked_node = self.model.nodes[i]
+        self.model.clear()
+        self.logger.info('Selected node {}'.format(self.model.tracked_node))
 
     def _change_selected_pid_loop(self, i):
-        self.pid_loop_to_plot = self.pid_loops[i]
-        self.data = self._empty_pid_data()
-        self.logger.info('Selected PID loop {}'.format(self.pid_loop_to_plot))
-
-    def _empty_pid_data(self):
-        return {
-            'setpoint': {
-                'time': np.array([]),
-                'value': np.array([]),
-            },
-            'measured': {
-                'time': np.array([]),
-                'value': np.array([]),
-            },
-        }
+        self.model.tracked_pid_loop = self.model.PID_LOOPS[i]
+        self.model.clear()
+        self.logger.info('Selected PID loop {}'.format(self.model.tracked_pid_loop))
 
 def main():
     args = parse_args()
