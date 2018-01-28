cf9ca30e2508efdd4acf0c7069b9b1777d96806e
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-01-28 21:20:35 +0100

    Live plot: Plot selected PID loop feedback

--- tools/studio/pid_plot.py
+++ tools/studio/pid_plot.py
@@ -31,7 +31,11 @@ class PidViewer(QtGui.QWidget):
 
         self.node_selector = Selector()
         layout.addWidget(self.node_selector)
-        self.plot = LivePlotter(buffer_size=30)
+
+        self.pid_loop_selector = Selector()
+        layout.addWidget(self.pid_loop_selector)
+
+        self.plot = LivePlotter(buffer_size=300)
         layout.addWidget(self.plot.widget)
 
         self.setLayout(layout)
@@ -39,20 +43,28 @@ class PidViewer(QtGui.QWidget):
         self.show()
 
 class PidPlotController:
-    def __init__(self, model, viewer):
+    def __init__(self, node):
         self.logger = logging.getLogger('PidPlotController')
-        self.model = model
-        self.viewer = viewer
+        self.node = node
+        self.model = NodeStatusMonitor(node)
+        self.viewer = PidViewer()
         self.curve = self.viewer.plot.getPort()
 
         self.data = self._empty_pid_data()
         self.node_to_plot = None
-        self.model.node.add_handler(uavcan.thirdparty.cvra.motor.feedback.CurrentPID, self._current_pid_callback)
+        self.node.add_handler(uavcan.thirdparty.cvra.motor.feedback.CurrentPID, self._current_pid_callback)
+        self.node.add_handler(uavcan.thirdparty.cvra.motor.feedback.VelocityPID, self._velocity_pid_callback)
+        self.node.add_handler(uavcan.thirdparty.cvra.motor.feedback.PositionPID, self._position_pid_callback)
 
         self.nodes = []
         self.model.set_on_new_node_callback(self.update_selection)
 
         self.viewer.node_selector.set_callback(self._change_selected_node)
+
+        self.pid_loops = ['Current', 'Velocity', 'Position']
+        self.pid_loop_to_plot = self.pid_loops[0]
+        self.viewer.pid_loop_selector.set_nodes(self.pid_loops)
+        self.viewer.pid_loop_selector.set_callback(self._change_selected_pid_loop)
 
         threading.Thread(target=self.run).start()
 
@@ -60,7 +72,7 @@ class PidPlotController:
         self.logger.info('PID widget started')
         while True:
             self.curve.put(self.data)
-            time.sleep(1)
+            time.sleep(0.2)
 
     def update_selection(self):
         self.logger.debug('New node detected, updating available nodes list')
@@ -70,21 +82,44 @@ class PidPlotController:
         self.viewer.node_selector.set_nodes(self.nodes)
 
     def _current_pid_callback(self, event):
-        node_id = event.transfer.source_node_id
-        node_name = self.model.node_id_to_name(node_id)
-        self.logger.debug('Received current PID feedback from node {} ({})'.format(node_name, node_id))
-        if self.node_to_plot is not None and self.node_to_plot == node_name:
-            self.data['setpoint']['time'] = np.append(self.data['setpoint']['time'], time.time())
-            self.data['measured']['time'] = np.append(self.data['measured']['time'], time.time())
+        if self.pid_loop_to_plot == 'Current':
+            self._pid_callback(node_id=event.transfer.source_node_id,
+                               setpoint=event.message.current_setpoint,
+                               measured=event.message.current)
 
-            self.data['setpoint']['value'] = np.append(self.data['setpoint']['value'], event.message.current_setpoint)
-            self.data['measured']['value'] = np.append(self.data['measured']['value'], event.message.current)
+    def _velocity_pid_callback(self, event):
+        if self.pid_loop_to_plot == 'Velocity':
+            self._pid_callback(node_id=event.transfer.source_node_id,
+                               setpoint=event.message.velocity_setpoint,
+                               measured=event.message.velocity)
+
+    def _position_pid_callback(self, event):
+        if self.pid_loop_to_plot == 'Position':
+            self._pid_callback(node_id=event.transfer.source_node_id,
+                               setpoint=event.message.position_setpoint,
+                               measured=event.message.position)
+
+    def _pid_callback(self, node_id, setpoint, measured):
+        node_name = self.model.node_id_to_name(node_id)
+
+        if self.node_to_plot is None or self.node_to_plot != node_name:
+            return
+
+        current_time = time.time()
+        self.data['setpoint']['time'] = np.append(self.data['setpoint']['time'], current_time)
+        self.data['measured']['time'] = np.append(self.data['measured']['time'], current_time)
+        self.data['setpoint']['value'] = np.append(self.data['setpoint']['value'], setpoint)
+        self.data['measured']['value'] = np.append(self.data['measured']['value'], measured)
 
     def _change_selected_node(self, i):
-        self.logger.debug('Current index {} selection changed to {}'.format(i, self.nodes[i]))
         self.node_to_plot = self.nodes[i]
         self.data = self._empty_pid_data()
         self.logger.info('Selected node {}'.format(self.node_to_plot))
+
+    def _change_selected_pid_loop(self, i):
+        self.pid_loop_to_plot = self.pid_loops[i]
+        self.data = self._empty_pid_data()
+        self.logger.info('Selected PID loop {}'.format(self.pid_loop_to_plot))
 
     def _empty_pid_data(self):
         return {
@@ -102,13 +137,13 @@ def main():
     args = parse_args()
     logging.basicConfig(level=max(logging.CRITICAL - (10 * args.verbose), 0))
 
-    uavcan.load_dsdl(args.dsdl)
-    node = UavcanNode(interface=args.interface, node_id=args.node_id)
-
     app = QtGui.QApplication(sys.argv)
     app.setFont(QtGui.QFont('Open Sans', pointSize=20))
 
-    controller = PidPlotController(model=NodeStatusMonitor(node), viewer=PidViewer())
+    uavcan.load_dsdl(args.dsdl)
+    node = UavcanNode(interface=args.interface, node_id=args.node_id)
+
+    controller = PidPlotController(node=node)
 
     node.spin()
 
