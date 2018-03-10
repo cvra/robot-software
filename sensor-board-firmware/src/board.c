ecde0b7a29576fd19a0e5a16c43b3013ac7bbfde
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-03-10 14:10:56 +0100

    Sensor board: blink LED and setup UAVCAN

---
+++ sensor-board-firmware/src/board.c
@@ -0,0 +1,55 @@
+/*
+    ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
+
+#include "hal.h"
+
+#if HAL_USE_PAL || defined(__DOXYGEN__)
+/**
+ * @brief   PAL setup.
+ * @details Digital I/O ports static configuration as defined in @p board.h.
+ *          This variable is used by the HAL when initializing the PAL driver.
+ */
+const PALConfig pal_default_config = {
+    {VAL_GPIOA_MODER, VAL_GPIOA_OTYPER, VAL_GPIOA_OSPEEDR, VAL_GPIOA_PUPDR,
+     VAL_GPIOA_ODR,   VAL_GPIOA_AFRL,   VAL_GPIOA_AFRH},
+    {VAL_GPIOB_MODER, VAL_GPIOB_OTYPER, VAL_GPIOB_OSPEEDR, VAL_GPIOB_PUPDR,
+     VAL_GPIOB_ODR,   VAL_GPIOB_AFRL,   VAL_GPIOB_AFRH},
+
+    /* All those ports are present in STM32F302 but not in our package
+     * (UFQFN32). */
+    {0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0},
+};
+#endif
+
+/**
+ * @brief   Early initialization code.
+ * @details This initialization must be performed just after stack setup
+ *          and before any other initialization.
+ */
+void __early_init(void)
+{
+    stm32_clock_init();
+}
+
+/**
+ * @brief   Board-specific initialization code.
+ * @todo    Add your board-specific code, if any.
+ */
+void boardInit(void)
+{
+}
---
+++ sensor-board-firmware/src/main.c
@@ -0,0 +1,57 @@
+#include <ch.h>
+#include <hal.h>
+#include "uavcan/node.h"
+#include "bootloader_config.h"
+#include "error/error.h"
+#include "debug.h"
+#include "main.h"
+
+THD_FUNCTION(blinker, arg)
+{
+    (void) arg;
+    while (1) {
+        palSetPad(GPIOA, GPIOA_PIN0);
+        chThdSleepMilliseconds(100);
+        palClearPad(GPIOA, GPIOA_PIN0);
+        chThdSleepMilliseconds(100);
+    }
+}
+
+static void blinker_start(void)
+{
+    static THD_WORKING_AREA(blinker_wa, 256);
+    chThdCreateStatic(blinker_wa, sizeof(blinker_wa), LOWPRIO, blinker, NULL);
+}
+
+void _unhandled_exception(void)
+{
+    chSysHalt("unhandled exception");
+
+    while (true) {
+        /* wait forever */
+    }
+}
+
+bootloader_config_t config;
+
+int main(void)
+{
+    halInit();
+    chSysInit();
+
+    debug_init();
+    NOTICE("boot");
+
+    blinker_start();
+
+    if (!config_get(&config)) {
+        chSysHalt("Cannot load config");
+    }
+
+    NOTICE("Board name=\"%s\", ID=%d", config.board_name, config.ID);
+
+    // Never returns
+    uavcan_start(10, "hello");
+
+    return 0;
+}
