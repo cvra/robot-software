3260a785926f0b1cc0252f4cd7b8a5c31116bd9a
Michael Spieler <michael.spieler@epfl.ch>
2018-03-15 22:44:59 +0100

    Sensor board: run at 72MHz

--- sensor-board-firmware/src/mcuconf.h
+++ sensor-board-firmware/src/mcuconf.h
@@ -45,7 +45,7 @@
 #define STM32_LSE_ENABLED                   FALSE
 #define STM32_SW                            STM32_SW_PLL
 #define STM32_PLLSRC                        STM32_PLLSRC_HSE
-#define STM32_PREDIV_VALUE                  8
+#define STM32_PREDIV_VALUE                  2
 #define STM32_PLLMUL_VALUE                  9
 #define STM32_HPRE                          STM32_HPRE_DIV1
 #define STM32_PPRE1                         STM32_PPRE1_DIV2
