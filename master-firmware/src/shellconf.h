ca79f9bf5818a599d2fca7ae104980afa5793ee4
Michael Spieler <michael.spieler@epfl.ch>
2018-03-18 20:17:03 +0100

    Use header for shell configuration

---
+++ master-firmware/src/shellconf.h
@@ -0,0 +1,13 @@
+#ifndef SHELLCONF_H
+#define SHELLCONF_H
+
+/* Shell related config */
+#define SHELL_MAX_ARGUMENTS             5
+#define SHELL_USE_HISTORY               TRUE
+#define SHELL_USE_ESC_SEQ               TRUE
+#define SHELL_MAX_HIST_BUFF             200
+
+#define SHELL_CMD_THREADS_ENABLED       FALSE
+#define SHELL_CMD_TEST_ENABLED          FALSE
+
+#endif /* SHELLCONF_H */
