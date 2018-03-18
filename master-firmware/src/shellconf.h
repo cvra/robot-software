fc5290f84e0eb3e2d93e796ef24687855a8a2ec5
Michael Spieler <michael.spieler@epfl.ch>
2018-03-18 20:18:47 +0100

    Increase max shell line length

--- master-firmware/src/shellconf.h
+++ master-firmware/src/shellconf.h
@@ -3,9 +3,10 @@
 
 /* Shell related config */
 #define SHELL_MAX_ARGUMENTS             5
+#define SHELL_MAX_LINE_LENGTH           100
 #define SHELL_USE_HISTORY               TRUE
 #define SHELL_USE_ESC_SEQ               TRUE
-#define SHELL_MAX_HIST_BUFF             200
+#define SHELL_MAX_HIST_BUFF             4*SHELL_MAX_LINE_LENGTH
 
 #define SHELL_CMD_THREADS_ENABLED       FALSE
 #define SHELL_CMD_TEST_ENABLED          FALSE
