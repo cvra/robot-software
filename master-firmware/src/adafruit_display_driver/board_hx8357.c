26020ca5d795480333592cfe90a5d52c07c7d36a
Antoine Albertelli <antoine@antoinealb.net>
2018-03-23 22:35:37 +0100

    Slow down SPI to be more stable

--- master-firmware/src/adafruit_display_driver/board_hx8357.c
+++ master-firmware/src/adafruit_display_driver/board_hx8357.c
@@ -16,7 +16,7 @@ void hx8357_init_board(void *g)
     (void) g;
     static SPIConfig spi_cfg = {
         .end_cb = NULL,
-        .cr1 = SPI_CR1_BR_0,
+        .cr1 = SPI_CR1_BR_1,
     };
     spiStart(&LCD_SPID, &spi_cfg);
 }
