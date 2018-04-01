131f3d7a56e133504ecc3149a5d0779953523ef4
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-04-01 15:49:04 +0200

    Move common enum to a pure C header file

---
+++ master-firmware/src/robot_helpers/eurobot2018.h
@@ -0,0 +1,13 @@
+#ifndef EUROBOT2018_H
+#define EUROBOT2018_H
+
+/** Blocks color */
+enum block_color {
+    BLOCK_YELLOW = 0,
+    BLOCK_GREEN,
+    BLOCK_BLUE,
+    BLOCK_RED,
+    BLOCK_BLACK,
+};
+
+#endif /* EUROBOT2018_H */
