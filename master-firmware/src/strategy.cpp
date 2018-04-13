59a1575c39f80601fa50961419b7c0d86599e516
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-04-13 17:22:58 +0200

    Reactivate wastetower goal for Order

--- master-firmware/src/strategy.cpp
+++ master-firmware/src/strategy.cpp
@@ -782,11 +782,11 @@ void strategy_order_play_game(enum strat_color_t color, RobotState& state)
     WaterTowerGoal watertower_goal;
     BuildTowerGoal build_tower_goal[2] = {BuildTowerGoal(0), BuildTowerGoal(1)};
     goap::Goal<RobotState>* goals[] = {
-        // &watertower_goal,
         &bee_goal,
         &pickup_cubes_goal,
         &build_tower_goal[0],
         &build_tower_goal[1],
+        &watertower_goal,
     };
 
     IndexArms index_arms;
