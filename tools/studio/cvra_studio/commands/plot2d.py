4cfbf2b863a5c262b1b72bb7d1a6bbe4d807fbac
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-07-14 23:32:59 +0200

    Plot beacon position on table received by UAVCAN

--- tools/studio/cvra_studio/commands/plot2d.py
+++ tools/studio/cvra_studio/commands/plot2d.py
@@ -2,18 +2,25 @@ import logging
 import sys
 import threading
 import time
+import uavcan
 
 import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 
+from ..network.UavcanNode import UavcanNode
 from ..viewers.LivePlotter2D import LivePlotter2D
+
 
 def argparser(parser=None):
     parser = parser or argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("interface", help="Serial port or SocketCAN interface")
+    parser.add_argument("--dsdl", "-d", help="DSDL path", required=True)
+    parser.add_argument("--node_id", "-n", help="UAVCAN Node ID", default=127)
     parser.add_argument('--verbose', '-v', action='count', default=0)
 
     return parser
+
 
 def make_cube(x, y, color, mirror):
     SIZE = 60
@@ -22,6 +29,7 @@ def make_cube(x, y, color, mirror):
     if mirror: offset['g'], offset['o'] = offset['o'], offset['g']
     return { 'x': x + offset[color][0], 'y': y + offset[color][1], 'a': 45, 'n': 4, 'r': RADIUS, 'fill': color }
 
+
 def make_cubes(name, x, y, mirror):
     return {name.format('_y'): make_cube(x, y, 'y', mirror),
             name.format('_k'): make_cube(x, y, 'k', mirror),
@@ -29,8 +37,10 @@ def make_cubes(name, x, y, mirror):
             name.format('_g'): make_cube(x, y, 'g', mirror),
             name.format('_o'): make_cube(x, y, 'o', mirror)}
 
+
 class Model:
-    def __init__(self):
+    def __init__(self, node):
+        self.node = node
         self.data = {
             'wastewater_g': { 'pts': [[894, 1750], [1500, 1750], [1500, 2000], [894, 2000]], 'fill': 'g' },
             'wastewater_o': { 'pts': [[1500, 1750], [2106, 1750], [2106, 2000], [1500, 2000]], 'fill': 'o' },
@@ -44,29 +54,28 @@ class Model:
         self.data.update(make_cubes('cube_2_g{}', 2700, 1190, True))
         self.data.update(make_cubes('cube_3_g{}', 1900, 1500, True))
 
-        threading.Thread(target=self.run).start()
+        self.node.add_handler(uavcan.thirdparty.cvra.uwb_beacon.TagPosition, self._callback)
+
+    def _callback(self, event):
+        print(event)
+        self.data.update({'robot': {
+            'x': 1000 * event.message.x,
+            'y': 1000 * event.message.y,
+            'a': 0,
+            'r': 150,
+            'n': 6,
+            'fill': 'cvra'
+            }})
 
     def get_data(self):
         return self.data
 
-    def run(self):
-        while True:
-            random_number = lambda: np.random.random(size=(1,1))[0][0]
-            self.data.update({'robot': {
-                'x': 3000 * random_number(),
-                'y': 2000 * random_number(),
-                'a': 2000 * random_number(),
-                'r': 150,
-                'n': 6,
-                'fill': 'cvra'
-                }})
-            time.sleep(0.1)
 
 class Controller:
-    def __init__(self):
+    def __init__(self, node):
         self.viewer = LivePlotter2D(size=(3000, 2000))
         self.curve = self.viewer.getPort()
-        self.model = Model()
+        self.model = Model(node)
         threading.Thread(target=self.run).start()
         self.viewer.widget.show()
 
@@ -75,14 +84,21 @@ class Controller:
             self.curve.put(self.model.get_data())
             time.sleep(1)
 
+
 def main(args):
     logging.basicConfig(level=max(logging.CRITICAL - (10 * args.verbose), 0))
     app = QtGui.QApplication(sys.argv)
 
-    plot_controller = Controller()
+    uavcan.load_dsdl(args.dsdl)
+    node = UavcanNode(interface=args.interface, node_id=args.node_id)
+
+    plot_controller = Controller(node)
+
+    node.spin()
 
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
+
 
 if __name__ == "__main__":
     args = argparser().parse_args()
