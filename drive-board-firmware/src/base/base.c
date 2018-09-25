638b5ca4567782c9562de076b1827ec497b4e37c
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-09-25 20:46:53 +0200

    Allow base to advance or rotate
    never both at the same time

--- drive-board-firmware/src/base/base.c
+++ drive-board-firmware/src/base/base.c
@@ -1,12 +1,39 @@
 #include "base.h"
 
+#include <math.h>
+
+static void base_advance(base_t* base, float speed);
+static void base_rotate(base_t* base, float speed);
 static void wheel_set_speed(wheel_t* wheel, float speed);
 static float clamp(float value, float max);
 
 void base_set_speed(base_t* base, float linear_x, float angular_z)
 {
-    float right_speed = 0.5f * (linear_x - angular_z);
-    float left_speed = 0.5f * (linear_x + angular_z);
+    if (fabsf(linear_x) >= fabsf(angular_z)) {
+        base_advance(base, linear_x);
+    } else {
+        base_rotate(base, angular_z);
+    }
+}
+
+void base_advance(base_t* base, float speed)
+{
+    float right_speed = speed;
+    float left_speed = speed;
+
+    wheel_set_speed(&base->right.back_wheel, right_speed);
+    wheel_set_speed(&base->right.center_wheel, right_speed);
+    wheel_set_speed(&base->right.front_wheel, right_speed);
+
+    wheel_set_speed(&base->left.back_wheel, left_speed);
+    wheel_set_speed(&base->left.center_wheel, left_speed);
+    wheel_set_speed(&base->left.front_wheel, left_speed);
+}
+
+void base_rotate(base_t* base, float speed)
+{
+    float right_speed = - speed;
+    float left_speed = speed;
 
     wheel_set_speed(&base->right.back_wheel, right_speed);
     wheel_set_speed(&base->right.center_wheel, right_speed);
