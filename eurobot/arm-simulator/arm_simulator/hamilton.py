0c65710332ece43bb393614b9d5251bc6fe0ffd2
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2019-01-19 18:52:14 +0100

    Simulate system in arm-simulator package
    Cleans up the example notebooks.
    Also enables real-time simulation and display

--- eurobot/arm-simulator/arm_simulator/hamilton.py
+++ eurobot/arm-simulator/arm_simulator/hamilton.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import autograd.numpy as np
 from autograd import grad, hessian, jacobian
 
@@ -25,13 +27,44 @@ class System:
         self.grad_U = grad(U)
 
     """
+    Reset the position and velocity
+    """
+    def reset(self, q, qdot):
+        self.q = q
+        self.p = self.K(q) @ qdot
+
+    """
     Inertia matrix expressed in generalized coordinates
     """
     def K(self, q):
         return self.J_f(q).T @ self.M @ self.J_f(q)
 
+    """
+    Time-derivative of the generalized coordinates
+    """
     def q_dot(self, p, q):
         return np.linalg.inv(self.K(q)) @ p
 
+    """
+    Time-derivative of the conjugate momenta
+    """
     def p_dot(self, p, q):
         return p.T @ np.linalg.inv(self.K(q)) @ self.H_f(q).T @ self.M @ self.J_f(q) @ np.linalg.inv(self.K(q)) @ p - self.grad_U(q)
+
+    """
+    Advance the system through the given time step
+    Returns the resulting generalized coordinates and conjugate momenta
+    """
+    def step(self, dt):
+        dq = self.q_dot(self.p, self.q)
+        self.q += dt * dq
+
+        dp = self.p_dot(self.p, self.q)
+        self.p += dt * dp
+
+    """
+    Query the current state of the system, returns generalized coordinate q
+    and conjugate momenta p
+    """
+    def state(self):
+        return deepcopy(self.p), deepcopy(self.q)
