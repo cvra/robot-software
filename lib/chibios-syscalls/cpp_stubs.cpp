4e14abf1b876b768345ccae63e32c3944953f9f3
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2019-03-09 17:11:49 +0100

    Split C++ stubs for readability

--- lib/chibios-syscalls/cpp_stubs.cpp
+++ lib/chibios-syscalls/cpp_stubs.cpp
@@ -22,34 +22,6 @@ void __attribute__((weak)) __cxa_pure_virtual()
     osalSysHalt("Pure virtual function call.");
 }
 
-#ifndef CVRA_NO_DYNAMIC_ALLOCATION
-# define CVRA_NO_DYNAMIC_ALLOCATION 0
-#endif
-
-#if CVRA_NO_DYNAMIC_ALLOCATION
-void* operator new(size_t)
-{
-    osalSysHalt("operator new");
-    return reinterpret_cast<void*>(0xFFFFFFFF);
-}
-
-void* operator new[](size_t)
-{
-    osalSysHalt("operator new[]");
-    return reinterpret_cast<void*>(0xFFFFFFFF);
-}
-
-void operator delete(void*)
-{
-    osalSysHalt("operator delete");
-}
-
-void operator delete[](void*)
-{
-    osalSysHalt("operator delete[]");
-}
-#endif /* CVRA_NO_DYNAMIC_ALLOCATION */
-
 void __attribute__((weak)) _exit(int status)
 {
     (void)status;
