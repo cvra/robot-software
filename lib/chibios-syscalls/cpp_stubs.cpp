e83efd0ef8b5b0929b94e10b7a95c3bed08aad6b
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2019-03-09 17:10:24 +0100

    Make C++ stubs definitions weak
    Allows overriding, for example UWB firmware
    uses syscall definitions from ChibiOS for some
    of the calls, so we can allow that

--- lib/chibios-syscalls/cpp_stubs.cpp
+++ lib/chibios-syscalls/cpp_stubs.cpp
@@ -11,13 +11,13 @@ int __cxa_guard_acquire(__guard*);
 void __cxa_guard_release(__guard*);
 void __cxa_guard_abort(__guard*);
 void __cxa_pure_virtual();
-void* __dso_handle = NULL;
+void* __attribute__((weak)) __dso_handle = NULL;
 void _exit(int status);
 pid_t _getpid(void);
 int _kill(int pid, int sig);
 }
 
-void __cxa_pure_virtual()
+void __attribute__((weak)) __cxa_pure_virtual()
 {
     osalSysHalt("Pure virtual function call.");
 }
@@ -50,7 +50,7 @@ void operator delete[](void*)
 }
 #endif /* CVRA_NO_DYNAMIC_ALLOCATION */
 
-void _exit(int status)
+void __attribute__((weak)) _exit(int status)
 {
     (void)status;
     osalSysHalt("Unrealized");
@@ -58,14 +58,14 @@ void _exit(int status)
     }
 }
 
-pid_t _getpid(void)
+pid_t __attribute__((weak)) _getpid(void)
 {
     return 1;
 }
 
 #undef errno
 extern int errno;
-int _kill(int pid, int sig)
+int __attribute__((weak)) _kill(int pid, int sig)
 {
     (void)pid;
     (void)sig;
