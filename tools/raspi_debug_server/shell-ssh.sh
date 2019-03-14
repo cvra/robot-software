3146421aef1955002c14fadc708978941dc5f1d1
Lu-ni <lucas.nicollier@gmail.com>
2019-03-14 21:41:56 +0100

    change ip

--- tools/raspi_debug_server/shell-ssh.sh
+++ tools/raspi_debug_server/shell-ssh.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh-copy-id pi@192.168.1.129 && ssh pi@192.168.1.129 -t "python3 -m serial.tools.miniterm /dev/serial/by-id/usb-CVRA_ChibiOS_RT_Virtual_COM_Port_master-if00"
+ssh-copy-id pi@192.168.1.202 && ssh pi@192.168.1.202 -t "python3 -m serial.tools.miniterm /dev/serial/by-id/usb-CVRA_ChibiOS_RT_Virtual_COM_Port_master-if00"
