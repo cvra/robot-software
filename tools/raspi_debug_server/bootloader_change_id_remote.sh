9a4a6c564971d70c853ca0794ac211c543e0722b
Lu-ni <lucas.nicollier@gmail.com>
2019-03-19 10:36:14 +0100

    add remote script for all bootloader_*(192.168.1.201)

---
+++ tools/raspi_debug_server/bootloader_change_id_remote.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if ssh-keygen -F 192.168.1.201 >/dev/null;
+then echo SSH key found;
+else ssh-copy-id pi@192.168.1.201 >/dev/null
+fi
+ssh pi@192.168.1.201 -t /home/pi/.local/bin/bootloader_change_id $@
