ad5fa5aee437107d797f65483e14f21cc309b073
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2019-03-23 20:57:14 +0100

    Add script to log UDP protobuf stream
    Imported from hydrocontest firmware

---
+++ tools/log_udp_protobuf/log_udp_protobuf.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""
+Logs traffic coming on protobuf from the various topics.
+"""
+
+import sys
+import os.path
+import argparse
+import socketserver
+import re
+
+from google.protobuf import text_format
+
+import messages
+
+
+def parse_packet(data):
+    messagesize_size = len(messages.MessageSize(bytes=0).SerializeToString())
+
+    header_size = messages.MessageSize()
+    header_size.ParseFromString(data[0:messagesize_size])
+
+    cut_point = messagesize_size + header_size.bytes
+    header_data, msg_data = data[:cut_point], data[cut_point:]
+
+    header = messages.TopicHeader()
+    header.ParseFromString(header_data[messagesize_size:])
+
+    for m in messages.messages:
+        if messages.msgid(m) == header.msgid:
+            msg = m()
+            msg.ParseFromString(msg_data)
+            break
+    else:
+        msg = None
+
+    return header, msg
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--port", "-p", default=10000, help="Port to listen on (10000)")
+    parser.add_argument(
+        "--filter", "-f", help="Filter regexp to apply on the topic name")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    if args.filter:
+        topic_filter = re.compile(args.filter, flags=re.IGNORECASE)
+    else:
+        topic_filter = re.compile(".*")
+
+    class Handler(socketserver.BaseRequestHandler):
+        def handle(self):
+            data = self.request[0]
+            header, msg = parse_packet(data)
+
+            if not topic_filter.search(header.name):
+                return
+
+            print("=" * 5)
+            print("topic: '{}'".format(header.name))
+            print("type: {}".format(msg.DESCRIPTOR.name))
+            print("data:")
+            print(text_format.MessageToString(msg, indent=2))
+
+    with socketserver.UDPServer(("0.0.0.0", args.port), Handler) as server:
+        server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tools/log_udp_protobuf/messages.py
@@ -0,0 +1,44 @@
+"""
+Loads all protobuf messages for the project
+"""
+
+import os.path
+import glob
+import importlib
+import sys
+from google.protobuf.message import Message
+
+msgdir = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                      '..', '..', 'master-firmware', 'build', 'protobuf')
+nanopbdir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', '..',
+                         'lib', 'nanopb', 'nanopb', 'generator', 'proto')
+
+modules = glob.glob(os.path.join(msgdir, '*.py'))
+
+
+sys.path.insert(0, msgdir)
+sys.path.insert(0, nanopbdir)
+
+import nanopb_pb2 as nanopb
+
+messages = []
+
+for f in modules:
+    if not os.path.isfile(f):
+        continue
+
+    if f.endswith('__init__.py'):
+        continue
+
+    name = os.path.basename(f)[:-3]
+
+    mod = importlib.import_module(name)
+
+    for k, v in vars(mod).items():
+        if isinstance(v, type) and issubclass(v, Message):
+            globals()[k] = v
+            messages.append(v)
+
+
+def msgid(msg):
+    return msg.DESCRIPTOR.GetOptions().Extensions[nanopb.nanopb_msgopt].msgid
