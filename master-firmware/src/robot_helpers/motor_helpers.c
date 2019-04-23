abf3a90e77b12d40824eb14f94d280aad5e6ca06
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2019-04-24 00:57:24 +0200

    Set torque once while indexing a single joint

--- master-firmware/src/robot_helpers/motor_helpers.c
+++ master-firmware/src/robot_helpers/motor_helpers.c
@@ -6,14 +6,14 @@
 // By how many radians we overshoot the index, in order to detect the hysteresis
 static const float MOTOR_INDEX_ANGLE_OVERSHOOT = 0.6; // in rad
 
-float motor_wait_for_index(motor_driver_t* motor, float motor_speed)
+float motor_wait_for_index(motor_driver_t* motor, float torque)
 {
     uint32_t index_count = motor->stream.value_stream_index_update_count;
 
     NOTICE("Moving axis...");
+    motor_driver_set_torque(motor, torque);
 
     while (motor->stream.value_stream_index_update_count == index_count) {
-        motor_driver_set_torque(motor, motor_speed);
 #ifndef TESTS
         chThdSleepMilliseconds(10);
 #endif
@@ -24,20 +24,20 @@ float motor_wait_for_index(motor_driver_t* motor, float motor_speed)
     return motor_driver_get_and_clear_stream_value(motor, MOTOR_STREAM_INDEX);
 }
 
-float motor_auto_index_sym(motor_driver_t* motor, int motor_dir, float motor_speed)
+float motor_auto_index_sym(motor_driver_t* motor, int motor_dir, float torque)
 {
-    float index_rising = motor_wait_for_index(motor, -motor_dir * motor_speed);
+    float index_rising = motor_wait_for_index(motor, -motor_dir * torque);
 
     NOTICE("Seen index for first time at %.4f!", index_rising);
 #ifndef TESTS
-    chThdSleepMilliseconds(1000.f * MOTOR_INDEX_ANGLE_OVERSHOOT / motor_speed);
+    chThdSleepMilliseconds(1000.f * MOTOR_INDEX_ANGLE_OVERSHOOT / torque);
 #endif
 
-    float index_falling = motor_wait_for_index(motor, motor_dir * motor_speed);
+    float index_falling = motor_wait_for_index(motor, motor_dir * torque);
 
     NOTICE("Seen index for second time at %.4f!", index_falling);
 #ifndef TESTS
-    chThdSleepMilliseconds(1000.f * MOTOR_INDEX_ANGLE_OVERSHOOT / motor_speed);
+    chThdSleepMilliseconds(1000.f * MOTOR_INDEX_ANGLE_OVERSHOOT / torque);
 #endif
 
     motor_driver_set_velocity(motor, 0);
@@ -46,7 +46,7 @@ float motor_auto_index_sym(motor_driver_t* motor, int motor_dir, float motor_spe
     return index_avg;
 }
 
-float motor_auto_index(const char* motor_name, int motor_dir, float motor_speed)
+float motor_auto_index(const char* motor_name, int motor_dir, float torque)
 {
     // get motor driver
     motor_driver_t* motor = bus_enumerator_get_driver(motor_manager.bus_enumerator, motor_name);
@@ -55,7 +55,7 @@ float motor_auto_index(const char* motor_name, int motor_dir, float motor_speed)
     }
 
     // move until index is found
-    float index = motor_wait_for_index(motor, motor_dir * motor_speed);
+    float index = motor_wait_for_index(motor, motor_dir * torque);
     NOTICE("Seen index at %.4f!", index);
 
     return index;
