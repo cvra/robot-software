d952115f6aac4aedcf87c8349a9ef5e3096971cc
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2019-04-24 20:10:27 +0200

    Move motor helpers to helpers folder
    I don't like helper files/folders either,
    but there's no time for caution

--- master-firmware/src/manipulator/hw.cpp
+++ master-firmware/src/manipulator/hw.cpp
@@ -3,29 +3,14 @@
 #include <error/error.h>
 
 #include "main.h"
-
-namespace {
-motor_driver_t* get_motor_driver(const char* name)
-{
-    motor_driver_t* motor = (motor_driver_t*)bus_enumerator_get_driver(motor_manager.bus_enumerator, name);
-    if (motor == NULL) {
-        ERROR("Motor \"%s\" doesn't exist", name);
-    }
-    return motor;
-}
-
-float read_motor_position(const char* name)
-{
-    return motor_driver_get_and_clear_stream_value(get_motor_driver(name), MOTOR_STREAM_POSITION);
-}
-} // namespace
+#include "robot_helpers/motor_helpers.h"
 
 namespace manipulator {
 Angles System::measure_feedback() const
 {
     Angles angles;
     for (size_t i = 0; i < 3; i++) {
-        angles[i] = directions[i] * read_motor_position(motors[i]) - offsets[i];
+        angles[i] = directions[i] * motor_get_position(motors[i]) - offsets[i];
     }
 
     // axis are decoupled
