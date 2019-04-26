51c92987df76638b882ff2679a54f1e287bd0121
Antoine Albertelli <antoine@antoinealb.net>
2019-04-27 01:10:01 +0200

    Add locks around sprintf

---
+++ lib/chibios-syscalls/stdio_lock.c
@@ -0,0 +1,20 @@
+#include <ch.h>
+#include <hal.h>
+
+#include "stdio_lock.h"
+
+static MUTEX_DECL(lock);
+
+#if !CH_CFG_USE_MUTEXES_RECURSIVE
+#error "stdio lock must be recursive, please enable CH_CFG_USE_MUTEXES_RECURSIVE"
+#endif
+
+void stdio_lock(void)
+{
+    chMtxLock(&lock);
+}
+
+void stdio_unlock(void)
+{
+    chMtxUnlock(&lock);
+}
---
+++ lib/chibios-syscalls/stdio_lock.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void stdio_lock(void);
+void stdio_unlock(void);
+
+#ifdef __cplusplus
+}
+#endif
