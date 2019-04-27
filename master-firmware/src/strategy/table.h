706083aac66ad54a28abe95ae82ecf1c9c6e86e0
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2019-04-27 23:38:28 +0200

    Define positions and orientations of 6 more pucks

--- master-firmware/src/strategy/table.h
+++ master-firmware/src/strategy/table.h
@@ -3,16 +3,31 @@
 
 #include "protobuf/strategy.pb.h"
 
+enum PuckOrientiation {
+    PuckOrientiation_HORIZONTAL = 0,
+    PuckOrientiation_VERTICAL,
+};
+
 struct Puck {
     PuckColor color;
+    PuckOrientiation orientation;
     float pos_x_mm;
     float pos_y_mm;
 };
 
 static const Puck pucks[] = {
-    {PuckColor_RED, 500, 450},
-    {PuckColor_RED, 500, 750},
-    {PuckColor_GREEN, 500, 1050},
+    /* in front of start zone */
+    {PuckColor_RED, PuckOrientiation_HORIZONTAL, 500, 450},
+    {PuckColor_RED, PuckOrientiation_HORIZONTAL, 500, 750},
+    {PuckColor_GREEN, PuckOrientiation_HORIZONTAL, 500, 1050},
+
+    /* distributor of 6 */
+    {PuckColor_RED, PuckOrientiation_VERTICAL, 500, 1543},
+    {PuckColor_GREEN, PuckOrientiation_VERTICAL, 600, 1543},
+    {PuckColor_RED, PuckOrientiation_VERTICAL, 700, 1543},
+    {PuckColor_BLUE, PuckOrientiation_VERTICAL, 800, 1543},
+    {PuckColor_RED, PuckOrientiation_VERTICAL, 900, 1543},
+    {PuckColor_GREEN, PuckOrientiation_VERTICAL, 1000, 1543},
 };
 
 struct DepositArea {
