2a2a2f478df43791d164c860660ca6d985cc53f8
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2019-05-05 02:54:30 +0200

    Make strategy simulator run on PC

---
+++ strategy_simulator/msgbus_port.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <pthread.h>
+
+#define PB_MSGID 1
+
+#include <pb.h>
+#include <msgbus/messagebus.h>
+
+#include <unistd.h>
+#include <stdbool.h>
+
+extern "C" {
+
+typedef struct {
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+} condvar_wrapper_t;
+
+typedef struct {
+    const pb_field_t* fields;
+    uint32_t msgid;
+    messagebus_watcher_t udp_watcher;
+} topic_metadata_t;
+
+#define TOPIC_DECL(name, type)                 \
+    struct {                                   \
+        messagebus_topic_t topic;              \
+        pthread_mutex_t lock;                  \
+        condvar_wrapper_t condvar;             \
+        type value;                            \
+        topic_metadata_t metadata;             \
+    } name = {                                 \
+        _MESSAGEBUS_TOPIC_DATA(name.topic,     \
+                               name.lock,      \
+                               name.condvar,   \
+                               &name.value,    \
+                               sizeof(type),   \
+                               name.metadata), \
+        name.lock,                             \
+        name.condvar,                          \
+        type##_init_default,                   \
+        {                                      \
+            type##_fields,                     \
+            type##_msgid,                      \
+            {NULL, NULL},                      \
+        },                                     \
+    }
+
+#define _MESSAGEBUS_TOPIC_DATA(topic, lock, condvar, buffer, buffer_size, metadata) \
+    {                                                                               \
+        buffer, buffer_size, &lock, &condvar, "", 0, NULL, NULL, &metadata,         \
+    }
+
+/* Wraps the topic information in a header (in protobuf format) to be sent over
+ * UDP or logged to disk or whatever.
+ *
+ * The scratch buffer must be big enough to hold the topic's content.
+ *
+ * @return The message size in bytes, or zero if there was an error.
+ */
+size_t messagebus_encode_topic_message(messagebus_topic_t* topic,
+                                       uint8_t* buf,
+                                       size_t buf_len,
+                                       uint8_t* scratch,
+                                       size_t scratch_len);
+
+/** Takes a topic information with a header and injects it into the
+ * corresponding topic.
+ *
+ * @warning This functions is not thread safe due to the use of a static buffer.
+ */
+void messagebus_inject_encoded_message(messagebus_t* bus, uint8_t* buf, size_t len);
+}
