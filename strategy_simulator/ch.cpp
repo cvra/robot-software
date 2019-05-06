8ea88587a80df07be598c1676f7cb4a59ab7cf40
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2019-05-06 21:24:25 +0200

    Add missing ChibiOS mock files, oups!

---
+++ strategy_simulator/ch.cpp
@@ -0,0 +1,19 @@
+#include <ch.h>
+
+void chMtxObjectInit(mutex_t* mp)
+{
+    (void)mp;
+}
+
+void chMtxLock(mutex_t* lock)
+{
+}
+
+void chMtxUnlock(mutex_t* lock)
+{
+}
+
+void chThdSleepMilliseconds(int milliseconds)
+{
+    (void)milliseconds;
+}
