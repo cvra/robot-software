cf4385f8258b0690c2408ba65ee21e68c4824ab8
Lu-ni <lucas.nicollier@gmail.com>
2019-05-10 13:44:51 +0200

    remove take two puck

--- master-firmware/src/strategy_impl/game.h
+++ master-firmware/src/strategy_impl/game.h
@@ -107,7 +107,6 @@ const int MAX_GOAP_PATH_LEN = 10;
     goap::Action<RobotState>* actions[] = {             \
         &index_arms,                                    \
         &retract_arms,                                  \
-        &take_two_pucks,                                \
         &take_pucks_right[0],                           \
         &take_pucks_right[1],                           \
         &take_pucks_right[2],                           \
