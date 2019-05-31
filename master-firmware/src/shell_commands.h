a1fc333df398e0e7b790848fb23b38966deb575a
Michael Spieler <michael.spieler@epfl.ch>
2019-05-31 23:43:41 +0200

    Enforce shell command order by sorting

--- master-firmware/src/shell_commands.h
+++ master-firmware/src/shell_commands.h
@@ -5,13 +5,13 @@
 
 /* Must define shell_commands final element */
 #define SHELL_COMMAND_END() \
-__attribute__((section(".shell_commands_end"))) \
+__attribute__((section(".shell_commands.1"))) \
 const ShellCommand __cmd_final = {NULL, NULL}
 
 #define SHELL_COMMAND(NAME, CHP, ARGC, ARGV) \
 static void __cmd_##NAME (BaseSequentialStream *, int, char *[]); \
 /* shell command list entry */ \
-__attribute__((section(".shell_commands.entry."#NAME))) \
+__attribute__((section(".shell_commands.0."#NAME))) \
 const ShellCommand __cmd_##NAME##_entry = {#NAME, __cmd_##NAME}; \
 /* shell command function */ \
 static void __cmd_##NAME (BaseSequentialStream *CHP, int ARGC, char *ARGV[])
