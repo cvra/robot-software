e3ed34fbe5022fda0e7892bd151f5ac2870be7e0
Antoine Albertelli <antoinealb@google.com>
2019-07-06 15:05:29 +0200

    Import argument support macros from google/benchmark

--- cmake/cvra.cmake
+++ cmake/cvra.cmake
@@ -1,3 +1,5 @@
+include(AddCXXCompilerFlag)
+include(AddCCompilerFlag)
 
 # Generate a test exec given test sources and their dependencies
 # Called like cvra_add_test(TARGET foo_test SOURCES foo_test.cpp DEPENDENCIES foo_lib)
@@ -14,3 +16,8 @@ function(cvra_add_test)
         add_test(${TEST_EXECUTABLE_TARGET} ${TEST_EXECUTABLE_TARGET})
     endif()
 endfunction()
+
+macro(add_c_cxx_compiler_flag TARGET)
+    add_c_compiler_flag(${TARGET})
+    add_cxx_compiler_flag(${TARGET})
+endmacro()
