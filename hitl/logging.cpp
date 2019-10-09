6e624ed261e314aa57d6e80d353160cee8c4e692
Antoine Albertelli <antoinealb@google.com>
2019-10-09 18:10:07 +0200

    Make logging thread safe

--- hitl/logging.cpp
+++ hitl/logging.cpp
@@ -2,16 +2,24 @@
 #include <cstdarg>
 #include "error/error.h"
 #include "absl/flags/flag.h"
+#include "absl/synchronization/mutex.h"
 
 ABSL_FLAG(bool, verbose, false, "Enable verbose logging.");
+static ABSL_CONST_INIT absl::Mutex logging_lock(absl::kConstInit);
 
 static void log_message(struct error* e, ...)
 {
+    absl::MutexLock l(&logging_lock);
     va_list va;
     va_start(va, e);
     printf("%s ", error_severity_get_name(e->severity));
     vprintf(e->text, va);
+    printf("\n");
     va_end(va);
+
+    if (e->severity == ERROR_SEVERITY_ERROR) {
+        exit(1);
+    }
 }
 
 void logging_init()
