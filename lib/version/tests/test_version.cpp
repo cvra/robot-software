0c76103817f4b59d65797cdabfc7d5d72a2cdf6b
Antoine Albertelli <antoinealb@google.com>
2019-12-17 20:16:10 +0100

    Port version to CMakeLists

---
+++ lib/version/tests/test_version.cpp
@@ -0,0 +1,14 @@
+#include <CppUTest/TestHarness.h>
+#include <string>
+#include "version/version.h"
+
+TEST_GROUP(VersionTestGroup)
+{
+
+};
+
+TEST(VersionTestGroup, GitCommitIsCorrectLength)
+{
+    std::string software_version{software_version_str};
+    CHECK_TRUE(0 <  software_version.length());
+}
--- lib/version/version.sh
+++ lib/version/version.sh
@@ -4,35 +4,37 @@
 set -e
 set -u
 
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
-    echo "Usage: $0 destination.c"
+    echo "Usage: $0 board_name destination.c"
     exit 1
 fi
 
-echo "// AUTOMATICALLY GENERATED" > $1.new
-echo "#include <board.h>" >> $1.new
-echo "#include <stdint.h>" >> $1.new
-echo "const char * software_version_str = \"$(git rev-parse HEAD)-$(git rev-parse --abbrev-ref HEAD)\";" >> $1.new
-echo "const uint32_t software_version_short = 0x$(git rev-parse --short HEAD);" >> $1.new
-echo "const char * hardware_version_str = BOARD_NAME;" >> $1.new
+dst=$2
+board_name=$1
+
+echo "// AUTOMATICALLY GENERATED" > $dst.new
+echo "#include <stdint.h>" >> $dst.new
+echo "const char * software_version_str = \"$(git rev-parse HEAD)-$(git rev-parse --abbrev-ref HEAD)\";" >> $dst.new
+echo "const uint32_t software_version_short = 0x$(git rev-parse --short HEAD);" >> $dst.new
+echo "const char * hardware_version_str = \"$board_name\";" >> $dst.new
 
 # If version.c does not exist, overwrite it
-if [ ! -e $1 ]
+if [ ! -e $dst ]
 then
-    echo "$1 does not exist, creating it..."
-    mv $1.new $1
+    echo "$dst does not exist, creating it..."
+    mv $dst.new $dst
 else
     # If there is a version.c, check if the file should change before
     # overriding it.
     # If we don't do this, make will detect a new file and always relink the
     # project.
-    if cmp --quiet $1 $1.new
+    if cmp --quiet $dst $dst.new
     then
-        echo "No change detected in $1 skipping..."
-        rm $1.new
+        echo "No change detected in $dst skipping..."
+        rm $dst.new
     else
-        echo "$1 changed, overwriting it"
-        mv $1.new $1
+        echo "$dst changed, overwriting it"
+        mv $dst.new $dst
     fi
 fi
