4b101966801d6659e21155603a3f983d9db0f26c
Antoine Albertelli <antoinealb@google.com>
2020-01-24 22:46:18 +0100

    Wrap calibration tool in proper exe

--- computer-vision/tools/calibrate.py
+++ computer-vision/tools/calibrate.py
@@ -1,43 +1,59 @@
+#!/usr/bin/env python3
+"""
+Computes the calibration matrix of a camera given a number of sample images.
+"""
+import argparse
 import numpy as np
 import cv2
-import glob
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("images", nargs="+", required=True, help="Images to process, in jpg format.")
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((9*6,3), np.float32)
-objp[:,:2] = np.mgrid[0:6,0:9].T.reshape(-1,2)
+    return parser.parse_args()
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+def main():
+    args = parse_args()
 
-images = glob.glob('*.jpg')
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-gray = []
-for fname in images:
-    img = cv2.imread(fname, cv2.IMREAD_COLOR)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((9*6,3), np.float32)
+    objp[:,:2] = np.mgrid[0:6,0:9].T.reshape(-1,2)
 
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (6,9),None)
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
+    gray = []
+    for fname in args.images:
+        img = cv2.imread(fname, cv2.IMREAD_COLOR)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        imgpoints.append(corners2)
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (6,9),None)
 
-        # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (6,9), corners2,ret)
-        cv2.imshow('img',img)
-        cv2.waitKey(500)
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
 
-cv2.destroyAllWindows()
+            corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+            imgpoints.append(corners2)
 
-err, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+            # Draw and display the corners
+            img = cv2.drawChessboardCorners(img, (6,9), corners2,ret)
+            cv2.imshow('img',img)
+            cv2.waitKey(500)
 
-print('cameraMatrix\n', mtx)
-print('distCoeffs\n', dist)
+    cv2.destroyAllWindows()
+
+    err, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+
+    print('cameraMatrix\n', mtx)
+    print('distCoeffs\n', dist)
+
+if __name__ == '__main__':
+    main()
+
+
