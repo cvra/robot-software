a10810b4cc6adb46bdce9374a8d4178c005030f6
Michael Spieler <mspieler101@gmail.com>
2020-01-26 00:44:33 +0100

    main in grab_image

--- computer-vision/tools/grab_image.py
+++ computer-vision/tools/grab_image.py
@@ -1,27 +1,31 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+def main():
+    cap = cv2.VideoCapture(1)
 
-n = 0
-while(n < 10):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    n = 0
+    while(n < 10):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    k = cv2.waitKey(1)
-    if k & 0xFF == ord(' '):
-        cv2.imwrite(f'camera_calibration_{n}.jpg',gray)
-        n += 1
-        
-    if k & 0xFF == ord('q'):
-        break
+        # Display the resulting frame
+        cv2.imshow('frame',gray)
+        k = cv2.waitKey(1)
+        if k & 0xFF == ord(' '):
+            cv2.imwrite(f'camera_calibration_{n}.jpg',gray)
+            n += 1
+            
+        if k & 0xFF == ord('q'):
+            break
 
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
