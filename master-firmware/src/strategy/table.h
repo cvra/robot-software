f47fa67a4b3a478c6c6dc4e488ab31a5267440d0
Lu-ni <lucas.nicollier@gmail.com>
2020-01-25 17:49:38 +0100

    add cup to table.h

--- master-firmware/src/strategy/table.h
+++ master-firmware/src/strategy/table.h
@@ -3,6 +3,53 @@
 
 #include "protobuf/strategy.pb.h"
 
-// TODO(all): Write the table content here
+const uint32_t MAX_PUCKS_IN_SCALE = 6;
 
+typedef enum {
+    CupColor_GREEN = 0,
+    CupColor_RED,
+    CupColor_GREEN_OR_RED
+} CupColor;
+
+typedef enum {
+    CupOrientiation_UP = 0,
+    CupOrientiation_DOWN,
+} CupOrientiation;
+
+typedef struct {
+    CupColor color;
+    CupOrientiation orientation;
+    float pos_x_mm;
+    float pos_y_mm;
+} Cup;
+
+static const Cup cups[] = {
+    /* in front of start zone */
+    {CupColor_RED, CupOrientiation_DOWN, 300, 400},
+    {CupColor_GREEN, CupOrientiation_DOWN, 450, 510},
+    {CupColor_RED, CupOrientiation_DOWN, 450, 1080},
+    {CupColor_GREEN, CupOrientiation_DOWN, 300, 1200},
+    /* diagonal on our side */
+    {CupColor_RED, CupOrientiation_DOWN, 670, 100},
+    {CupColor_GREEN, CupOrientiation_DOWN, 950, 400},
+    {CupColor_RED, CupOrientiation_DOWN, 1100, 800},
+    {CupColor_GREEN, CupOrientiation_DOWN, 1270, 1200},
+    /* Our distributor */
+    {CupColor_RED, CupOrientiation_UP, -67, 1450},
+    {CupColor_GREEN, CupOrientiation_UP, -67, 1525},
+    {CupColor_RED, CupOrientiation_UP, -67, 1600},
+    {CupColor_GREEN, CupOrientiation_UP, -67, 1675},
+    {CupColor_RED, CupOrientiation_UP, -67, 1750},
+    /* Mixed distrubutor on our side*/
+    {CupColor_GREEN_OR_RED, CupOrientiation_UP, 700, -67},
+    {CupColor_GREEN_OR_RED, CupOrientiation_UP, 775, -67},
+    {CupColor_GREEN_OR_RED, CupOrientiation_UP, 850, -67},
+    {CupColor_GREEN_OR_RED, CupOrientiation_UP, 925, -67},
+    {CupColor_GREEN_OR_RED, CupOrientiation_UP, 1000, -67},
+    /*Rocky area*/
+    {CupColor_GREEN, CupOrientiation_DOWN, 1065, 1650},
+    {CupColor_RED, CupOrientiation_DOWN, 1005, 1955},
+    {CupColor_RED, CupOrientiation_DOWN, 1335, 1650},
+    {CupColor_GREEN, CupOrientiation_DOWN, 1395, 1995},
+};
 #endif /* STRATEGY_TABLE_H */
