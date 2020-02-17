4936f281194582c9f5f3217bd282c48b833bfaa4
Lu-ni <lucas.nicollier@gmail.com>
2020-02-17 08:57:45 +0100

    change name to use the same standar as 52f0fa18c06c0

--- master-firmware/src/strategy/table.h
+++ master-firmware/src/strategy/table.h
@@ -3,53 +3,53 @@
 
 #include "protobuf/strategy.pb.h"
 
-// Cup color for the green side, the yellow side is reversed
+// Glass color for the green side, the yellow side is reversed
 
 typedef enum {
-    CupColor_GREEN = 0,
-    CupColor_RED,
-    CupColor_GREEN_OR_RED
-} CupColor;
+    GlassColor_UNKNOWN = 0,
+    GlassColor_GREEN,
+    GlassColor_RED
+} GlassColor;
 
 typedef enum {
-    CupOrientiation_UP = 0,
-    CupOrientiation_DOWN,
-} CupOrientiation;
+    GlassOrientiation_UP = 0,
+    GlassOrientiation_DOWN,
+} GlassOrientiation;
 
 typedef struct {
-    CupColor color;
-    CupOrientiation orientation;
+    GlassColor color;
+    GlassOrientiation orientation;
     float pos_x_mm;
     float pos_y_mm;
-} Cup;
+} Glass;
 
-static const Cup cups[] = {
+static const Glass Glasss[] = {
     /* in front of start zone */
-    {CupColor_RED, CupOrientiation_DOWN, 300, 400},
-    {CupColor_GREEN, CupOrientiation_DOWN, 450, 510},
-    {CupColor_RED, CupOrientiation_DOWN, 450, 1080},
-    {CupColor_GREEN, CupOrientiation_DOWN, 300, 1200},
+    {GlassColor_RED, GlassOrientiation_DOWN, 300, 400},
+    {GlassColor_GREEN, GlassOrientiation_DOWN, 450, 510},
+    {GlassColor_RED, GlassOrientiation_DOWN, 450, 1080},
+    {GlassColor_GREEN, GlassOrientiation_DOWN, 300, 1200},
     /* diagonal on our side */
-    {CupColor_RED, CupOrientiation_DOWN, 670, 100},
-    {CupColor_GREEN, CupOrientiation_DOWN, 950, 400},
-    {CupColor_RED, CupOrientiation_DOWN, 1100, 800},
-    {CupColor_GREEN, CupOrientiation_DOWN, 1270, 1200},
+    {GlassColor_RED, GlassOrientiation_DOWN, 670, 100},
+    {GlassColor_GREEN, GlassOrientiation_DOWN, 950, 400},
+    {GlassColor_RED, GlassOrientiation_DOWN, 1100, 800},
+    {GlassColor_GREEN, GlassOrientiation_DOWN, 1270, 1200},
     /* Our distributor */
-    {CupColor_RED, CupOrientiation_UP, -67, 1450},
-    {CupColor_GREEN, CupOrientiation_UP, -67, 1525},
-    {CupColor_RED, CupOrientiation_UP, -67, 1600},
-    {CupColor_GREEN, CupOrientiation_UP, -67, 1675},
-    {CupColor_RED, CupOrientiation_UP, -67, 1750},
+    {GlassColor_RED, GlassOrientiation_UP, -67, 1450},
+    {GlassColor_GREEN, GlassOrientiation_UP, -67, 1525},
+    {GlassColor_RED, GlassOrientiation_UP, -67, 1600},
+    {GlassColor_GREEN, GlassOrientiation_UP, -67, 1675},
+    {GlassColor_RED, GlassOrientiation_UP, -67, 1750},
     /* Mixed distrubutor on our side*/
-    {CupColor_GREEN_OR_RED, CupOrientiation_UP, 700, -67},
-    {CupColor_GREEN_OR_RED, CupOrientiation_UP, 775, -67},
-    {CupColor_GREEN_OR_RED, CupOrientiation_UP, 850, -67},
-    {CupColor_GREEN_OR_RED, CupOrientiation_UP, 925, -67},
-    {CupColor_GREEN_OR_RED, CupOrientiation_UP, 1000, -67},
+    {GlassColor_UNKNOWN, GlassOrientiation_UP, 700, -67},
+    {GlassColor_UNKNOWN, GlassOrientiation_UP, 775, -67},
+    {GlassColor_UNKNOWN, GlassOrientiation_UP, 850, -67},
+    {GlassColor_UNKNOWN, GlassOrientiation_UP, 925, -67},
+    {GlassColor_UNKNOWN, GlassOrientiation_UP, 1000, -67},
     /*Rocky area*/
-    {CupColor_GREEN, CupOrientiation_DOWN, 1065, 1650},
-    {CupColor_RED, CupOrientiation_DOWN, 1005, 1955},
-    {CupColor_RED, CupOrientiation_DOWN, 1335, 1650},
-    {CupColor_GREEN, CupOrientiation_DOWN, 1395, 1995},
+    {GlassColor_GREEN, GlassOrientiation_DOWN, 1065, 1650},
+    {GlassColor_RED, GlassOrientiation_DOWN, 1005, 1955},
+    {GlassColor_RED, GlassOrientiation_DOWN, 1335, 1650},
+    {GlassColor_GREEN, GlassOrientiation_DOWN, 1395, 1995},
 };
 #endif /* STRATEGY_TABLE_H */
