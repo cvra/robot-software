1cc92cdbc1969da20b706aed0afb826347ff072c
Antoine Albertelli <antoinealb@google.com>
2020-03-30 22:10:45 +0200

    Manually handle chip select SPI line

--- actuator-firmware/src/pressure_sensor_interface.c
+++ actuator-firmware/src/pressure_sensor_interface.c
@@ -1,33 +1,43 @@
-#include "board.h"
 #include "pressure_sensor_interface.h"
 #include <ch.h>
 #include <hal.h>
 
-static void mpr_select(void* arg)
+#include "error/error.h"
+
+void mpr_start(void)
 {
     // SPI3 clock: PCLK1 = 36MHz
     static SPIConfig config = {
         .circular = false,
         .end_cb = NULL,
-        .ssport = GPIOA,
-        .sspad = GPIOA_CS1,
         .cr1 = SPI_CR1_BR_2 | SPI_CR1_BR_1 | SPI_CR1_BR_0,
-        .cr2 = SPI_CR2_DS_2 | SPI_CR2_DS_1 | SPI_CR2_DS_0};
+        .cr2 = SPI_CR2_DS_2 | SPI_CR2_DS_1 | SPI_CR2_DS_0
+    };
 
-    spiAcquireBus(&SPID3);
-    if ((int)arg == 0) {
-        config.sspad = GPIOA_CS1;
-    } else {
-        config.sspad = GPIOA_CS2;
-    }
     spiStart(&SPID3, &config);
-    spiSelect(&SPID3);
+}
+
+
+static void mpr_select(void* arg)
+{
+    spiAcquireBus(&SPID3);
+
+    if ((int)arg == 0) {
+        palClearPad(GPIOA, GPIOA_CS1);
+    } else {
+        palClearPad(GPIOA, GPIOA_CS2);
+    }
 }
 
 static void mpr_unselect(void* arg)
 {
     (void)arg;
-    spiUnselect(&SPID3);
+    if ((int)arg == 0) {
+        palSetPad(GPIOA, GPIOA_CS1);
+    } else {
+        palSetPad(GPIOA, GPIOA_CS2);
+    }
+
     spiReleaseBus(&SPID3);
 }
 
