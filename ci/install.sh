3a22f56c8251c2dddf031b9c85259788c3f5d66f
Antoine Albertelli <antoinealb@google.com>
2020-05-16 01:20:42 +0200

    Auto generate files that were manually done before

--- ci/install.sh
+++ ci/install.sh
@@ -20,6 +20,10 @@ pushd lib/uavcan/libuavcan/dsdl_compiler/
 python setup.py install
 popd
 
+pushd lib/nanopb/nanopb/generator/proto
+make
+popd
+
 if [ "$BUILD_TYPE" == "build-cmake" -o "$BUILD_TYPE" == "build-packager" ]
 then
     wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/8-2018q4/gcc-arm-none-eabi-8-2018-q4-major-linux.tar.bz2 -O arm-gcc-linux.tar.bz2
