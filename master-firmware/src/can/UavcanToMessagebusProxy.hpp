3b451c40dea79c51434bc311bcc7ab780a7952dd
Antoine Albertelli <antoinealb@google.com>
2020-09-11 15:08:38 +0200

    Abstract forwarding UAVCAN to msgbus

---
+++ master-firmware/src/can/UavcanToMessagebusProxy.hpp
@@ -0,0 +1,119 @@
+#pragma once
+
+#include <memory>
+#include <absl/strings/string_view.h>
+#include <absl/types/optional.h>
+#include <unordered_map>
+#include <can/bus_enumerator.h>
+#include <msgbus/messagebus.h>
+#include <msgbus_protobuf.h>
+#include <msgbus/posix/port.h>
+#include <pb.h>
+#include <uavcan/uavcan.hpp>
+
+// This class is responsible for forwarding messages coming in on the UAVCAN
+// bus to the software message bus.
+// It will create one topic per board that sends messages, and put messages
+// coming from this board on the corresponding topic.
+// This proxy can be useful when building integration with a new sensor or a new
+// board.
+// It is templated over the types of the messages (both on the UAVCAN side, as
+// well as on the messagebus side), as well as some metadata required for
+// protobuf.
+template <
+    typename UavcanMessage,
+    typename TopicMessage,
+    const pb_field_t TopicFields[],
+    uint32_t TopicMsgId>
+class UavcanToMessagebusProxy {
+    struct TopicData {
+        TopicMessage data;
+        condvar_wrapper_t var;
+        messagebus_topic_t topic;
+    };
+
+    bus_enumerator_t* bus_enumerator;
+    messagebus_t* msgbus;
+
+    topic_metadata_t metadata;
+
+    std::unique_ptr<uavcan::Subscriber<UavcanMessage>> subscriber;
+    std::unordered_map<std::string, TopicData> topic_map;
+
+public:
+    // Constructor. Takes a bus enumerator that will be used to gather the
+    // sender's board name from received messages, as well as the messagebus to
+    // send messages to.
+    UavcanToMessagebusProxy(bus_enumerator_t* be, messagebus_t* bus)
+        : bus_enumerator(be)
+        , msgbus(bus)
+    {
+        metadata.fields = TopicFields;
+        metadata.msgid = TopicMsgId;
+    };
+
+    // Transforms a board name into a topic name. For example, a user
+    // implementing some distance sensor integration might want to transform
+    // messages coming from a board named "front-left" into
+    // "/distance/front-left"
+    // This must be implemented by users of this class.
+    virtual std::string topic_name(absl::string_view board_name) = 0;
+
+    // Returns the message translated to messagebus format, or empty if the
+    // message should not be forwarded to the bus.
+    // This must be implemented by users of this class.
+    virtual absl::optional<TopicMessage> translate(const UavcanMessage& in) = 0;
+
+    // Starts a listener for the provided UAVCAN type on the node, and starts
+    // forwarding messages.
+    void start(uavcan::INode& node)
+    {
+        subscriber = std::make_unique<uavcan::Subscriber<UavcanMessage>>(node);
+        subscriber->start([&](const uavcan::ReceivedDataStructure<UavcanMessage>& msg) {
+            process(msg, msg.getSrcNodeID().get());
+        });
+    }
+
+protected:
+    void process(const UavcanMessage& msg, uint8_t src_id)
+    {
+        // Finds out the name of the sender from the bus enumerator and abors
+        // if that sender is not known yet.
+        const char* board_name = bus_enumerator_get_str_id(bus_enumerator, src_id);
+
+        if (!board_name) {
+            // TODO(antoinealb): Maybe we want to log that failure
+            return;
+        }
+
+        messagebus_topic_t* topic = find_or_create_topic(topic_name(board_name));
+
+        // Sends the message to messagebus if the user was able to translate it
+        auto topic_msg_opt = translate(msg);
+        if (topic_msg_opt) {
+            messagebus_topic_publish(topic, &(*topic_msg_opt),
+                                     sizeof(TopicMessage));
+        }
+    }
+
+    messagebus_topic_t* find_or_create_topic(std::string topic_name)
+    {
+        // If the topic already exists, return it immediately
+        auto elem = topic_map.find(topic_name);
+        if (elem != topic_map.end()) {
+            return &elem->second.topic;
+        }
+
+        // Otherwise create it from scratch and store it in the map
+        topic_map.insert({topic_name, {}});
+
+        messagebus_topic_init(&topic_map[topic_name].topic,
+                              &topic_map[topic_name].var,
+                              &topic_map[topic_name].var,
+                              &topic_map[topic_name].data, sizeof(TopicMessage));
+        topic_map[topic_name].topic.metadata = &metadata;
+        messagebus_advertise_topic(msgbus, &topic_map[topic_name].topic, topic_name.c_str());
+
+        return &topic_map[topic_name].topic;
+    }
+};
---
+++ master-firmware/tests/uavcan_to_messagebus_test.cpp
@@ -0,0 +1,118 @@
+#include <absl/strings/str_cat.h>
+#include <uavcan_linux/uavcan_linux.hpp>
+#include "can/UavcanToMessagebusProxy.hpp"
+#include <msgbus/messagebus.h>
+
+#include <CppUTest/TestHarness.h>
+#include <CppUTestExt/MockSupport.h>
+
+#include <protobuf/beacons.pb.h>
+#include <cvra/proximity_beacon/Signal.hpp>
+
+using ProxyBase = UavcanToMessagebusProxy<cvra::proximity_beacon::Signal, BeaconSignal, BeaconSignal_fields, BeaconSignal_msgid>;
+struct Proxy : public ProxyBase {
+    Proxy(bus_enumerator_t* be, messagebus_t* bus)
+        : ProxyBase(be, bus)
+    {
+    }
+
+    std::string topic_name(absl::string_view board_name) override
+    {
+        return absl::StrCat("/", board_name);
+    }
+
+    absl::optional<BeaconSignal> translate(const cvra::proximity_beacon::Signal& in) override
+    {
+        // Example translation function. In general you would have a more
+        // complicated translation function here
+        BeaconSignal out;
+        out.range.range.distance = in.length;
+        return {out};
+    }
+
+    // for test only, expose process as a method, which would usually be called
+    // from UAVCAN
+    void process(const cvra::proximity_beacon::Signal& msg, uint8_t src_id)
+    {
+        ProxyBase::process(msg, src_id);
+    }
+};
+
+TEST_GROUP (ProxyTestGroup) {
+    bus_enumerator_t be;
+    bus_enumerator_entry_allocator bea[10];
+    messagebus_t bus;
+    Proxy proxy{&be, &bus};
+    cvra::proximity_beacon::Signal signal;
+
+    void setup()
+    {
+        bus_enumerator_init(&be, bea, 10);
+        bus_enumerator_add_node(&be, "myboard", nullptr);
+        messagebus_init(&bus, nullptr, nullptr);
+    }
+};
+
+TEST(ProxyTestGroup, DoesNothingWhenUnknownBoardIsThere)
+{
+    proxy.process(signal, 42);
+}
+
+TEST(ProxyTestGroup, CreatesTopicForKnownBoards)
+{
+    bus_enumerator_update_node_info(&be, "myboard", 42);
+    proxy.process(signal, 42);
+
+    auto* topic = messagebus_find_topic(&bus, "/myboard");
+    CHECK_TRUE_TEXT(topic, "A topic should be created matching on received messages");
+}
+
+TEST(ProxyTestGroup, DoesNotDoubleTopic)
+{
+    bus_enumerator_update_node_info(&be, "myboard", 42);
+    proxy.process(signal, 42);
+    auto* topic1 = messagebus_find_topic(&bus, "/myboard");
+    proxy.process(signal, 42);
+    auto* topic2 = messagebus_find_topic(&bus, "/myboard");
+    CHECK_EQUAL(topic1, topic2);
+}
+
+TEST(ProxyTestGroup, ForwardsCANReceivedMessages)
+{
+    bus_enumerator_update_node_info(&be, "myboard", 42);
+    signal.length = 42.;
+    proxy.process(signal, 42);
+    auto* topic = messagebus_find_topic(&bus, "/myboard");
+    BeaconSignal topic_data;
+    auto success = messagebus_topic_read(topic, &topic_data, sizeof topic_data);
+    CHECK_TRUE_TEXT(success, "Topic should contain proxied data");
+
+    // Finally, check that the translation function was called
+    CHECK_EQUAL(topic_data.range.range.distance, signal.length);
+}
+
+struct NonForwardingProxy : public Proxy {
+    absl::optional<BeaconSignal> translate(const cvra::proximity_beacon::Signal& /* in */) override
+    {
+        // Do not forward message by returning an empty optional
+        return {};
+    }
+
+    NonForwardingProxy(bus_enumerator_t* be, messagebus_t* bus)
+        : Proxy(be, bus)
+    {
+    }
+};
+
+TEST(ProxyTestGroup, NonForwardingMessageStillCreatesTopic)
+{
+    NonForwardingProxy nonproxy{&be, &bus};
+    bus_enumerator_update_node_info(&be, "myboard", 42);
+    signal.length = 42.;
+    nonproxy.process(signal, 42);
+    auto* topic = messagebus_find_topic(&bus, "/myboard");
+    CHECK_TRUE(topic);
+    BeaconSignal topic_data;
+    auto success = messagebus_topic_read(topic, &topic_data, sizeof topic_data);
+    CHECK_FALSE_TEXT(success, "The topic should not have been published");
+}
