7d64153c7c9c90b9bd64aaad100cf62e643597cc
Michael Spieler <mspieler101@gmail.com>
2020-09-27 17:20:28 +0200

    Pressure sensor look out for status error

--- actuator-firmware/src/uavcan/feedback_publisher.cpp
+++ actuator-firmware/src/uavcan/feedback_publisher.cpp
@@ -21,11 +21,25 @@ void feedback_publish(uavcan::INode& node)
     msg.analog_input[1] = analog[1];
 
     for (int i = 0; i < 2; i++) {
-        /* TODO(antoinealb): Cleanup the sequencing and check sensor status */
-        mpr_start_measurement(&pressure_sensors[i]);
-        chThdSleepMilliseconds(10);
+        uint8_t status;
 
         mpr_start_measurement(&pressure_sensors[i]);
+        for (int timeout_ms = 10; timeout_ms > 0; timeout_ms--) {
+            status = mpr_read_status(&pressure_sensors[i]);
+            if (!mpr_status_is_busy(status)) {
+                break;
+            }
+            chThdSleepMilliseconds(1);
+        }
+
+        if (mpr_status_is_error(status) || mpr_status_is_busy(status)) {
+            WARNING("Pressure sensor %d status error: 0x%x", i, status);
+            continue; // discard reading on error
+        }
+
+        // TODO(mspieler): Why is another start measurement required?
+        mpr_start_measurement(&pressure_sensors[i]);
+
         uint32_t pressure = mpr_read_data(&pressure_sensors[i]);
         msg.pressure[i] = mpr_pressure_raw_to_pascal(pressure);
     }
